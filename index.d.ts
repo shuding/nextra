f30fa7ef19509914030b4d1a77715c0f4763c8b4
rot1024 <aayhrot@gmail.com>
2020-12-21 21:16:38 +0900

    add locale type

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,8 @@ import { ComponentType, PropsWithChildren } from "react";
 
 export type Page =
   | { name: string; route: string; children: PageMap }
-  | { name: string; route: string; frontMatter?: FrontMatter; locale: string }
-  | { name: "meta.json"; meta: Meta; locale: string };
+  | { name: string; route: string; frontMatter?: FrontMatter; locale: Locale }
+  | { name: "meta.json"; meta: Meta; locale: Locale };
 export type PageMap = Page[];
 export type Options = {
   filename: string;
@@ -15,6 +15,7 @@ export type WithLayout = (opts: Options, config: Config) => ComponentType<PropsW
 const WithNextra: (theme: string, themeConfig: string) => (nextConf: any) => any;
 export default WithNextra;
 
+export type Locale = string;
 export type Config = any;
 export type Meta = any;
 export type FrontMatter = any;
