e8a9f78c9ef27b2c07c1a6ab521f9a874bfdce72
Roberto Vidal <vidal.roberto.j@gmail.com>
2022-02-08 09:32:50 +0100

    fix sort-date

--- packages/nextra-theme-blog/src/utils/sort-date.ts
+++ packages/nextra-theme-blog/src/utils/sort-date.ts
@@ -3,5 +3,6 @@ import { PageMapItem } from 'nextra'
 export default (a: PageMapItem, b: PageMapItem) => {
   if (!a.frontMatter || !a.frontMatter.date) return -1
   if (!b.frontMatter || !b.frontMatter.date) return -1
-  return new Date(a.frontMatter.date) > new Date(b.frontMatter.date) ? -1 : 1
+  
+  return new Date(a.frontMatter.date) - new Date(b.frontMatter.date);
 }
