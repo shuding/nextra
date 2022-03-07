117eac8d732e538de918bc94c863f06a06e3cd21
Shu Ding <g@shud.in>
2022-03-07 13:16:38 +0100

    tweak animation

--- packages/nextra-theme-docs/src/polyfill.tsx
+++ packages/nextra-theme-docs/src/polyfill.tsx
@@ -1,18 +1,30 @@
 if (typeof window !== 'undefined') {
   let onResize
+  let resizeTimer: any
+
+  const addResizingClass = () => {
+    document.body.classList.add('resizing')
+    clearTimeout(resizeTimer)
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove('resizing')
+    }, 200)
+  }
+
   if (window.visualViewport) {
     onResize = () => {
       const vh = window.visualViewport.height
       document.documentElement.style.setProperty('--vh', `${vh}px`)
+      addResizingClass()
     }
-    window.visualViewport.addEventListener('resize', onResize)
     onResize()
+    window.visualViewport.addEventListener('resize', onResize)
   } else {
     onResize = () => {
       const vh = window.innerHeight
       document.documentElement.style.setProperty('--vh', `${vh}px`)
+      addResizingClass()
     }
-    window.addEventListener('resize', onResize)
     onResize()
+    window.addEventListener('resize', onResize)
   }
 }
--- packages/nextra-theme-docs/tailwind.config.js
+++ packages/nextra-theme-docs/tailwind.config.js
@@ -8,8 +8,7 @@ module.exports = {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      '2xl': '1536px',
-      mobile: { max: '767px' }
+      '2xl': '1536px'
     },
     fontSize: {
       xs: '.75rem',
