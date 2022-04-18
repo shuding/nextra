32e5418477e99bee0932047a4025015106667c7c
Shu Ding <g@shud.in>
2022-04-18 23:19:58 +0200

    fix toc gradient

--- packages/nextra-theme-docs/src/toc.tsx
+++ packages/nextra-theme-docs/src/toc.tsx
@@ -205,7 +205,7 @@ export default function ToC({
 
   return (
     <div className="nextra-toc w-64 hidden xl:block text-sm px-4">
-      <div className="overflow-y-auto pr-4 -mr-4 sticky max-h-[calc(100vh-4rem-env(safe-area-inset-bottom))] top-16 pt-8">
+      <div className="nextra-toc-content overflow-y-auto pr-4 -mr-4 sticky max-h-[calc(100vh-4rem-env(safe-area-inset-bottom))] top-16 pt-8">
         {hasHeadings && headings ? (
           <ul>
             <p className="font-semibold tracking-tight mb-4">On This Page</p>
