86b02a35380b90fc9dbd106c53338c07e3b782b9
Jiachi Liu <inbox@huozhi.im>
2022-07-16 15:21:34 +0200

    extract locale for w/o trailing slash

--- packages/nextra/src/locales.ts
+++ packages/nextra/src/locales.ts
@@ -2,6 +2,15 @@ import { NextRequest, NextResponse } from 'next/server'
 
 const PUBLIC_FILE = /\.(.*)$/
 
+type LegacyMiddlewareCookies = { [key: string]: string }
+type StableMiddlewareCookies = Map<string, string>
+function getCookie(cookies: LegacyMiddlewareCookies | StableMiddlewareCookies, key: string) {
+  if (typeof cookies.get === 'function') {
+    return cookies.get(key)
+  }
+  return (cookies as LegacyMiddlewareCookies)[key]
+}
+
 export function locales(request: NextRequest) {
   const { nextUrl } = request
 
@@ -16,12 +25,11 @@ export function locales(request: NextRequest) {
   // The locale code prefixed in the current URL, which can be empty.
   const fullUrl = nextUrl.toString()
   const hasEndingSlash = nextUrl.pathname.endsWith('/')
-  let localeInPath = fullUrl
+  const localeInPath = fullUrl
     .slice(fullUrl.indexOf('//' + nextUrl.host) + nextUrl.host.length + 3)
-    .slice(0, -(nextUrl.pathname + nextUrl.search).length + (hasEndingSlash ? 1 : 0))
+    .slice(0, hasEndingSlash ? undefined : -(nextUrl.pathname + nextUrl.search).length)
 
   let finalLocale
-
   if (localeInPath) {
     // If a locale is explicitly set, we don't do any modifications.
     finalLocale = localeInPath
@@ -29,15 +37,26 @@ export function locales(request: NextRequest) {
     // If there is a locale cookie, we try to use it. If it doesn't exist or
     // it's invalid, `nextUrl.locale` will be automatically figured out by Next
     // via the `accept-languages` header.
-    if (request.cookies.has('NEXT_LOCALE')) {
+    if (getCookie(request.cookies, 'NEXT_LOCALE')) {
       try {
-        nextUrl.locale = request.cookies.get('NEXT_LOCALE')!
+        nextUrl.locale = getCookie(request.cookies, 'NEXT_LOCALE')!
       } catch (err) {
         // The locale from the cookie isn't valid.
         // https://github.com/vercel/next.js/blob/e5dee17f776dcc79ebb269f7b7341fa6e2b6c3f1/packages/next/server/web/next-url.ts#L122-L129
       }
     }
     finalLocale = nextUrl.locale
+    // Now we want to display the locale. If it's not the default one, we have
+    // to prefix the URL with that locale since it's missing. Only the default
+    // locale can be missing from there for consistency.
+    if (finalLocale !== nextUrl.defaultLocale) {
+      return NextResponse.redirect(
+        new URL(
+          '/' + finalLocale + nextUrl.pathname + nextUrl.search,
+          request.url
+        )
+      )
+    }
   }
 
   let pathname = nextUrl.pathname
