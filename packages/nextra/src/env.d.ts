44aad8cc2e27810426367435f3e590dc2dc40561
Dimitri POSTOLOV <en3m@ya.ru>
2022-07-22 01:09:05 +0200

    fix(nextra): Update env.d.ts (#548)
    Update env.d.ts

--- packages/nextra/src/env.d.ts
+++ packages/nextra/src/env.d.ts
@@ -1,11 +1,3 @@
-declare module 'slash' {
-  export default function slash(path: string): string
-}
-
-declare module 'download' {
-  export default function dwonload(url: string, path: string): Promise<void>
-}
-
 declare module 'github-slugger' {
   export default class Slugger {
     slug(data: string): string
