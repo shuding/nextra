97e6141addf0ffb6c6799dbed25aa97244a22e3a
Dimitri POSTOLOV <en3m@ya.ru>
2022-07-23 22:27:18 +0200

    fix(nextra/docs): fallback search to `en-US` instead `default` (#574)

--- packages/nextra/src/loader.ts
+++ packages/nextra/src/loader.ts
@@ -11,7 +11,7 @@ import { parseFileName } from './utils'
 import { compileMdx } from './compile'
 import { getPageMap, findPagesDir } from './page-map'
 import { collectFiles, collectMdx } from './plugin'
-import { MARKDOWN_EXTENSION_REGEX, IS_PRODUCTION } from './constants'
+import { MARKDOWN_EXTENSION_REGEX, IS_PRODUCTION, DEFAULT_LOCALE } from './constants'
 
 // TODO: create this as a webpack plugin.
 const indexContentEmitted = new Set<string>()
@@ -130,7 +130,7 @@ async function loader(
     // We only add .MD and .MDX contents
     if (MARKDOWN_EXTENSION_REGEX.test(filename) && data.searchable !== false) {
       addPage({
-        fileLocale: fileLocale || 'default',
+        fileLocale: fileLocale || DEFAULT_LOCALE,
         route,
         title,
         data,
