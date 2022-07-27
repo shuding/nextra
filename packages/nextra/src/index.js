fd418ab9b817351cb9c839d5ed7e84dc3821be5a
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-07-27 12:17:23 +0200

    Refactor: make i18n mutation easier to read

--- packages/nextra/src/index.js
+++ packages/nextra/src/index.js
@@ -15,28 +15,27 @@ const nextra = (...args) =>
         : args[0]
 
     const nextraPlugin = new NextraPlugin(nextraConfig)
-    const { i18n, pageExtensions = DEFAULT_EXTENSIONS } = nextConfig
-    let defaulti18n = i18n || {};
+    const { pageExtensions = DEFAULT_EXTENSIONS } = nextConfig
 
-    if (i18n?.locales) {
+    if (nextConfig.i18n?.locales) {
       console.log(
         '[nextra] You have Next.js i18n enabled, read here https://nextjs.org/docs/advanced-features/i18n-routing for the docs.'
       )
-    } else if (!i18n?.defaultLocale) {
+    }
+
+    const i18n = nextConfig.i18n || {
       // If `i18n.locales` and `i18n.defaultLocale` were not specified,
       // client will receive error - Text content does not match server-rendered HTML.
       // Due to `const { locale } = useRouter()` where `locale` will be `undefined`
       // To fix it we need to explicitly specify `i18n.locales` and `i18n.defaultLocale`
-      defaulti18n = {
-        ...i18n,
-        locales: [DEFAULT_LOCALE],
-        defaultLocale: DEFAULT_LOCALE
-      }
+      ...nextConfig.i18n,
+      locales: nextConfig.i18n.locales || [DEFAULT_LOCALE],
+      defaultLocale: nextConfig.i18n.defaultLocale || DEFAULT_LOCALE
     }
 
     return {
       ...nextConfig,
-      i18n: defaulti18n,
+      i18n,
       pageExtensions: [...pageExtensions, ...MARKDOWN_EXTENSIONS],
       webpack(config, options) {
         config.plugins ||= []
@@ -50,8 +49,8 @@ const nextra = (...args) =>
               loader: 'nextra/loader',
               options: {
                 ...nextraConfig,
-                locales: defaulti18n.locales,
-                defaultLocale: defaulti18n.defaultLocale,
+                locales: i18n.locales,
+                defaultLocale: i18n.defaultLocale,
                 pageMapCache
               }
             }
