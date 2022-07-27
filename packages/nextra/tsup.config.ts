5841053c7d6c1ca6c0de3dc21e8788581620d2b6
Yixuan Xu <yixuanxu94@outlook.com>
2022-07-28 00:25:05 +0800

    fix: wrong entry

--- packages/nextra/tsup.config.ts
+++ packages/nextra/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 export default defineConfig([
   {
-    entry: ['src/index.ts'],
+    entry: ['src/index.js'],
     format: 'cjs',
     name: 'nextra',
     dts: false
