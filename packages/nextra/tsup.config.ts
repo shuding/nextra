4af42cbf322f804fb1e3b8d023af7b2baa486b41
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-07-27 13:09:04 +0200

    Fix add index.ts tsup

--- packages/nextra/tsup.config.ts
+++ packages/nextra/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 export default defineConfig([
   {
-    entry: ['src/index.js'],
+    entry: ['src/index.ts'],
     format: 'cjs',
     name: 'nextra',
     dts: false
