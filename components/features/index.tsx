0bdb05258cc4acefc5caec3aceff2e7de2b94ecd
Shu Ding <g@shud.in>
2022-08-01 20:09:43 +0200

    (wip) new homepage

--- components/features/index.tsx
+++ components/features/index.tsx
@@ -1,13 +1,21 @@
 import cn from 'clsx'
 import styles from './style.module.css'
 
-export function Feature({ large, centered, children, className, ...props }) {
+export function Feature({
+  large,
+  centered,
+  children,
+  lightOnly,
+  className,
+  ...props
+}) {
   return (
     <div
       className={cn(
         styles.feature,
         large && styles.large,
         centered && styles.centered,
+        lightOnly && styles['light-only'],
         className
       )}
       {...props}
