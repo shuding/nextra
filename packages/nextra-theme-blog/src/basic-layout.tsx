abbee8739e3d7d07f3c2a6deaddaf1598d94a6bc
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-07 16:52:40 +0200

    fix `Warning: Prop `href` did not match. Server: "#" Client: ...`

--- packages/nextra-theme-blog/src/basic-layout.tsx
+++ packages/nextra-theme-blog/src/basic-layout.tsx
@@ -1,15 +1,12 @@
 import Head from 'next/head'
-import React, { ReactNode, useEffect } from 'react'
+import React, { ReactNode, useRef } from 'react'
 import { useBlogContext } from './blog-context'
 import { HeadingContext } from './mdx-theme'
-import { getTitle } from './utils/title'
 
 export const BasicLayout = ({ children }: { children: ReactNode }) => {
   const { config, opts } = useBlogContext()
-  const { title, pageTitle } = getTitle({ config, opts })
-  const ref = React.useRef<HTMLHeadingElement>(null)
-  const { hasJsxInH1 } = opts
-  const portalHeading = hasJsxInH1
+  const title = `${opts.title}${config.titleSuffix || ''}`
+  const ref = useRef<HTMLHeadingElement>(null)
   return (
     <article className="container prose prose-sm dark:prose-dark md:prose">
       <Head>
@@ -17,8 +14,8 @@ export const BasicLayout = ({ children }: { children: ReactNode }) => {
         {config.head?.({ title, meta: opts.meta })}
       </Head>
       <HeadingContext.Provider value={ref}>
-        {portalHeading ? <h1 ref={ref} /> : null}
-        {!portalHeading ? <h1>{pageTitle}</h1> : null}
+        {opts.hasJsxInH1 ? <h1 ref={ref} /> : null}
+        {!opts.hasJsxInH1 ? <h1>{opts.title}</h1> : null}
         {children}
         {config.footer}
       </HeadingContext.Provider>
--- packages/nextra-theme-docs/src/config.ts
+++ packages/nextra-theme-docs/src/config.ts
@@ -1,11 +1,8 @@
-import { PageOpts } from 'nextra'
 import { createContext, useContext } from 'react'
-import { DocsThemeConfig } from './types'
+import { Config } from './types'
 
-interface Config extends DocsThemeConfig {
-  unstable_flexsearch?: PageOpts['unstable_flexsearch']
-  newNextLinkBehavior?: PageOpts['newNextLinkBehavior']
-}
-
-export const ThemeConfigContext = createContext<Config>({})
+export const ThemeConfigContext = createContext<Config>({
+  title: '',
+  meta: {}
+})
 export const useConfig = () => useContext(ThemeConfigContext)
