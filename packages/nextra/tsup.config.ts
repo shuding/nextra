e4dcfae766d97f32576e66ea2c9ce80f3651ccd9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-31 02:54:51 +0300

    compile `context.ts`, `ssg.ts` and `locales.ts` to esm

--- packages/nextra/tsup.config.ts
+++ packages/nextra/tsup.config.ts
@@ -12,31 +12,19 @@ export default defineConfig([
     target
   },
   {
-    name: 'nextra-utils',
-    entry: ['src/ssg.ts', 'src/locales.ts', 'src/context.ts'],
-    format: 'cjs',
-    external: ['next/server'],
-    dts: true,
-    target
-  },
-  {
-    name: 'nextra-loader',
-    entry: ['src/loader.ts'],
-    format: 'esm',
-    dts: true,
-    target
-  },
-  {
     name: 'nextra-esm',
     entry: [
       'src/loader.ts',
       'src/compile.ts',
       'src/icons/index.ts',
-      'src/components/index.ts'
+      'src/components/index.ts',
+      'src/ssg.ts',
+      'src/locales.ts',
+      'src/context.ts'
     ],
     format: 'esm',
     dts: true,
-    target,
+    target
   },
   {
     entry: ['src/types.ts'],
