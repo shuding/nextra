26966e691ae9084c3b7d67e758eb38994c0e1f28
Lucas Friedrich Faehnrich <lucasfaehnrich@icloud.com>
2022-10-13 21:23:58 +0200

    added dynamic git basepaths to support self-hosting

--- packages/nextra-theme-docs/src/utils/get-git-issue-url.ts
+++ packages/nextra-theme-docs/src/utils/get-git-issue-url.ts
@@ -13,11 +13,11 @@ export const getGitIssueUrl = ({
   if (!repo) throw new Error('Invalid `docsRepositoryBase` URL!')
 
   if (repo.resource.includes('gitlab')) {
-    return `https://gitlab.com/${repo.owner}/${
+    return `${repo.protocol}:${repo.resource}/${repo.owner}/${
       repo.name
     }/-/issues/new?issue[title]=${encodeURIComponent(title)}`
   } else if (repo.resource.includes('github')) {
-    return `https://github.com/${repo.owner}/${
+    return `${repo.protocol}:${repo.resource}/${
       repo.name
     }/issues/new?title=${encodeURIComponent(title)}&labels=${labels || ''}`
   } else {
