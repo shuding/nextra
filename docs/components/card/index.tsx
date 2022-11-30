0b37dccf904cd3538ed5d8fc9016747a9d8c274d
Shu Ding <g@shud.in>
2022-11-30 14:57:46 +0100

    update docs to use next 13

--- docs/components/card/index.tsx
+++ docs/components/card/index.tsx
@@ -17,42 +17,16 @@ export function Card({ children, title, icon, image, arrow, href, ...props }) {
 
   if (image) {
     return (
-      <Link href={href} passHref>
-        <a
-          className={cn(
-            styles.card,
-            'group flex flex-col justify-start overflow-hidden rounded-lg border border-transparent bg-gray-100 text-current no-underline shadow shadow-gray-200 transition-all duration-200',
-            'hover:border-gray-200 hover:shadow-lg hover:shadow-gray-200'
-          )}
-          {...props}
-        >
-          {children}
-          <span
-            className={cn(
-              styles.title,
-              'gap-1 p-4 text-gray-700',
-              'hover:text-gray-900'
-            )}
-          >
-            {icon}
-            {title}
-            {animatedArrow}
-          </span>
-        </a>
-      </Link>
-    )
-  }
-
-  return (
-    <Link href={href} passHref>
-      <a
+      <Link
+        href={href}
         className={cn(
           styles.card,
-          'group flex flex-col justify-start overflow-hidden rounded-lg border border-transparent bg-gray-100 text-current no-underline shadow shadow-transparent transition-all duration-200',
-          'hover:border-gray-200 hover:shadow-md hover:shadow-gray-100'
+          'group flex flex-col justify-start overflow-hidden rounded-lg border border-transparent bg-gray-100 text-current no-underline shadow shadow-gray-200 transition-all duration-200',
+          'hover:border-gray-200 hover:shadow-lg hover:shadow-gray-200'
         )}
         {...props}
       >
+        {children}
         <span
           className={cn(
             styles.title,
@@ -64,7 +38,31 @@ export function Card({ children, title, icon, image, arrow, href, ...props }) {
           {title}
           {animatedArrow}
         </span>
-      </a>
+      </Link>
+    )
+  }
+
+  return (
+    <Link
+      href={href}
+      className={cn(
+        styles.card,
+        'group flex flex-col justify-start overflow-hidden rounded-lg border border-transparent bg-gray-100 text-current no-underline shadow shadow-transparent transition-all duration-200',
+        'hover:border-gray-200 hover:shadow-md hover:shadow-gray-100'
+      )}
+      {...props}
+    >
+      <span
+        className={cn(
+          styles.title,
+          'gap-1 p-4 text-gray-700',
+          'hover:text-gray-900'
+        )}
+      >
+        {icon}
+        {title}
+        {animatedArrow}
+      </span>
     </Link>
   )
 }
