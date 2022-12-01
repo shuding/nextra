bdd5b57f016df7497f50d91836194d584c58cc78
Shu Ding <g@shud.in>
2022-12-01 03:37:19 +0100

    more docs work

--- docs/components/card/index.tsx
+++ docs/components/card/index.tsx
@@ -58,16 +58,16 @@ export function Card({
       href={href}
       className={cn(
         styles.card,
-        'group flex flex-col justify-start overflow-hidden rounded-lg border border-gray-200 text-current no-underline shadow-sm shadow-gray-100 transition-all duration-200',
-        'hover:border-gray-300 hover:bg-slate-50 hover:shadow-md hover:shadow-gray-100'
+        'group flex flex-col justify-start overflow-hidden rounded-lg border border-gray-200 bg-transparent text-current no-underline shadow-sm shadow-gray-100 transition-all duration-200 dark:border-neutral-800 dark:shadow-none',
+        'hover:border-gray-300 hover:bg-slate-50 hover:shadow-md hover:shadow-gray-100 dark:hover:border-neutral-700 dark:hover:bg-neutral-900 dark:hover:shadow-none'
       )}
       {...props}
     >
       <span
         className={cn(
           styles.title,
-          'gap-2 p-4 text-gray-700',
-          'hover:text-gray-900'
+          'gap-2 p-4 text-gray-700 dark:text-neutral-400',
+          'hover:text-gray-900 dark:hover:text-neutral-100'
         )}
       >
         {icon}
