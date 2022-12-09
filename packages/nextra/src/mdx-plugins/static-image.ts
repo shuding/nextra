43c96026c07ee94181b4509dcf25dab0ef8dd5bc
Yixuan Xu <yixuanxu94@outlook.com>
2022-12-10 04:40:00 +0800

    should only use blur placeholder when file type is jpeg,png,webp,avif

--- packages/nextra/src/mdx-plugins/static-image.ts
+++ packages/nextra/src/mdx-plugins/static-image.ts
@@ -6,6 +6,8 @@ import { truthy } from '../utils'
 import { existsSync } from '../file-system'
 import { EXTERNAL_URL_REGEX, PUBLIC_DIR } from '../constants'
 
+const blurFormat = [".jpeg", ".png", ".webp", ".avif"]
+
 const getASTNodeImport = (name: string, from: string) => ({
   type: 'mdxjsEsm',
   value: `import ${name} from "${from}"`,
@@ -33,90 +35,91 @@ const getASTNodeImport = (name: string, from: string) => ({
   }
 })
 
+
 // Based on the remark-embed-images project
 // https://github.com/remarkjs/remark-embed-images
 export const remarkStaticImage: Plugin<[{ filePath: string }], Root> =
   ({ filePath }) =>
-  (tree, _file, done) => {
-    const importsToInject: any[] = []
+    (tree, _file, done) => {
+      const importsToInject: any[] = []
 
-    visit(tree, 'image', node => {
-      let { url } = node
-      if (!url) {
-        console.warn(
-          `[nextra] File "${filePath}" contain image with empty "src" property, skipping…`
-        )
-        return
-      }
-
-      if (EXTERNAL_URL_REGEX.test(url)) {
-        // do nothing with images with external url
-        return
-      }
-
-      if (url.startsWith('/')) {
-        const urlPath = path.join(PUBLIC_DIR, url)
-        if (!existsSync(urlPath)) {
-          console.error(
-            `[nextra] File "${filePath}" contain image with url "${url}" that not found in "/public" directory, skipping…`
+      visit(tree, 'image', node => {
+        let { url } = node
+        if (!url) {
+          console.warn(
+            `[nextra] File "${filePath}" contain image with empty "src" property, skipping…`
           )
           return
         }
-        url = urlPath
-      }
-      // Unique variable name for the given static image URL.
-      const tempVariableName = `$nextraImage${importsToInject.length}`
 
-      // Replace the image node with a MDX component node (Next.js Image).
-      Object.assign(node, {
-        type: 'mdxJsxFlowElement',
-        name: '$NextImageNextra',
-        children: [],
-        attributes: [
-          // do not render empty alt in html markup
-          node.alt && {
-            type: 'mdxJsxAttribute',
-            name: 'alt',
-            value: node.alt
-          },
-          !url.endsWith('.svg') && {
-            type: 'mdxJsxAttribute',
-            name: 'placeholder',
-            value: 'blur'
-          },
-          {
-            type: 'mdxJsxAttribute',
-            name: 'src',
-            value: {
-              type: 'mdxJsxAttributeValueExpression',
-              value: tempVariableName,
-              data: {
-                estree: {
-                  type: 'Program',
-                  sourceType: 'module',
-                  body: [
-                    {
-                      type: 'ExpressionStatement',
-                      expression: {
-                        type: 'Identifier',
-                        name: tempVariableName
+        if (EXTERNAL_URL_REGEX.test(url)) {
+          // do nothing with images with external url
+          return
+        }
+
+        if (url.startsWith('/')) {
+          const urlPath = path.join(PUBLIC_DIR, url)
+          if (!existsSync(urlPath)) {
+            console.error(
+              `[nextra] File "${filePath}" contain image with url "${url}" that not found in "/public" directory, skipping…`
+            )
+            return
+          }
+          url = urlPath
+        }
+        // Unique variable name for the given static image URL.
+        const tempVariableName = `$nextraImage${importsToInject.length}`
+        const blur = blurFormat.some((ext) => url.endsWith(ext))
+        // Replace the image node with a MDX component node (Next.js Image).
+        Object.assign(node, {
+          type: 'mdxJsxFlowElement',
+          name: '$NextImageNextra',
+          children: [],
+          attributes: [
+            // do not render empty alt in html markup
+            node.alt && {
+              type: 'mdxJsxAttribute',
+              name: 'alt',
+              value: node.alt
+            },
+            blur && {
+              type: 'mdxJsxAttribute',
+              name: 'placeholder',
+              value: 'blur'
+            },
+            {
+              type: 'mdxJsxAttribute',
+              name: 'src',
+              value: {
+                type: 'mdxJsxAttributeValueExpression',
+                value: tempVariableName,
+                data: {
+                  estree: {
+                    type: 'Program',
+                    sourceType: 'module',
+                    body: [
+                      {
+                        type: 'ExpressionStatement',
+                        expression: {
+                          type: 'Identifier',
+                          name: tempVariableName
+                        }
                       }
-                    }
-                  ]
+                    ]
+                  }
                 }
               }
             }
-          }
-        ].filter(truthy)
+          ].filter(truthy)
+        })
+
+        // Inject the static image import into the root node.
+        importsToInject.push(getASTNodeImport(tempVariableName, url))
       })
 
-      // Inject the static image import into the root node.
-      importsToInject.push(getASTNodeImport(tempVariableName, url))
-    })
-
-    tree.children.unshift(
-      getASTNodeImport('$NextImageNextra', 'next/image') as any,
-      ...importsToInject
-    )
-    done()
-  }
+      tree.children.unshift(
+        getASTNodeImport('$NextImageNextra', 'next/image') as any,
+        ...importsToInject
+      )
+      done()
+    }
