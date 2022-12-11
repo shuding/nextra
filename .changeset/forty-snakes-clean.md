f488e2e35793fbe9ffdb86374ecb00a9589463cf
Yixuan Xu <yixuanxu94@outlook.com>
2022-12-11 19:53:16 +0800

    fix: staticImage should only set blur placeholder for jpeg,png,webp,avif (#1058)
    * should only use blur placeholder when file type is jpeg,png,webp,avif
    
    * should only use blur placeholder when file type is jpeg,png,webp,avif
    
    * update changesets
    
    * update test snapshots
    
    * chore: add comments about VALID_BLUR_EXT for staticImage
    
    * chore: add jpg
    
    * prettify
    
    * update changeset description
    
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

---
+++ .changeset/forty-snakes-clean.md
@@ -0,0 +1,9 @@
+---
+'nextra': patch
+'nextra-theme-blog': patch
+'nextra-theme-docs': patch
+---
+
+remove @react/skip-nev #1051
+
+fix: staticImage should only set blur placeholder for jpeg,png,webp,avif
--- packages/nextra/src/mdx-plugins/static-image.ts
+++ packages/nextra/src/mdx-plugins/static-image.ts
@@ -6,6 +6,12 @@ import { truthy } from '../utils'
 import { existsSync } from '../file-system'
 import { EXTERNAL_URL_REGEX, PUBLIC_DIR } from '../constants'
 
+/**
+ * @ link https://github.com/vercel/next.js/blob/6cfebfb02c2a52a1f99fca59a2eac2d704d053db/packages/next/build/webpack/loaders/next-image-loader.js#L6
+ * @ link https://github.com/vercel/next.js/blob/6cfebfb02c2a52a1f99fca59a2eac2d704d053db/packages/next/client/image.tsx#LL702
+ */
+const VALID_BLUR_EXT = ['.jpeg', '.png', '.webp', '.avif', '.jpg']
+
 const getASTNodeImport = (name: string, from: string) => ({
   type: 'mdxjsEsm',
   value: `import ${name} from "${from}"`,
@@ -66,7 +72,7 @@ export const remarkStaticImage: Plugin<[{ filePath: string }], Root> =
       }
       // Unique variable name for the given static image URL.
       const tempVariableName = `$nextraImage${importsToInject.length}`
-
+      const blur = VALID_BLUR_EXT.some(ext => url.endsWith(ext))
       // Replace the image node with a MDX component node (Next.js Image).
       Object.assign(node, {
         type: 'mdxJsxFlowElement',
@@ -79,7 +85,7 @@ export const remarkStaticImage: Plugin<[{ filePath: string }], Root> =
             name: 'alt',
             value: node.alt
           },
-          !url.endsWith('.svg') && {
+          blur && {
             type: 'mdxJsxAttribute',
             name: 'placeholder',
             value: 'blur'
