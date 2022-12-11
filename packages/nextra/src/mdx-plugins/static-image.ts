6acaa32e858e7ab0e0499c0c3ab01cc5f1f81c4b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-12-11 11:54:37 +0100

    prettify

--- packages/nextra/src/mdx-plugins/static-image.ts
+++ packages/nextra/src/mdx-plugins/static-image.ts
@@ -10,7 +10,7 @@ import { EXTERNAL_URL_REGEX, PUBLIC_DIR } from '../constants'
  * @ link https://github.com/vercel/next.js/blob/6cfebfb02c2a52a1f99fca59a2eac2d704d053db/packages/next/build/webpack/loaders/next-image-loader.js#L6
  * @ link https://github.com/vercel/next.js/blob/6cfebfb02c2a52a1f99fca59a2eac2d704d053db/packages/next/client/image.tsx#LL702
  */
-const VALID_BLUR_EXT = [".jpeg", ".png", ".webp", ".avif", '.jpg']
+const VALID_BLUR_EXT = ['.jpeg', '.png', '.webp', '.avif', '.jpg']
 
 const getASTNodeImport = (name: string, from: string) => ({
   type: 'mdxjsEsm',
@@ -39,91 +39,90 @@ const getASTNodeImport = (name: string, from: string) => ({
   }
 })
 
-
 // Based on the remark-embed-images project
 // https://github.com/remarkjs/remark-embed-images
 export const remarkStaticImage: Plugin<[{ filePath: string }], Root> =
   ({ filePath }) =>
-    (tree, _file, done) => {
-      const importsToInject: any[] = []
+  (tree, _file, done) => {
+    const importsToInject: any[] = []
 
-      visit(tree, 'image', node => {
-        let { url } = node
-        if (!url) {
-          console.warn(
-            `[nextra] File "${filePath}" contain image with empty "src" property, skipping…`
+    visit(tree, 'image', node => {
+      let { url } = node
+      if (!url) {
+        console.warn(
+          `[nextra] File "${filePath}" contain image with empty "src" property, skipping…`
+        )
+        return
+      }
+
+      if (EXTERNAL_URL_REGEX.test(url)) {
+        // do nothing with images with external url
+        return
+      }
+
+      if (url.startsWith('/')) {
+        const urlPath = path.join(PUBLIC_DIR, url)
+        if (!existsSync(urlPath)) {
+          console.error(
+            `[nextra] File "${filePath}" contain image with url "${url}" that not found in "/public" directory, skipping…`
           )
           return
         }
-
-        if (EXTERNAL_URL_REGEX.test(url)) {
-          // do nothing with images with external url
-          return
-        }
-
-        if (url.startsWith('/')) {
-          const urlPath = path.join(PUBLIC_DIR, url)
-          if (!existsSync(urlPath)) {
-            console.error(
-              `[nextra] File "${filePath}" contain image with url "${url}" that not found in "/public" directory, skipping…`
-            )
-            return
-          }
-          url = urlPath
-        }
-        // Unique variable name for the given static image URL.
-        const tempVariableName = `$nextraImage${importsToInject.length}`
-        const blur = VALID_BLUR_EXT.some((ext) => url.endsWith(ext))
-        // Replace the image node with a MDX component node (Next.js Image).
-        Object.assign(node, {
-          type: 'mdxJsxFlowElement',
-          name: '$NextImageNextra',
-          children: [],
-          attributes: [
-            // do not render empty alt in html markup
-            node.alt && {
-              type: 'mdxJsxAttribute',
-              name: 'alt',
-              value: node.alt
-            },
-            blur && {
-              type: 'mdxJsxAttribute',
-              name: 'placeholder',
-              value: 'blur'
-            },
-            {
-              type: 'mdxJsxAttribute',
-              name: 'src',
-              value: {
-                type: 'mdxJsxAttributeValueExpression',
-                value: tempVariableName,
-                data: {
-                  estree: {
-                    type: 'Program',
-                    sourceType: 'module',
-                    body: [
-                      {
-                        type: 'ExpressionStatement',
-                        expression: {
-                          type: 'Identifier',
-                          name: tempVariableName
-                        }
+        url = urlPath
+      }
+      // Unique variable name for the given static image URL.
+      const tempVariableName = `$nextraImage${importsToInject.length}`
+      const blur = VALID_BLUR_EXT.some(ext => url.endsWith(ext))
+      // Replace the image node with a MDX component node (Next.js Image).
+      Object.assign(node, {
+        type: 'mdxJsxFlowElement',
+        name: '$NextImageNextra',
+        children: [],
+        attributes: [
+          // do not render empty alt in html markup
+          node.alt && {
+            type: 'mdxJsxAttribute',
+            name: 'alt',
+            value: node.alt
+          },
+          blur && {
+            type: 'mdxJsxAttribute',
+            name: 'placeholder',
+            value: 'blur'
+          },
+          {
+            type: 'mdxJsxAttribute',
+            name: 'src',
+            value: {
+              type: 'mdxJsxAttributeValueExpression',
+              value: tempVariableName,
+              data: {
+                estree: {
+                  type: 'Program',
+                  sourceType: 'module',
+                  body: [
+                    {
+                      type: 'ExpressionStatement',
+                      expression: {
+                        type: 'Identifier',
+                        name: tempVariableName
                       }
-                    ]
-                  }
+                    }
+                  ]
                 }
               }
             }
-          ].filter(truthy)
-        })
-
-        // Inject the static image import into the root node.
-        importsToInject.push(getASTNodeImport(tempVariableName, url))
+          }
+        ].filter(truthy)
       })
 
-      tree.children.unshift(
-        getASTNodeImport('$NextImageNextra', 'next/image') as any,
-        ...importsToInject
-      )
-      done()
-    }
+      // Inject the static image import into the root node.
+      importsToInject.push(getASTNodeImport(tempVariableName, url))
+    })
+
+    tree.children.unshift(
+      getASTNodeImport('$NextImageNextra', 'next/image') as any,
+      ...importsToInject
+    )
+    done()
+  }
