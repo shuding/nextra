6fd94d4bf2e497b653f8d6905a9aeac14f56da9c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-12-25 17:10:51 +0100

    support `nextConfig.basePath` with i18n

--- examples/swr-site/next.config.js
+++ examples/swr-site/next.config.js
@@ -13,55 +13,54 @@ module.exports = withNextra({
     locales: ["en-US", "es-ES", "ja", "ko", "ru", "zh-CN"],
     defaultLocale: "en-US",
   },
+  // basePath: "/some-base-path",
   distDir: "./.next", // Nextra supports custom `nextConfig.distDir`
-  redirects: () => {
-    return [
-      // {
-      //   source: "/docs.([a-zA-Z-]+)",
-      //   destination: "/docs/getting-started",
-      //   statusCode: 301,
-      // },
-      // {
-      //   source: "/advanced/performance",
-      //   destination: "/docs/advanced/performance",
-      //   statusCode: 301,
-      // },
-      // {
-      //   source: "/advanced/cache",
-      //   destination: "/docs/advanced/cache",
-      //   statusCode: 301,
-      // },
-      // {
-      //   source: "/docs/cache",
-      //   destination: "/docs/advanced/cache",
-      //   statusCode: 301,
-      // },
-      {
-        source: "/change-log",
-        destination: "/docs/change-log",
-        statusCode: 301,
-      },
-      {
-        source: "/blog/swr-1",
-        destination: "/blog/swr-v1",
-        statusCode: 301,
-      },
-      {
-        source: "/docs.([a-zA-Z-]+)",
-        destination: "/docs/getting-started",
-        statusCode: 302,
-      },
-      {
-        source: "/docs",
-        destination: "/docs/getting-started",
-        statusCode: 302,
-      },
-      {
-        source: "/examples",
-        destination: "/examples/basic",
-        statusCode: 302,
-      },
-    ];
-  },
+  redirects: () => [
+    // {
+    //   source: "/docs.([a-zA-Z-]+)",
+    //   destination: "/docs/getting-started",
+    //   statusCode: 301,
+    // },
+    // {
+    //   source: "/advanced/performance",
+    //   destination: "/docs/advanced/performance",
+    //   statusCode: 301,
+    // },
+    // {
+    //   source: "/advanced/cache",
+    //   destination: "/docs/advanced/cache",
+    //   statusCode: 301,
+    // },
+    // {
+    //   source: "/docs/cache",
+    //   destination: "/docs/advanced/cache",
+    //   statusCode: 301,
+    // },
+    {
+      source: "/change-log",
+      destination: "/docs/change-log",
+      statusCode: 301,
+    },
+    {
+      source: "/blog/swr-1",
+      destination: "/blog/swr-v1",
+      statusCode: 301,
+    },
+    {
+      source: "/docs.([a-zA-Z-]+)",
+      destination: "/docs/getting-started",
+      statusCode: 302,
+    },
+    {
+      source: "/docs",
+      destination: "/docs/getting-started",
+      statusCode: 302,
+    },
+    {
+      source: "/examples",
+      destination: "/examples/basic",
+      statusCode: 302,
+    },
+  ],
   reactStrictMode: true,
 });
