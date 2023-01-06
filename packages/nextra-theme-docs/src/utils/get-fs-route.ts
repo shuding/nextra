dd498008de20a69a09c72de231a5f2e6b33b8a28
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-01-06 18:16:08 +0100

    lint with 'eslint:recommended', 'plugin:@typescript-eslint/recommended'

--- packages/nextra-theme-docs/src/utils/get-fs-route.ts
+++ packages/nextra-theme-docs/src/utils/get-fs-route.ts
@@ -3,7 +3,7 @@ const template = 'https://nextra.vercel.app'
 export const getFSRoute = (asPath: string, locale?: string | undefined) => {
   const pathname = new URL(asPath, template).pathname
   const cleanedPath = locale
-    ? pathname.replace(new RegExp(`\.${locale}(\/|$)`), '$1')
+    ? pathname.replace(new RegExp(`\\.${locale}(\\/|$)`), '$1')
     : pathname
   return (
     cleanedPath.replace(new RegExp('/index(/|$)'), '$1').split('#')[0] || '/'
