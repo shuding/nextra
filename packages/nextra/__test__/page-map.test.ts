30c849b6686db05e007b9de4383ce6354607603e
Shu Ding <g@shud.in>
2023-01-12 01:27:12 +0100

    fix tests

--- packages/nextra/__test__/page-map.test.ts
+++ packages/nextra/__test__/page-map.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect, beforeAll } from 'vitest'
 import path from 'node:path'
-import { getPageMap } from '../src/page-map'
+import { resolvePageMap } from '../src/page-map'
 import { collectFiles } from '../src/plugin'
 import { FileMap, PageMapItem } from '../src/types'
 import { CWD } from '../src/constants'
@@ -14,27 +14,27 @@ describe('Page Process', () => {
   let pageMap: PageMapItem[]
   let fileMap: FileMap
   beforeAll(async () => {
-    const { items, fileMap: data } = await collectFiles(PAGES_DIR)
+    const { items, fileMap: data } = await collectFiles(PAGES_DIR, [''])
     pageMap = items
     fileMap = data
   })
 
   it('pageMap en-US', () => {
-    const indexData = getPageMap({
+    const indexData = resolvePageMap({
       filePath: filePath('docs/data-fetching.en-US.mdx'),
-      pageMap,
+      items: pageMap,
       fileMap,
       defaultLocale
     })
     expect([
       indexData.pageMap,
       indexData.route,
-      indexData.title
+      indexData.dynamicMetaItems
     ]).toMatchSnapshot()
 
-    const gettingStartData = getPageMap({
+    const gettingStartData = resolvePageMap({
       filePath: filePath('docs/getting-started.en-US.mdx'),
-      pageMap,
+      items: pageMap,
       fileMap,
       defaultLocale
     })
@@ -42,21 +42,21 @@ describe('Page Process', () => {
   })
 
   it('pageMap zh-CN', () => {
-    const indexData = getPageMap({
+    const indexData = resolvePageMap({
       filePath: filePath('docs/data-fetching.zh-CN.mdx'),
-      pageMap,
+      items: pageMap,
       fileMap,
       defaultLocale
     })
     expect([
       indexData.pageMap,
       indexData.route,
-      indexData.title
+      indexData.dynamicMetaItems
     ]).toMatchSnapshot()
 
-    const gettingStartData = getPageMap({
+    const gettingStartData = resolvePageMap({
       filePath: filePath('docs/getting-started.zh-CN.mdx'),
-      pageMap,
+      items: pageMap,
       fileMap,
       defaultLocale
     })
