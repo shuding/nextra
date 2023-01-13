6bb4c43cfe46339d68adb4a4f90cfb0acdc0ccbc
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-01-13 20:11:08 +0100

    remove `data-rehype-pretty-code-fragment`

--- packages/nextra/src/components/pre.tsx
+++ packages/nextra/src/components/pre.tsx
@@ -1,11 +1,4 @@
-import {
-  ComponentProps,
-  ReactElement,
-  useCallback,
-  useRef,
-  useState,
-  useEffect
-} from 'react'
+import { ComponentProps, ReactElement, useCallback, useRef } from 'react'
 import { CopyToClipboard } from './copy-to-clipboard'
 import { Button } from './button'
 import { WordWrapIcon } from '../icons'
@@ -21,7 +14,6 @@ export const Pre = ({
   value?: string
 }): ReactElement => {
   const preRef = useRef<HTMLPreElement | null>(null)
-  const [codeString, setCodeString] = useState(value)
 
   const toggleWordWrap = useCallback(() => {
     const htmlDataset = document.documentElement.dataset
@@ -33,36 +25,26 @@ export const Pre = ({
     }
   }, [])
 
-  useEffect(() => {
-    const codeEl = preRef.current?.querySelector('code')
-
-    if (codeEl?.textContent) {
-      setCodeString(codeEl.textContent)
-    }
-  }, [preRef])
-
   return (
-    <>
+    <pre
+      className={[
+        'nextra-code nx-relative nx-mt-6 first:nx-mt-0 nx-bg-primary-700/5 nx-mb-4 nx-overflow-x-auto nx-rounded-xl nx-font-medium nx-subpixel-antialiased dark:nx-bg-primary-300/10 nx-text-[.9em]',
+        'contrast-more:nx-border contrast-more:nx-border-primary-900/20 contrast-more:nx-contrast-150 contrast-more:dark:nx-border-primary-100/40',
+        filename ? 'nx-pt-12 nx-pb-4' : 'nx-py-4',
+        className
+      ].join(' ')}
+      ref={preRef}
+      {...props}
+    >
       {filename && (
         <div className="nx-absolute nx-top-0 nx-z-[1] nx-w-full nx-truncate nx-rounded-t-xl nx-bg-primary-700/5 nx-py-2 nx-px-4 nx-text-xs nx-text-gray-700 dark:nx-bg-primary-300/10 dark:nx-text-gray-200">
           {filename}
         </div>
       )}
-      <pre
-        className={[
-          'nx-bg-primary-700/5 nx-mb-4 nx-overflow-x-auto nx-rounded-xl nx-font-medium nx-subpixel-antialiased dark:nx-bg-primary-300/10 nx-text-[.9em]',
-          'contrast-more:nx-border contrast-more:nx-border-primary-900/20 contrast-more:nx-contrast-150 contrast-more:dark:nx-border-primary-100/40',
-          filename ? 'nx-pt-12 nx-pb-4' : 'nx-py-4',
-          className
-        ].join(' ')}
-        ref={preRef}
-        {...props}
-      >
-        {children}
-      </pre>
+      {children}
       <div
         className={[
-          'nx-opacity-0 nx-transition [div:hover>&]:nx-opacity-100 focus-within:nx-opacity-100',
+          'nx-opacity-0 nx-transition [pre:hover>&]:nx-opacity-100 focus-within:nx-opacity-100',
           'nx-flex nx-gap-1 nx-absolute nx-m-[11px] nx-right-0',
           filename ? 'nx-top-8' : 'nx-top-0'
         ].join(' ')}
@@ -74,14 +56,14 @@ export const Pre = ({
         >
           <WordWrapIcon className="nx-pointer-events-none nx-h-4 nx-w-4" />
         </Button>
-        {codeString && (
+        {value && (
           <CopyToClipboard
-            getValue={() => {
-              return preRef.current?.querySelector('code')?.textContent || ''
-            }}
+            getValue={() =>
+              preRef.current?.querySelector('code')?.textContent || ''
+            }
           />
         )}
       </div>
-    </>
+    </pre>
   )
 }
