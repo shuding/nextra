9c5902e7d45695914be5743ed808697a63cb1afc
Shu Ding <g@shud.in>
2023-01-19 00:44:40 +0100

    fix dynamic imports

--- packages/nextra/src/utils.ts
+++ packages/nextra/src/utils.ts
@@ -71,3 +71,24 @@ export function isSerializable(o: any): boolean {
     return false
   }
 }
+
+/**
+ * Calculate a 32 bit FNV-1a hash
+ * Found here: https://gist.github.com/vaiorabbit/5657561
+ * Ref.: http://isthe.com/chongo/tech/comp/fnv/
+ *
+ * @param {string} str the input value
+ * @param {number} [seed] optionally pass the hash of the previous chunk
+ * @returns {string}
+ */
+export function hashFnv32a(str: string, seed = 0x811c9dc5): string {
+  let hval = seed
+
+  for (let i = 0; i < str.length; i++) {
+    hval ^= str.charCodeAt(i)
+    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24)
+  }
+
+  // Convert to 8 digit hex string
+  return ('0000000' + (hval >>> 0).toString(16)).substring(-8)
+}
