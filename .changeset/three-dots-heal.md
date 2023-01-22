065a856a4ebe476c82f2fb532ba8964bc58e5768
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-01-22 14:55:25 +0100

    use decodeURI in static-image.ts

---
+++ .changeset/three-dots-heal.md
@@ -0,0 +1,5 @@
+---
+'nextra': patch
+---
+
+use decodeURI in static-image.ts
--- packages/nextra/src/mdx-plugins/static-image.ts
+++ packages/nextra/src/mdx-plugins/static-image.ts
@@ -47,7 +47,7 @@ export const remarkStaticImage: Plugin<[{ filePath: string }], Root> =
     const importsToInject: any[] = []
 
     visit(tree, 'image', node => {
-      let { url } = node
+      const url = decodeURI(node.url)
       if (!url) {
         return
       }
