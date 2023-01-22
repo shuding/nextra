29d9f73706ebc5a541cf3f92d3369d360a142ec9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-01-22 04:13:26 +0100

    add zod validation for _meta.json "theme" property

--- packages/nextra-theme-docs/src/types.ts
+++ packages/nextra-theme-docs/src/types.ts
@@ -3,19 +3,6 @@ import { ReactNode } from 'react'
 import { PageOpts } from 'nextra'
 import { DocsThemeConfig } from './constants'
 
-export type PageTheme = {
-  breadcrumb: boolean
-  collapsed: boolean
-  footer: boolean
-  layout: 'default' | 'full' | 'raw'
-  navbar: boolean
-  pagination: boolean
-  sidebar: boolean
-  timestamp: boolean
-  toc: boolean
-  typesetting: 'default' | 'article'
-}
-
 export type Context = {
   pageOpts: PageOpts
   themeConfig: DocsThemeConfig
