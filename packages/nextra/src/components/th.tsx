b3219c3d741840824b03c75397eb0cd21cb8cee3
Christian Ivicevic <mail@christian-ivicevic.com>
2023-01-28 13:46:12 +0100

    fix custom class names fully overriding default component styles (#1408)
    * fix custom class names fully overriding default component styles
    
    * Update .changeset/spotty-poets-count.md
    
    ---------
    
    Co-authored-by: Dimitri POSTOLOV <en3m@ya.ru>

--- packages/nextra/src/components/th.tsx
+++ packages/nextra/src/components/th.tsx
@@ -1,8 +1,11 @@
 import type { ComponentProps } from 'react'
 
-export const Th = (props: ComponentProps<'th'>) => (
+export const Th = ({ className = '', ...props }: ComponentProps<'th'>) => (
   <th
-    className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600"
+    className={
+      'nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600 ' +
+      className
+    }
     {...props}
   />
 )
