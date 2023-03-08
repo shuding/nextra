07eb53b8f931ba99b60549eebd6b959e7df2ded7
ocavue <ocavue@gmail.com>
2023-03-08 16:27:42 +0800

    Fix types:check

--- packages/nextra-theme-docs/tsup.config.ts
+++ packages/nextra-theme-docs/tsup.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
   dts: true,
   external: ['nextra'],
   outExtension: () => ({ js: '.js' }),
-  target: tsconfig.compilerOptions.target
+  target: tsconfig.compilerOptions.target as 'es2016'
 })
