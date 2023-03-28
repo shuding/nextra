0d5d956e3a5df58b551fe960ae7c56c4a4f3ae55
Dimitri POSTOLOV <en3m@ya.ru>
2023-03-29 01:59:51 +0200

    Update loader.ts

--- packages/nextra/src/loader.ts
+++ packages/nextra/src/loader.ts
@@ -57,8 +57,10 @@ const initGitRepo = (async () => {
       // repository.path() returns the `/path/to/repo/.git`, we need the parent directory of it
       const gitRoot = path.join(repository.path(), '..')
       return { repository, gitRoot }
-    } catch (e: any) {
-      console.warn(`[nextra] Init git repository failed ${e.message || e}`)
+    } catch (error) {
+      console.warn(
+        `[nextra] Init git repository failed ${(error as Error).message}`
+      )
     }
   }
   return {}
