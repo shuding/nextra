f41d8d6d7f8da4e92ab2028d1f92bf99692f0b47
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-04-13 13:03:10 +0200

    fix

--- packages/nextra-theme-docs/src/components/locale-switch.tsx
+++ packages/nextra-theme-docs/src/components/locale-switch.tsx
@@ -32,10 +32,10 @@ export function LocaleSwitch({
       selected={{
         key: selected?.locale || '',
         name: (
-          <div className="nx-flex nx-items-center nx-gap-2">
+          <span className="nx-flex nx-items-center nx-gap-2">
             <GlobeIcon />
             <span className={lite ? 'nx-hidden' : ''}>{selected?.text}</span>
-          </div>
+          </span>
         )
       }}
       options={options.map(l => ({
