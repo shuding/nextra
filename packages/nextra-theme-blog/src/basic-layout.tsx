b987cd59ac1b2899d4fb3fa0863ece830eda6673
Tristan Dubbeld <tristan.dubbeld@gmail.com>
2023-05-11 16:59:45 +0200

    Simplify classes on basic layout

--- packages/nextra-theme-blog/src/basic-layout.tsx
+++ packages/nextra-theme-blog/src/basic-layout.tsx
@@ -9,10 +9,7 @@ export const BasicLayout = ({ children }: { children: ReactNode }) => {
   const title = `${opts.title}${config.titleSuffix || ''}`
   const ref = useRef<HTMLHeadingElement>(null)
   return (
-    <article
-      className="nx-container nx-prose nx-prose-sm dark:nx-prose-dark md:nx-prose"
-      dir="ltr"
-    >
+    <article className="nx-container nx-prose dark:nx-prose-dark" dir="ltr">
       <Head>
         <title>{title}</title>
         {config.head?.({ title, meta: opts.frontMatter })}
