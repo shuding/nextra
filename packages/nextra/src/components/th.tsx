dd3165036a46f7d92a0696791d9c048c08b4baee
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-07-05 07:21:10 +0200

    tweaks

--- packages/nextra/src/components/th.tsx
+++ packages/nextra/src/components/th.tsx
@@ -1,11 +1,12 @@
+import cn from 'clsx'
 import type { ComponentProps } from 'react'
 
 export const Th = ({ className = '', ...props }: ComponentProps<'th'>) => (
   <th
-    className={
-      'nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600 ' +
+    className={cn(
+      'nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600',
       className
-    }
+    )}
     {...props}
   />
 )
