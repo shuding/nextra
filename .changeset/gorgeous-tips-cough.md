cf4d1ab58d36917997abd42c971836b87947da0d
L Lllvvuu <git@llllvvuu.dev>
2023-07-25 04:57:07 -0700

    fix: apply h2 styles to footnote heading, fix backlink jump

---
+++ .changeset/gorgeous-tips-cough.md
@@ -0,0 +1,7 @@
+---
+"nextra-theme-docs": patch
+"nextra": patch
+"docs": patch
+---
+
+fix: apply <h2/> styles to footnote heading, fix backlink jump
--- packages/nextra-theme-docs/src/mdx-components.tsx
+++ packages/nextra-theme-docs/src/mdx-components.tsx
@@ -48,7 +48,7 @@ function HeadingLink({
       className={cn(
         'nx-font-semibold nx-tracking-tight nx-text-slate-900 dark:nx-text-slate-100',
         {
-          h2: 'nx-mt-10 nx-border-b nx-pb-1 nx-text-3xl nx-border-neutral-200/70 contrast-more:nx-border-neutral-400 dark:nx-border-primary-100/10 contrast-more:dark:nx-border-neutral-400',
+          h2: 'mdx-h2',
           h3: 'nx-mt-8 nx-text-2xl',
           h4: 'nx-mt-8 nx-text-xl',
           h5: 'nx-mt-8 nx-text-lg',
