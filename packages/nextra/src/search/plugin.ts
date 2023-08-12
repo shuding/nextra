dd0414d0d37354d40a88c15581e65e86c336e4af
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-08-12 13:45:12 +0200

    try

--- packages/nextra/src/search/plugin.ts
+++ packages/nextra/src/search/plugin.ts
@@ -3,6 +3,7 @@ import type { Compiler } from 'webpack'
 
 const PLUGIN_NAME = 'NextraSearchPlugin'
 const isDev = process.env.NODE_ENV !== 'production'
+
 export class NextraSearchPlugin {
   apply(compiler: Compiler) {
     compiler.hooks.make.tap(PLUGIN_NAME, compilation => {
@@ -17,10 +18,10 @@ export class NextraSearchPlugin {
           for (const [, entry] of compilation.entries.entries()) {
             const entryDependency = entry.dependencies?.[0]
 
-            // There're some Next.js refactors that might cause the MDX module
+            // There are some Next.js refactors that might cause the MDX module
             // to be a dependency of the entry module, instead of the entry
             // itself. This is a workaround to find the MDX module loaded by
-            // Nextra.
+            // Nextra
             let entryModule =
               compilation.moduleGraph.getResolvedModule(entryDependency)
             if (!entryModule?.buildInfo?.nextraSearch) {
@@ -32,11 +33,9 @@ export class NextraSearchPlugin {
                 }
               }
             }
-
-            if (entryModule?.buildInfo?.nextraSearch) {
-              const { title, data, indexKey, route } =
-                entryModule.buildInfo.nextraSearch
-
+            const nextraSearch = entryModule?.buildInfo?.nextraSearch
+            if (nextraSearch) {
+              const { title, data, indexKey, route } = nextraSearch
               const indexFilename = `nextra-data-${indexKey}.json`
               if (indexFiles[indexFilename] === undefined) {
                 indexFiles[indexFilename] = '{'
@@ -44,18 +43,16 @@ export class NextraSearchPlugin {
               if (indexFiles[indexFilename] !== '{') {
                 indexFiles[indexFilename] += ','
               }
-              indexFiles[indexFilename] += `${JSON.stringify(
-                route
-              )}:{"title":${JSON.stringify(title)},"data":${JSON.stringify(
+              indexFiles[indexFilename] += `"${route}":${JSON.stringify({
+                title,
                 data
-              )}}`
+              })}`
             }
           }
 
           for (const [file, content] of Object.entries(indexFiles)) {
-            assets[
-              (isDev ? '../static/chunks/' : '../../static/chunks/') + file
-            ] = new sources.RawSource(content + '}')
+            assets[`${isDev ? '' : '../'}../static/chunks/${file}`] =
+              new sources.RawSource(content + '}')
           }
         }
       )
