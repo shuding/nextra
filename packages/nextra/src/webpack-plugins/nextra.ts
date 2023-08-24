70663d1f9d95bb597bb46ed04dd38e9e42dfaf2a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-08-24 22:49:16 +0200

    more

--- packages/nextra/src/webpack-plugins/nextra.ts
+++ packages/nextra/src/webpack-plugins/nextra.ts
@@ -8,18 +8,16 @@ export class NextraPlugin {
   constructor(private config: NextraConfig & { locales: string[] }) {}
 
   apply(compiler: Compiler) {
-    compiler.hooks.beforeCompile.tapAsync(
-      'NextraPlugin',
-      async (_, callback) => {
-        const { locales } = this.config
-        try {
-          const result = await collectFiles({ dir: PAGES_DIR, locales })
-          pageMapCache.set(result)
-          callback()
-        } catch (err) {
-          callback(err as Error)
-        }
+    const pluginName = this.constructor.name
+    compiler.hooks.beforeCompile.tapAsync(pluginName, async (_, callback) => {
+      const { locales } = this.config
+      try {
+        const result = await collectFiles({ dir: PAGES_DIR, locales })
+        pageMapCache.set(result)
+        callback()
+      } catch (err) {
+        callback(err as Error)
       }
-    )
+    })
   }
 }
