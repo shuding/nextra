a4db01c6b95b0eb0202689611b7e01f1799f62e3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-06 03:29:15 +0200

    prettier

--- packages/nextra-theme-docs/src/components/theme-switch.tsx
+++ packages/nextra-theme-docs/src/components/theme-switch.tsx
@@ -4,8 +4,8 @@ import { MoonIcon, SunIcon } from 'nextra/icons'
 import type { ReactElement } from 'react'
 import type { z } from 'zod'
 import { useConfig } from '../contexts'
-import { Select } from './select'
 import type { themeOptionsSchema } from '../schemas'
+import { Select } from './select'
 
 type ThemeSwitchProps = {
   lite?: boolean
