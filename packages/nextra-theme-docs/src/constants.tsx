5602f43f8b08470935402484bf305d83daeafe09
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-07 22:16:23 +0200

    add changeset

--- packages/nextra-theme-docs/src/constants.tsx
+++ packages/nextra-theme-docs/src/constants.tsx
@@ -97,18 +97,7 @@ export const DEFAULT_THEME: DocsThemeConfig = {
       </>
     )
   },
-  head: (
-    <>
-      <meta name="msapplication-TileColor" content="#fff" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next docs builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@shuding_" />
-      <meta property="og:title" content="Nextra: the next docs builder" />
-      <meta property="og:description" content="Nextra: the next docs builder" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-    </>
-  ),
+  head: null,
   i18n: [],
   logo: (
     <>
