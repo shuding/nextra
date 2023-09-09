4bc8dd6ba85953e2afb7f1831a3ad57f04cbfed8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-09 14:44:16 +0200

    improve next webpack plugin

--- packages/nextra/src/webpack-plugins/nextra.ts
+++ packages/nextra/src/webpack-plugins/nextra.ts
@@ -1,11 +1,14 @@
-import { join } from 'node:path'
+import { join, relative } from 'node:path'
 import pkg from 'graceful-fs'
 import type { Compiler } from 'webpack'
-import { CHUNKS_DIR } from '../constants'
+import { CHUNKS_DIR, CWD } from '../constants'
 import { PAGES_DIR } from '../file-system'
 import { collectPageMap } from '../server/page-map'
+import { logger } from '../server/utils'
 
 const fs = pkg.promises
+
+// let isSaved = false
 
 export class NextraPlugin {
   constructor(private config: { locales: string[] }) {}
@@ -14,20 +17,32 @@ export class NextraPlugin {
     const pluginName = this.constructor.name
     const { locales } = this.config
 
+    // TODO: Improve this, this a temporal workaround to make things works
+    // Saving with fs is not ideal, we should attach to webpack's assets objects
     compiler.hooks.beforeCompile.tapAsync(pluginName, async (_, callback) => {
+      // if (isSaved) {
+      //   console.warn('PageMap is already saved, skipping…')
+      //   callback()
+      // }
       try {
         const label = 'Done in'
         console.time(label)
         await fs.mkdir(join(CHUNKS_DIR), { recursive: true })
+
         for (const locale of locales) {
           const route = `/${locale}`
           const dir = PAGES_DIR + route
+
           const rawJs = await collectPageMap({ dir, route })
+
           const pageMapPath = join(CHUNKS_DIR, `nextra-page-map-${locale}.mjs`)
           await fs.writeFile(pageMapPath, rawJs)
-          console.log(`✅ ${pageMapPath} saved`)
+
+          logger.info(`PageMap "${relative(CWD, pageMapPath)}" saved`)
         }
+
         console.timeEnd(label)
+        // isSaved = true
         callback()
       } catch (error) {
         console.error(error)
