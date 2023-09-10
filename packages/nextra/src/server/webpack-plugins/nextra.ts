c64c886bd05135b4e260bb92f46e5d48747e775c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-10 21:26:41 +0200

    comment code

--- packages/nextra/src/server/webpack-plugins/nextra.ts
+++ packages/nextra/src/server/webpack-plugins/nextra.ts
@@ -1,6 +1,6 @@
 import path from 'node:path'
 import gracefulFs from 'graceful-fs'
-import pkg from 'next/dist/compiled/webpack/webpack.js'
+// import pkg from 'next/dist/compiled/webpack/webpack.js'
 import type { Compiler } from 'webpack'
 import { IS_PRODUCTION } from '../../constants'
 import { CHUNKS_DIR } from '../constants'
@@ -8,7 +8,7 @@ import { PAGES_DIR } from '../file-system'
 import { collectPageMap } from '../page-map'
 import { logger } from '../utils'
 
-const { webpack, sources } = pkg
+// const { webpack, sources } = pkg
 const fs = gracefulFs.promises
 
 let isSaved = false
@@ -21,17 +21,24 @@ export class NextraPlugin {
     const locales = new Set(this.config.locales)
 
     compiler.hooks.beforeCompile.tapAsync(pluginName, async (_, callback) => {
-      if (isSaved || !IS_PRODUCTION) {
-        // Never call hook 2 times
-        // Also on `production` environment we get error:
-        // Module not found: Can't resolve '.../.next/static/chunks/nextra-page-map-en.mjs'
-        // while using only `processAssets` hook, but without `beforeCompile`
+      // if (isSaved || !IS_PRODUCTION) {
+      //   // Never call hook 2 times
+      //   // Also on `production` environment we get error:
+      //   // Module not found: Can't resolve '.../.next/static/chunks/nextra-page-map-en.mjs'
+      //   // while using only `processAssets` hook, but without `beforeCompile`
+      //   callback()
+      //   return
+      // }
+      if (IS_PRODUCTION && !isSaved) {
         callback()
         return
       }
 
-      // Create chunks directory since it doesn't exist yet
-      await fs.mkdir(path.join(CHUNKS_DIR), { recursive: true })
+      if (!isSaved) {
+        // Create chunks directory since it doesn't exist yet
+        await fs.mkdir(path.join(CHUNKS_DIR), { recursive: true })
+      }
+
       try {
         for (const locale of locales) {
           const route = `/${locale}`
@@ -51,38 +58,38 @@ export class NextraPlugin {
       }
     })
 
-    if (IS_PRODUCTION) {
-      // Do not fire `processAssets` on production
-      return
-    }
+    // if (IS_PRODUCTION) {
+    //   // Do not fire `processAssets` on production
+    //   return
+    // }
 
-    compiler.hooks.compilation.tap(pluginName, compilation => {
-      compilation.hooks.processAssets.tapAsync(
-        {
-          name: pluginName,
-          stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL
-        },
-        async (assets, callback) => {
-          try {
-            // TODO: Find a way to get filename only for current asset? and get PageMap only for it
-            for (const locale of locales) {
-              const route = `/${locale}`
-              const dir = PAGES_DIR + route
-              const rawJs = await collectPageMap({ dir, route })
-
-              const assetPath =
-                (IS_PRODUCTION ? '../' : '') +
-                `../static/chunks/nextra-page-map-${locale}.mjs`
-
-              assets[assetPath] = new sources.RawSource(rawJs)
-            }
-            logger.info('`processAssets`')
-            callback()
-          } catch (error) {
-            callback(error as Error)
-          }
-        }
-      )
-    })
+    // compiler.hooks.compilation.tap(pluginName, compilation => {
+    //   compilation.hooks.processAssets.tapAsync(
+    //     {
+    //       name: pluginName,
+    //       stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL
+    //     },
+    //     async (assets, callback) => {
+    //       try {
+    //         // TODO: Find a way to get filename only for current asset? and get PageMap only for it
+    //         for (const locale of locales) {
+    //           const route = `/${locale}`
+    //           const dir = PAGES_DIR + route
+    //           const rawJs = await collectPageMap({ dir, route })
+    //
+    //           const assetPath =
+    //             (IS_PRODUCTION ? '../' : '') +
+    //             `../static/chunks/nextra-page-map-${locale}.mjs`
+    //
+    //           assets[assetPath] = new sources.RawSource(rawJs)
+    //         }
+    //         logger.info('`processAssets`')
+    //         callback()
+    //       } catch (error) {
+    //         callback(error as Error)
+    //       }
+    //     }
+    //   )
+    // })
   }
 }
