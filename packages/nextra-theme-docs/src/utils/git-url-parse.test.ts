b30b8ef5281a10f193a65acc5cf3ad59ae2eb836
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-11 14:48:39 +0200

    update test

--- packages/nextra-theme-docs/src/utils/git-url-parse.test.ts
+++ packages/nextra-theme-docs/src/utils/git-url-parse.test.ts
@@ -10,7 +10,7 @@ describe('gitUrlParse', () => {
       {
         "href": "https://github.com/B2o5T/graphql-eslint/tree/master/website",
         "name": "graphql-eslint",
-        "origin": "https://github.com", 
+        "origin": "https://github.com",
         "owner": "B2o5T",
       }
     `
