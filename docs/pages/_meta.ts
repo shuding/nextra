e24cb11a58da1bd07ec1fd1dbdd53f008e44b0b2
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-18 03:21:42 +0200

    fixx

---
+++ docs/pages/_meta.ts
@@ -0,0 +1,36 @@
+export default {
+  index: {
+    type: 'page',
+    title: 'Nextra',
+    display: 'hidden',
+    theme: {
+      layout: 'raw'
+    }
+  },
+  docs: {
+    type: 'page',
+    title: 'Documentation'
+  },
+  showcase: {
+    type: 'page',
+    title: 'Showcase',
+    theme: {
+      typesetting: 'article',
+      layout: 'full'
+    }
+  },
+  about: {
+    type: 'page',
+    title: 'About',
+    theme: {
+      typesetting: 'article'
+    }
+  },
+  '404': {
+    type: 'page',
+    theme: {
+      timestamp: false,
+      typesetting: 'article'
+    }
+  }
+}
