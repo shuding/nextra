59fa7bd8f7c694533489c3f284fb5ccc0b990b43
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-19 08:44:53 +0200

    fix

---
+++ packages/nextra/src/icon.d.ts
@@ -0,0 +1,5 @@
+import type { SVGProps } from 'react'
+
+declare const ReactComponent: (props: SVGProps<SVGSVGElement>) => JSX.Element
+
+export { ReactComponent }
--- packages/nextra/tsup.config.ts
+++ packages/nextra/tsup.config.ts
@@ -31,7 +31,6 @@ export default defineConfig([
       'client/icons/index': 'src/client/icons/index.ts'
     },
     esbuildPlugins: [
-      // @ts-expect-error idk what's wrong with types here
       svgr({
         exportType: 'named',
         typescript: true
@@ -40,29 +39,6 @@ export default defineConfig([
     format: 'esm',
     target: 'es2020',
     treeshake: true,
-    // todo: find better way to export svg types
-    async onSuccess() {
-      const filePath = path.join(CWD, 'dist', 'client', 'icons', 'index.js')
-      const content = await fs.readFile(filePath, 'utf-8')
-
-      const components = content
-        .match(/(?<=export \{ )[\w, ]+/)![0]
-        .replaceAll(/\w+ as /g, '')
-        .trimEnd()
-        .split(', ')
-        .filter(name => !['ArrowRightIcon', 'ExpandIcon'].includes(name))
-
-      await fs.writeFile(
-        filePath.replace('.js', '.d.ts'),
-        `import type { ComponentProps, ReactElement } from 'react'
-
-type Svg = (props: ComponentProps<'svg'>) => ReactElement
-
-export const ArrowRightIcon: (props: ComponentProps<'svg'> & { pathClassName?: string }): ReactElement
-
-export const ExpandIcon: (props: ComponentProps<'svg'> & { isOpen?: boolean }): ReactElement
-${components.map(component => `export const ${component}: Svg`).join('\n')}`
-      )
-    }
+    dts: true
   }
 ])
