68ebe31aa81423c658bf1fa60fb73d3d8f4c2a7a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-24 08:52:14 +0200

    aa

--- packages/nextra/src/client/components/button.tsx
+++ packages/nextra/src/client/components/button.tsx
@@ -1,17 +1,17 @@
 import cn from 'clsx'
 import type { ComponentProps, ReactElement } from 'react'
 
-export const Button = ({
+export function Button({
   children,
   className,
   ...props
-}: ComponentProps<'button'>): ReactElement => {
+}: ComponentProps<'button'>): ReactElement {
   return (
     <button
       className={cn(
-        'nextra-button _transition-all active:_opacity-50',
-        '_bg-primary-700/5 _border _border-black/5 _text-gray-600 hover:_text-gray-900 _rounded-md _p-1.5',
-        'dark:_bg-primary-300/10 dark:_border-white/10 dark:_text-gray-400 dark:hover:_text-gray-50',
+        'nextra-button _transition active:_opacity-50',
+        '_border _border-gray-300 hover:_text-gray-900 _rounded-md _p-1.5',
+        'dark:_border-neutral-700 dark:hover:_text-gray-50',
         className
       )}
       {...props}
--- packages/nextra/src/client/components/pre.tsx
+++ packages/nextra/src/client/components/pre.tsx
@@ -1,7 +1,12 @@
 import cn from 'clsx'
 import type { ComponentProps, ReactElement } from 'react'
 import { useCallback, useRef } from 'react'
-import { WordWrapIcon } from '../icons/index.js'
+import {
+  JavaScriptIcon,
+  MarkdownIcon,
+  TypeScriptIcon,
+  WordWrapIcon
+} from '../icons/index.js'
 import { Button } from './button.js'
 import { CopyToClipboard } from './copy-to-clipboard.js'
 
@@ -10,7 +15,7 @@ export const Pre = ({
   className,
   'data-filename': filename,
   'data-copy': copy,
-  'data-language': _language,
+  'data-language': language,
   ...props
 }: ComponentProps<'pre'> & {
   'data-filename'?: string
@@ -29,53 +34,66 @@ export const Pre = ({
     }
   }, [])
 
+  const hasCopyCode = copy === ''
+
+  const copyButton = hasCopyCode && (
+    <CopyToClipboard
+      className={filename ? '_ml-auto' : ''}
+      getValue={() => preRef.current?.querySelector('code')?.textContent || ''}
+    />
+  )
+
+  const IconToUse = {
+    js: JavaScriptIcon,
+    ts: TypeScriptIcon,
+    md: MarkdownIcon
+  }[language!]
+
   return (
     <div className="nextra-code _relative _mt-6 first:_mt-0">
       {filename && (
         <div
           className={cn(
-            '_absolute _top-0 _z-[1] _w-full _truncate _rounded-t-xl _bg-primary-700/5 _py-2 _px-4 _text-xs _text-gray-700 dark:_bg-primary-300/10 dark:_text-gray-200',
-            '_border-b _border-primary-700/20 dark:_border-primary-100/20'
+            '_absolute _z-[1] _w-full _truncate _rounded-t-md _pl-3 _pr-4 _text-xs _text-gray-700 dark:_text-gray-200',
+            '_bg-gray-100 _border _border-gray-300 dark:_border-neutral-700 dark:_bg-neutral-900',
+            '_flex _items-center _h-12 _gap-2'
           )}
         >
+          {IconToUse && <IconToUse className="_h-5 _w-5" />}
           {filename}
+          {copyButton}
         </div>
       )}
       <pre
         className={cn(
-          '_ring-1 _ring-inset _ring-primary-700/20 dark:_ring-primary-100/20',
-          '_bg-primary-700/5 _mb-4 _overflow-x-auto _rounded-xl _subpixel-antialiased dark:_bg-primary-300/10 _text-[.9em]',
+          '_border _border-gray-300 dark:_border-neutral-700',
+          '_mb-4 _overflow-x-auto _rounded-md _subpixel-antialiased _text-[.9em]',
           'contrast-more:_border contrast-more:_border-primary-900/20 contrast-more:_contrast-150 contrast-more:dark:_border-primary-100/40',
-          filename ? '_pt-12 _pb-4' : '_py-4',
+          filename ? '_pt-16 _pb-4' : '_py-4',
+          'dark:_bg-black _relative',
           className
         )}
         ref={preRef}
         {...props}
       >
         {children}
-      </pre>
-      <div
-        className={cn(
-          '_opacity-0 _transition [div:hover>&]:_opacity-100 focus-within:_opacity-100',
-          '_flex _gap-1 _absolute _m-[11px] _right-0',
-          filename ? '_top-8' : '_top-0'
-        )}
-      >
-        <Button
-          onClick={toggleWordWrap}
-          className="md:_hidden"
-          title="Toggle word wrap"
+        <div
+          className={cn(
+            '_opacity-0 _transition [div:hover>pre>&]:_opacity-100 focus-within:_opacity-100',
+            '_flex _gap-1 _absolute _right-4',
+            filename ? '_top-14' : '_top-2'
+          )}
         >
-          <WordWrapIcon className="_pointer-events-none _h-4 _w-4" />
-        </Button>
-        {copy === '' && (
-          <CopyToClipboard
-            getValue={() =>
-              preRef.current?.querySelector('code')?.textContent || ''
-            }
-          />
-        )}
-      </div>
+          <Button
+            onClick={toggleWordWrap}
+            className="md:_hidden"
+            title="Toggle word wrap"
+          >
+            <WordWrapIcon className="_pointer-events-none _h-4 _w-4" />
+          </Button>
+          {!filename && copyButton}
+        </div>
+      </pre>
     </div>
   )
 }
