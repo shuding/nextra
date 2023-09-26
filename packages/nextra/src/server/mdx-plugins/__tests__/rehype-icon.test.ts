9de8c540b41a3ddd5477456b1df97ac38212fff4
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-26 02:32:16 +0200

    aa

--- packages/nextra/src/server/mdx-plugins/__tests__/rehype-icon.test.ts
+++ packages/nextra/src/server/mdx-plugins/__tests__/rehype-icon.test.ts
@@ -1,9 +1,8 @@
-import { compile } from '@mdx-js/mdx';
-import type { VFile } from '@mdx-js/mdx/lib/compile';
-import rehypePrettyCode from 'rehype-pretty-code';
-import { DEFAULT_REHYPE_PRETTY_CODE_OPTIONS } from '../../compile.js';
-import { parseMeta, rehypeIcon, attachMeta } from '../index.js';
-
+import { compile } from '@mdx-js/mdx'
+import type { VFile } from '@mdx-js/mdx/lib/compile'
+import rehypePrettyCode from 'rehype-pretty-code'
+import { DEFAULT_REHYPE_PRETTY_CODE_OPTIONS } from '../../compile.js'
+import { attachMeta, parseMeta, rehypeIcon } from '../index.js'
 
 function process(content: string): Promise<VFile> {
   return compile(content, {
@@ -17,12 +16,17 @@ function process(content: string): Promise<VFile> {
   })
 }
 
+function clean(content: string) {
+  return content
+    .slice(content.indexOf('\n'), content.lastIndexOf('function MDXContent'))
+    .trim()
+}
+
 describe('rehypeIcon', () => {
   it('should work', async () => {
     const { value } = await process(['```md', '', '```'].join('\n'))
-    expect(value).toMatchInlineSnapshot(`
-      "/*@jsxRuntime automatic @jsxImportSource react*/
-      import {MarkdownIcon} from \\"nextra/icons\\";
+    expect(clean(value)).toMatchInlineSnapshot(`
+      "import {MarkdownIcon} from \\"nextra/icons\\";
       function _createMdxContent(props) {
         const _components = Object.assign({
           pre: \\"pre\\",
@@ -30,13 +34,7 @@ describe('rehypeIcon', () => {
           span: \\"span\\"
         }, props.components);
         return <_components.pre icon={MarkdownIcon}><_components.code data-language=\\"md\\" data-theme=\\"default\\"><_components.span>{\\" \\"}</_components.span></_components.code></_components.pre>;
-      }
-      function MDXContent(props = {}) {
-        const {wrapper: MDXLayout} = props.components || ({});
-        return MDXLayout ? <MDXLayout {...props}><_createMdxContent {...props} /></MDXLayout> : _createMdxContent(props);
-      }
-      export default MDXContent;
-      "
+      }"
     `)
   })
 })
