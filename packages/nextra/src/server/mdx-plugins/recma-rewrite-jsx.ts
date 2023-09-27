afecbf6d2c8f7ccf54bc32b326f68160d8c5b5a9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-27 07:44:31 +0200

    refactor

--- packages/nextra/src/server/mdx-plugins/recma-rewrite-jsx.ts
+++ packages/nextra/src/server/mdx-plugins/recma-rewrite-jsx.ts
@@ -1,31 +1,42 @@
-import type { ObjectExpression, SpreadElement } from 'estree'
+import type {
+  FunctionDeclaration,
+  ObjectExpression,
+  Program,
+  ReturnStatement,
+  SpreadElement
+} from 'estree'
 import type { JsxAttribute } from 'estree-util-to-js/lib/jsx'
 import type { Plugin } from 'unified'
 
 const HEADING_NAMES = new Set(['h2', 'h3', 'h4', 'h5', 'h6'])
 
-export const recmaRewriteJsx: Plugin<[], any> = () => ast => {
+export const recmaRewriteJsx: Plugin<[], Program> = () => ast => {
   const createMdxContent = ast.body.find(
+    // @ts-expect-error
     o => o.type === 'FunctionDeclaration' && o.id.name === '_createMdxContent'
-  )
+  ) as FunctionDeclaration
   const returnStatementIndex = createMdxContent.body.body.findIndex(
     o => o.type === 'ReturnStatement'
   )
 
-  const returnStatement = createMdxContent.body.body[returnStatementIndex]
+  const returnStatement = createMdxContent.body.body[
+    returnStatementIndex
+  ] as ReturnStatement
 
-  function isHeading(o: any): boolean {
+  // @ts-expect-error
+  function isHeading(o): boolean {
     const name = o.openingElement?.name.property?.name
     return name && HEADING_NAMES.has(name)
   }
 
+  // @ts-expect-error
   const headings = returnStatement.argument.children.filter(isHeading)
   const toc = ast.body.find(
-    // @ts-expect-error
     node =>
       node.type === 'ExportNamedDeclaration' &&
+      // @ts-expect-error
       node.declaration.declarations[0].id.name === 'toc'
-  )
+  ) as any
 
   const tocProperties = toc.declaration.declarations[0].init.elements
 
