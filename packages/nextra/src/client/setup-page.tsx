25527ed3b75396e2ebff0e76f62370838540e553
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-28 15:45:29 +0200

    temp

--- packages/nextra/src/client/setup-page.tsx
+++ packages/nextra/src/client/setup-page.tsx
@@ -3,7 +3,8 @@
  * This file should be never used directly, only in loader.ts
  */
 
-import type { FC } from 'react'
+import { useRouter } from 'next/router'
+import type { FC, ReactElement } from 'react'
 import { NEXTRA_INTERNAL } from '../constants.js'
 import { normalizePageRoute, pageTitleFromFilename } from '../server/utils.js'
 import type {
@@ -18,6 +19,7 @@ import type {
   PageOpts
 } from '../types'
 import { findFolder } from './utils.js'
+import { DataProvider } from './data.js'
 
 function isFolder(value: DynamicMetaItem): value is DynamicFolder {
   return !!value && typeof value === 'object' && value.type === 'folder'
@@ -130,5 +132,53 @@ export function setupNextraPage(
     Content: MDXContent,
     pageOpts
   }
-  return MDXContent
+  return NextraLayout
+}
+
+function NextraLayout({
+  __nextra_pageMap = [],
+  __nextra_dynamic_opts,
+  ...props
+}: any): ReactElement {
+  const __nextra_internal__ = (globalThis as NextraInternalGlobal)[
+    NEXTRA_INTERNAL
+  ]
+  const { Layout, themeConfig } = __nextra_internal__
+  const { route } = useRouter()
+  console.log({ route })
+
+  const pageContext = __nextra_internal__.context[route]
+
+  if (!pageContext) {
+    throw new Error(
+      `No content found for the "${route}" route. Please report it as a bug.`
+    )
+  }
+
+  let { pageOpts } = pageContext
+
+  for (const { route, children } of __nextra_pageMap) {
+    // TODO 2 for locale, 1 without local
+    const paths = route.split('/').slice(2)
+    const folder = findFolder(pageOpts.pageMap, paths)
+    folder.children = children
+  }
+
+  if (__nextra_dynamic_opts) {
+    const { toc, title, frontMatter } = __nextra_dynamic_opts
+    pageOpts = {
+      ...pageOpts,
+      toc,
+      title,
+      frontMatter
+    }
+  }
+
+  return (
+    <Layout themeConfig={themeConfig} pageOpts={pageOpts} pageProps={props}>
+      <DataProvider value={props}>
+        <pageContext.Content />
+      </DataProvider>
+    </Layout>
+  )
 }
