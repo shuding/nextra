d5b0e6e6fb3de293476e8ddd9b8732cda0259925
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-28 07:14:05 +0200

    save headings

--- packages/nextra/src/server/mdx-plugins/rehype-extract-toc-content.ts
+++ packages/nextra/src/server/mdx-plugins/rehype-extract-toc-content.ts
@@ -2,11 +2,13 @@ import type { Plugin } from 'unified'
 import { visit } from 'unist-util-visit'
 
 export const rehypeExtractTocContent: Plugin<[], any> = () => (ast, file) => {
-  // @ts-expect-error
-  file.toc = {}
+  const toc: Record<string, any[]> = {}
+
   visit(ast, 'heading', node => {
     if (node.depth === 1) return
-    file.toc[node.data.hProperties.id] = node.children
+    toc[node.data.hProperties.id] = node.children
     node.children = []
   })
+
+  file.data.toc = toc
 }
