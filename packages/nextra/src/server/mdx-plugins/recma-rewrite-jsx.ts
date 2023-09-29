bbb48281090f4aa2a4213e8dadeab3fe4614e7d0
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-29 19:46:06 +0200

    more update deps

--- packages/nextra/src/server/mdx-plugins/recma-rewrite-jsx.ts
+++ packages/nextra/src/server/mdx-plugins/recma-rewrite-jsx.ts
@@ -5,103 +5,105 @@ import { visit } from 'unist-util-visit'
 
 const HEADING_NAMES = new Set(['h2', 'h3', 'h4', 'h5', 'h6'])
 
-export const recmaRewriteJsx: Plugin<[], Program> = () => (ast, file) => {
-  const createMdxContent = ast.body.find(
-    o => o.type === 'FunctionDeclaration' && o.id!.name === '_createMdxContent'
-  ) as FunctionDeclaration
+export const recmaRewriteJsx: Plugin<[], Program> =
+  () => (ast: Program, file) => {
+    const createMdxContent = ast.body.find(
+      o =>
+        o.type === 'FunctionDeclaration' && o.id!.name === '_createMdxContent'
+    ) as FunctionDeclaration
 
-  const mdxContent = ast.body.find(
-    node =>
-      node.type === 'FunctionDeclaration' && node.id!.name === 'MDXContent'
-  ) as FunctionDeclaration
+    const mdxContent = ast.body.find(
+      node =>
+        node.type === 'FunctionDeclaration' && node.id!.name === 'MDXContent'
+    ) as FunctionDeclaration
 
-  if (!mdxContent) {
-    throw new Error('`MDXContent` not found!')
-  }
+    if (!mdxContent) {
+      throw new Error('`MDXContent` not found!')
+    }
 
-  const returnStatement = createMdxContent.body.body.find(
-    o => o.type === 'ReturnStatement'
-  ) as ReturnStatement
+    const returnStatement = createMdxContent.body.body.find(
+      o => o.type === 'ReturnStatement'
+    ) as ReturnStatement
 
-  const { argument } = returnStatement as any
+    const { argument } = returnStatement as any
 
-  // if return statements doesn't wrap in fragment children will be []
-  const returnBody = argument.children.length ? argument.children : [argument]
+    // if return statements doesn't wrap in fragment children will be []
+    const returnBody = argument.children.length ? argument.children : [argument]
 
-  const tocProperties = file.data.toc as (
-    | { properties: { id: string } }
-    | string
-  )[]
+    const tocProperties = file.data.toc as (
+      | { properties: { id: string } }
+      | string
+    )[]
 
-  // Do not add `const toc = useTOC(props)`
-  if (!tocProperties.length) return
+    // Do not add `const toc = useTOC(props)`
+    if (!tocProperties.length) return
 
-  visit(
-    // @ts-expect-error -- fix type error
-    { children: returnBody },
-    'JSXElement',
-    (heading: any, _index, parent) => {
-      const { openingElement } = heading
-      const name = openingElement?.name.property?.name
-      const isHeading = name && HEADING_NAMES.has(name)
-      // @ts-expect-error -- fixes type error
-      const isFootnotes = parent.openingElement?.attributes.some(
-        (attr: JsxAttribute) => attr.name.name === 'data-footnotes'
-      )
-      if (!isHeading || isFootnotes) return
-      const idNode = openingElement.attributes.find(
-        (attr: JsxAttribute) => attr.name.name === 'id'
-      )
-      if (!idNode) return
+    visit(
+      // @ts-expect-error -- fix type error
+      { children: returnBody },
+      'JSXElement',
+      (heading: any, _index, parent) => {
+        const { openingElement } = heading
+        const name = openingElement?.name.property?.name
+        const isHeading = name && HEADING_NAMES.has(name)
+        // @ts-expect-error -- fixes type error
+        const isFootnotes = parent.openingElement?.attributes.some(
+          (attr: JsxAttribute) => attr.name.name === 'data-footnotes'
+        )
+        if (!isHeading || isFootnotes) return
+        const idNode = openingElement.attributes.find(
+          (attr: JsxAttribute) => attr.name.name === 'id'
+        )
+        if (!idNode) return
 
-      const id = idNode.value.value
+        const id = idNode.value.value
 
-      const foundIndex = tocProperties.findIndex(node => {
-        if (typeof node === 'string') return
-        return node.properties.id === id
-      })
+        const foundIndex = tocProperties.findIndex(node => {
+          if (typeof node === 'string') return
+          return node.properties.id === id
+        })
 
-      if (foundIndex === -1) return
-      idNode.value = {
-        type: 'JSXExpressionContainer',
-        expression: {
-          type: 'Identifier',
-          name: `toc[${foundIndex}].id`
-        }
-      }
-
-      delete openingElement.selfClosing
-      heading.children = [
-        {
+        if (foundIndex === -1) return
+        idNode.value = {
           type: 'JSXExpressionContainer',
           expression: {
             type: 'Identifier',
-            name: `toc[${foundIndex}].value`
+            name: `toc[${foundIndex}].id`
+          }
+        }
+
+        delete openingElement.selfClosing
+        heading.children = [
+          {
+            type: 'JSXExpressionContainer',
+            expression: {
+              type: 'Identifier',
+              name: `toc[${foundIndex}].value`
+            }
+          }
+        ]
+        heading.closingElement = {
+          ...openingElement,
+          type: 'JSXClosingElement',
+          attributes: []
+        }
+      }
+    )
+
+    createMdxContent.body.body.unshift({
+      type: 'VariableDeclaration',
+      kind: 'const',
+      declarations: [
+        {
+          type: 'VariableDeclarator',
+          id: { type: 'Identifier', name: 'toc' },
+          init: {
+            type: 'CallExpression',
+            callee: { type: 'Identifier', name: 'useTOC' },
+            arguments: [{ type: 'Identifier', name: 'props' }],
+            optional: false
           }
         }
       ]
-      heading.closingElement = {
-        ...openingElement,
-        type: 'JSXClosingElement',
-        attributes: []
-      }
-    }
-  )
-
-  createMdxContent.body.body.unshift({
-    type: 'VariableDeclaration',
-    kind: 'const',
-    declarations: [
-      {
-        type: 'VariableDeclarator',
-        id: { type: 'Identifier', name: 'toc' },
-        init: {
-          type: 'CallExpression',
-          callee: { type: 'Identifier', name: 'useTOC' },
-          arguments: [{ type: 'Identifier', name: 'props' }],
-          optional: false
-        }
-      }
-    ]
-  })
-}
+    })
+  }
