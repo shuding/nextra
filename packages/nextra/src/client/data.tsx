2ab2228fb99b432fcc8392f046e0e59f47e74174
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-30 07:03:50 +0200

    okk

--- packages/nextra/src/client/data.tsx
+++ packages/nextra/src/client/data.tsx
@@ -1,7 +1,7 @@
-import { MDXRemote } from './remote/index.js'
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useRef } from 'react'
 import type { Components } from './mdx'
-import { useMDXComponents } from './mdx.js'
+import { MDXProvider, useMDXComponents } from './mdx.js'
+import { jsxRuntime } from './remote/jsx-runtime.cjs'
 
 const SSGContext = createContext<Record<string, any>>({})
 
@@ -14,25 +14,45 @@ export const DataProvider = SSGContext.Provider
 export function RemoteContent({
   components: dynamicComponents
 }: {
+  /**
+   * An object mapping names to React components.
+   * The key used will be the name accessible to MDX.
+   *
+   * For example: `{ ComponentName: Component }` will be accessible in the MDX as `<ComponentName/>`.
+   */
   components?: Components
 }) {
   const dynamicContext = useData('__nextra_dynamic_mdx')
+  const resulRef = useRef()
 
   if (!dynamicContext) {
     throw new Error(
       'RemoteContent must be used together with the `buildDynamicMDX` API'
     )
   }
+
+  if (!resulRef.current) {
+    const hydrateFn = Reflect.construct(Function, [dynamicContext])
+    resulRef.current = hydrateFn({ useMDXComponents, ...jsxRuntime })
+  }
+
+  const { default: MDXContent, frontMatter, useTOC } = resulRef.current as any
   const components = useMDXComponents(dynamicComponents)
 
+  console.log(55, { frontMatter, toc: useTOC() })
+
+  // wrapping the content with MDXProvider will allow us to customize the standard
+  // markdown components (such as "h1" or "a") with the "components" object
   return (
-    <MDXRemote
-      compiledSource={dynamicContext}
+    <MDXProvider
       components={{
         ...components,
         // Unset wrapper component and pass `toc` through
+        // @ts-ignore
         wrapper: ({ children }) => children
       }}
-    />
+    >
+      <MDXContent />
+    </MDXProvider>
   )
 }
