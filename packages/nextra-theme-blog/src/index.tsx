676b332dd4a8e0b2eb2bd823230d43c2165156d8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-01 19:20:07 +0200

    more

--- packages/nextra-theme-blog/src/index.tsx
+++ packages/nextra-theme-blog/src/index.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
 import type { NextraThemeLayoutProps } from 'nextra'
 import { MDXProvider } from 'nextra/src/client/mdx'
-import { BasicLayout } from './basic-layout'
+import { useRef } from 'react'
 import { BlogProvider } from './blog-context'
 import { DEFAULT_THEME } from './constants'
-import { components } from './mdx-theme'
+import { components, HeadingContext } from './mdx-theme'
 import Meta from './meta'
 import Nav from './nav'
 import { PostsLayout } from './posts-layout'
@@ -19,49 +20,74 @@ export default function NextraLayout({
 }: NextraThemeLayoutProps) {
   const opts = pageOpts
   const config = { ...DEFAULT_THEME, ...themeConfig }
-  const type = opts.frontMatter.type || 'post'
-  const { date } = opts.frontMatter
-
-  if (!layoutSet.has(type)) {
-    throw new Error(
-      `nextra-theme-blog does not support the layout type "${type}" It only supports "post", "page", "posts" and "tag"`
-    )
-  }
-
-  if (date && !isValidDate(date)) {
-    throw new Error(
-      `Invalid date "${date}". Provide date in "YYYY/M/D", "YYYY/M/D H:m", "YYYY-MM-DD", "[YYYY-MM-DD]T[HH:mm]" or "[YYYY-MM-DD]T[HH:mm:ss.SSS]Z" format.`
-    )
-  }
+  const ref = useRef<HTMLHeadingElement>(null)
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <BlogProvider value={{ config, opts }}>
-        <BasicLayout>
-          {type === 'post' ? <Meta /> : <Nav />}
+        <article
+          className="_container _prose max-md:_prose-sm dark:_prose-dark"
+          dir="ltr"
+        >
           <MDXProvider
             components={{
               ...components,
-              ...config.components
+              ...config.components,
+              wrapper: function NextraWrapper({
+                frontMatter,
+                title: pageTitle,
+                children
+              }) {
+                const title = `${pageTitle}${config.titleSuffix || ''}`
+
+                const type = frontMatter.type || 'post'
+                const { date } = frontMatter
+
+                if (!layoutSet.has(type)) {
+                  throw new Error(
+                    `nextra-theme-blog does not support the layout type "${type}" It only supports "post", "page", "posts" and "tag"`
+                  )
+                }
+
+                if (date && !isValidDate(date)) {
+                  throw new Error(
+                    `Invalid date "${date}". Provide date in "YYYY/M/D", "YYYY/M/D H:m", "YYYY-MM-DD", "[YYYY-MM-DD]T[HH:mm]" or "[YYYY-MM-DD]T[HH:mm:ss.SSS]Z" format.`
+                  )
+                }
+
+                return (
+                  <HeadingContext.Provider value={ref}>
+                    <Head>
+                      <title>{title}</title>
+                      {config.head?.({ title, meta: frontMatter })}
+                    </Head>
+                    {opts.hasJsxInH1 ? <h1 ref={ref} /> : null}
+                    {opts.hasJsxInH1 ? null : <h1>{opts.title}</h1>}
+                    {type === 'post' ? <Meta /> : <Nav />}
+                    {children}
+                    {(() => {
+                      switch (type) {
+                        case 'post':
+                          return (
+                            <>
+                              {config.postFooter}
+                              {config.comments}
+                            </>
+                          )
+                        case 'posts':
+                        case 'tag':
+                          return <PostsLayout />
+                      }
+                    })()}
+                  </HeadingContext.Provider>
+                )
+              }
             }}
           >
             {children}
           </MDXProvider>
-          {(() => {
-            switch (type) {
-              case 'post':
-                return (
-                  <>
-                    {config.postFooter}
-                    {config.comments}
-                  </>
-                )
-              case 'posts':
-              case 'tag':
-                return <PostsLayout />
-            }
-          })()}
-        </BasicLayout>
+          {config.footer}
+        </article>
       </BlogProvider>
     </ThemeProvider>
   )
