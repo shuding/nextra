4087733a66e2dfaf0119e385ec29c7b290c38421
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-03 03:47:46 +0200

    update

--- packages/nextra/src/server/mdx-plugins/__tests__/remark-mdx-title.test.ts
+++ packages/nextra/src/server/mdx-plugins/__tests__/remark-mdx-title.test.ts
@@ -4,13 +4,11 @@ import { clean } from '../../../../__test__/test-utils.js'
 describe('remarkMdxTitle', () => {
   it('should work', async () => {
     const { result } = await compileMdx(
-      `
----
+      `---
 title: From frontMatter
 ---
 
-# Hello
-`,
+# Hello`,
       {
         mdxOptions: {
           outputFormat: 'program',
@@ -19,47 +17,23 @@ title: From frontMatter
       }
     )
     expect(clean(result)).resolves.toMatchInlineSnapshot(`
-      "const { Fragment: _Fragment, jsx: _jsx, jsxs: _jsxs } = arguments[0]
-      const { useMDXComponents: _provideComponents } = arguments[0]
-      const frontMatter = {}
-      function useTOC(props) {
-        return [
-          {
-            value: 'title: From frontMatter',
-            id: 'title-from-frontmatter',
-            depth: 2
-          }
-        ]
+      "import { createElement } from 'react'
+      import { useMDXComponents as _provideComponents } from 'nextra/mdx'
+      export const frontMatter = {
+        title: 'From frontMatter'
+      }
+      export function useTOC(props) {
+        return []
       }
       function _createMdxContent(props) {
         const _components = Object.assign(
           {
-            hr: 'hr',
-            h2: 'h2',
             h1: 'h1'
           },
           _provideComponents(),
           props.components
         )
-        return _jsxs(_Fragment, {
-          children: [
-            _jsx(_components.hr, {}),
-            '\\\\n',
-            _jsx(_components.h2, {
-              id: 'title-from-frontmatter',
-              children: 'title: From frontMatter'
-            }),
-            '\\\\n',
-            _jsx(_components.h1, {
-              children: 'Hello'
-            })
-          ]
-        })
-      }
-      return {
-        frontMatter,
-        useTOC,
-        default: _createMdxContent
+        return <_components.h1>{'Hello'}</_components.h1>
       }
       "
     `)
