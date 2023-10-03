116891c7f48a6acb7831ef3e1f6ec861042405f2
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-03 04:11:07 +0200

    upd

--- packages/nextra/src/server/mdx-plugins/__tests__/remark-mdx-title.test.ts
+++ packages/nextra/src/server/mdx-plugins/__tests__/remark-mdx-title.test.ts
@@ -1,31 +1,49 @@
 import { clean } from '../../../../__test__/test-utils.js'
 import { compileMdx } from '../../compile.js'
 
+const opts = {
+  mdxOptions: {
+    outputFormat: 'program',
+    jsx: true
+  }
+} as const
+
 describe('remarkMdxTitle', () => {
-  it('should work', async () => {
+  it('should take priority of yaml frontmatter', async () => {
     const { result } = await compileMdx(
       `---
-title: From frontMatter
+title: From yaml frontMatter
 ---
 
 # Hello`,
-      {
-        mdxOptions: {
-          outputFormat: 'program',
-          jsx: true
-        }
-      }
+      opts
     )
-    expect(clean(result.replace(/function _createMdxContent.+/s, ''))).resolves
+    expect(clean(result.replace(/export function useTOC.+/s, ''))).resolves
       .toMatchInlineSnapshot(`
       "import { createElement } from 'react'
       import { useMDXComponents as _provideComponents } from 'nextra/mdx'
-      export const title = 'From frontMatter'
+      export const title = 'From yaml frontMatter'
       export const frontMatter = {
-        title: 'From frontMatter'
+        title: 'From yaml frontMatter'
       }
-      export function useTOC(props) {
-        return []
+      "
+    `)
+  })
+
+  it('should take priority of esm frontmatter', async () => {
+    const { result } = await compileMdx(
+      `export const frontMatter = { title: 'From esm frontMatter' }
+
+# Hello`,
+      opts
+    )
+    expect(clean(result.replace(/export function useTOC.+/s, ''))).resolves
+      .toMatchInlineSnapshot(`
+      "import { createElement } from 'react'
+      import { useMDXComponents as _provideComponents } from 'nextra/mdx'
+      export const title = ''
+      export const frontMatter = {
+        title: 'From esm frontMatter'
       }
       "
     `)
