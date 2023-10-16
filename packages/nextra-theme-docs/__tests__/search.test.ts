3a1576de25cb49acf3484ed54e238a7e0ec3c64a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-16 06:32:31 +0200

    update snapshot

--- packages/nextra-theme-docs/__tests__/search.test.ts
+++ packages/nextra-theme-docs/__tests__/search.test.ts
@@ -1,6 +1,8 @@
 describe('search', () => {
   it('should match structurizeData snapshot', async () => {
-    const data = await import('../../../examples/swr-site/.next/static/chunks/nextra-data-en.json')
-    expect(data).toMatchSnapshot()
+    const nextraDataEnglish = await import(
+      '../../../examples/swr-site/.next/static/chunks/nextra-data-en.json'
+    )
+    expect(nextraDataEnglish.default).toMatchSnapshot()
   })
 })
