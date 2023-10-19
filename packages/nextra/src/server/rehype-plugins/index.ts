d9f00494bbcec945b2afc9b219ea58362687e31c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-19 18:26:19 +0200

    refactor

--- packages/nextra/src/server/rehype-plugins/index.ts
+++ packages/nextra/src/server/rehype-plugins/index.ts
@@ -1,4 +1,4 @@
-export { parseMeta, attachMeta } from './rehype.js'
+export { rehypeParseCodeMeta, rehypeAttachCodeMeta } from './rehype.js'
 export { rehypeBetterReactMathjax } from './rehype-better-react-mathjax.js'
 export { rehypeExtractTocContent } from './rehype-extract-toc-content.js'
 export { rehypeIcon } from './rehype-icon.js'
