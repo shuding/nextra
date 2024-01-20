451d44ead4fc1c108b21cb62ecd3c76a42cd4537
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-01-20 22:30:32 +0100

    move index page

---
+++ docs/app/layout.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from 'react'
+import '../style.css'
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
