def7a2f509b5de575e1490e9c962f4753bc73862
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-01-26 21:01:07 +0100

    fix

--- examples/blog/app/layout.jsx
+++ examples/blog/app/layout.jsx
@@ -1,7 +1,7 @@
-import fs from 'fs/promises'
-import path from 'path'
+// import fs from 'fs/promises'
+// import path from 'path'
 import { Layout } from 'nextra-theme-blog'
-import { collectPageMap } from 'nextra/page-map'
+// import { collectPageMap } from 'nextra/page-map'
 
 export const metadata = {
   // title: 'Next.js',
@@ -10,13 +10,13 @@ export const metadata = {
 
 const pageMap = [
   {
-    name: 'page',
-    route: '/page',
+    name: 'index',
+    route: '/',
     frontMatter: {
-      sidebarTitle: 'Page',
+      // sidebarTitle: 'Page',
       type: 'page',
       title: 'About',
-      date: new Date(1577836800000)
+      date: new Date(1577836800000).toISOString()
     }
   },
   {
@@ -26,105 +26,75 @@ const pageMap = [
       {
         name: 'aaron-swartz-a-programmable-web',
         route: '/posts/aaron-swartz-a-programmable-web',
-        children: [
-          {
-            name: 'page',
-            route: '/posts/aaron-swartz-a-programmable-web/page',
-            frontMatter: {
-              sidebarTitle: 'Page',
-              title: 'Notes on A Programmable Web by Aaron Swartz',
-              date: '2016/5/21',
-              description:
-                "At the time when I was getting into web development, I had the chance to read one of the most inspiring book about the web, Aaron Swartz's A Programmable Web. And it completely changed my mind.",
-              tag: 'web development',
-              author: 'Shu'
-            }
-          }
-        ]
+        frontMatter: {
+          // sidebarTitle: 'Page',
+          title: 'Notes on A Programmable Web by Aaron Swartz',
+          date: '2016/5/21',
+          description:
+            "At the time when I was getting into web development, I had the chance to read one of the most inspiring book about the web, Aaron Swartz's A Programmable Web. And it completely changed my mind.",
+          tag: 'web development',
+          author: 'Shu'
+        }
       },
       {
         name: 'callout',
         route: '/posts/callout',
-        children: [
-          {
-            name: 'page',
-            route: '/posts/callout/page',
-            frontMatter: {
-              sidebarTitle: 'Page',
-              title: 'Callout',
-              date: '2023/5/15',
-              description:
-                'En example of using the Callout component in your blog.',
-              tag: 'web development',
-              author: 'Tristan Dubbeld'
-            }
-          }
-        ]
+        frontMatter: {
+          // sidebarTitle: 'Page',
+          title: 'Callout',
+          date: '2023/5/15',
+          description:
+            'En example of using the Callout component in your blog.',
+          tag: 'web development',
+          author: 'Tristan Dubbeld'
+        }
       },
       {
         name: 'code-blocks',
         route: '/posts/code-blocks',
-        children: [
-          {
-            name: 'page',
-            route: '/posts/code-blocks/page',
-            frontMatter: {
-              sidebarTitle: 'Page',
-              title: 'Code blocks',
-              date: '2022/7/29',
-              description: 'En example of using code blocks in your blog.',
-              tag: 'web development,JavaScript,GraphQL,C++,Java,React,Next.js,The Guild,MacBook Pro',
-              author: 'Dimitri POSTOLOV'
-            }
-          }
-        ]
+        frontMatter: {
+          // sidebarTitle: 'Page',
+          title: 'Code blocks',
+          date: '2022/7/29',
+          description: 'En example of using code blocks in your blog.',
+          tag: 'web development,JavaScript,GraphQL,C++,Java,React,Next.js,The Guild,MacBook Pro',
+          author: 'Dimitri POSTOLOV'
+        }
       },
       {
         name: 'draft',
         route: '/posts/draft',
-        children: [
-          {
-            name: 'page',
-            route: '/posts/draft/page',
-            frontMatter: {
-              sidebarTitle: 'Page',
-              title: 'Draft',
-              date: '2023/6/28',
-              description: 'An example of a draft post.',
-              draft: true,
-              tag: 'web development',
-              author: 'Ada Lovelace'
-            }
-          }
-        ]
+        frontMatter: {
+          // sidebarTitle: 'Page',
+          title: 'Draft',
+          date: '2023/6/28',
+          description: 'An example of a draft post.',
+          draft: true,
+          tag: 'web development',
+          author: 'Ada Lovelace'
+        }
       },
       {
-        name: 'page',
-        route: '/posts/page',
+        name: 'index',
+        route: '/posts',
         frontMatter: {
-          sidebarTitle: 'Page',
+          // sidebarTitle: 'Page',
           type: 'posts',
           title: 'Random Thoughts',
-          date: new Date(1578009600000)
+          date: new Date(1578009600000).toISOString()
         }
       },
       {
         name: 'table',
         route: '/posts/table',
-        children: [
-          {
-            name: 'page',
-            route: '/posts/table/page',
-            frontMatter: {
-              sidebarTitle: 'Page',
-              title: 'Table',
-              date: '2022/8/28',
-              description: 'En example of using table.',
-              tag: 'web development',
-              author: 'Dimitri POSTOLOV'
-            }
-          }
-        ]
+        frontMatter: {
+          // sidebarTitle: 'Page',
+          title: 'Table',
+          date: '2022/8/28',
+          description: 'En example of using table.',
+          tag: 'web development',
+          author: 'Dimitri POSTOLOV'
+        }
       }
     ]
   },
@@ -148,7 +118,7 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Layout
-          pageOpts={{ frontMatter: {}, pageMap: [] }}
+          pageOpts={{ frontMatter: {}, pageMap }}
           themeConfig={{
             darkMode: true
           }}
