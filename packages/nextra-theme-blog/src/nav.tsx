18ca9b1e2c2e5454101d5fe70764faa053e7490b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-01-27 00:48:34 +0100

    nav

--- packages/nextra-theme-blog/src/nav.tsx
+++ packages/nextra-theme-blog/src/nav.tsx
@@ -1,39 +1,30 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import type { ReactElement } from 'react'
 import { useBlogContext } from './blog-context'
-import ThemeSwitch from './theme-switch'
-import { collectPostsAndNavs } from './utils/collect'
+import { ThemeSwitch } from './theme-switch'
 
-export default function Nav(): ReactElement {
-  const { opts, config } = useBlogContext()
-  const { navPages } = collectPostsAndNavs({ opts, config })
+export function Nav(): ReactElement {
+  const { config } = useBlogContext()
+  const pathname = usePathname()
 
   return (
     <div className="_mb-8 _flex _items-center _gap-3">
       <div className="_flex _grow _flex-wrap _items-center _justify-end _gap-3">
-        {navPages.map(page => {
-          const name = page.frontMatter?.title || page.name
-          if (page.active) {
-            return (
-              <span
-                key={page.route}
-                className="_cursor-default dark:_text-gray-400 _text-gray-600"
-              >
-                {name}
-              </span>
-            )
-          }
-          return (
-            <Link key={page.route} href={page.route}>
-              {name}
+        {config.navs?.map(nav =>
+          pathname === nav.url ? (
+            <span
+              key={nav.url}
+              className="_cursor-default dark:_text-gray-400 _text-gray-600"
+            >
+              {nav.name}
+            </span>
+          ) : (
+            <Link key={nav.url} href={nav.url}>
+              {nav.name}
             </Link>
           )
-        })}
-        {config.navs?.map(nav => (
-          <Link key={nav.url} href={nav.url}>
-            {nav.name}
-          </Link>
-        ))}
+        )}
       </div>
       {config.darkMode && <ThemeSwitch />}
     </div>
