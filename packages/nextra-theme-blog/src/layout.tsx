eb2631b61a95ef3436c9c842ea3b03a192917657
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-01-27 02:14:54 +0100

    more

--- packages/nextra-theme-blog/src/layout.tsx
+++ packages/nextra-theme-blog/src/layout.tsx
@@ -3,6 +3,7 @@ import type { ReactElement, ReactNode } from 'react'
 import { ThemeProvider } from './contexts'
 import { Nav } from './nav'
 import { ThemeConfigProvider } from './theme-config'
+import { ThemeSwitch } from './theme-switch'
 import type { BlogFrontMatter } from './types'
 
 export function Footer({
@@ -17,20 +18,20 @@ export function Layout({
   children,
   themeConfig
 }: NextraThemeLayoutProps<BlogFrontMatter>) {
+  const config = {
+    readMore: 'Read More →',
+    ...themeConfig
+  }
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <article
         className="_container _prose max-md:_prose-sm dark:_prose-dark"
         dir="ltr"
       >
-        <ThemeConfigProvider
-          value={{
-            readMore: 'Read More →',
-            ...themeConfig
-          }}
-        >
+        <ThemeConfigProvider value={config}>
           <div className="_mb-8 _flex _items-center _gap-3">
             <Nav />
+            {config.darkMode && <ThemeSwitch />}
           </div>
           {children}
         </ThemeConfigProvider>
--- packages/nextra-theme-blog/src/nav.tsx
+++ packages/nextra-theme-blog/src/nav.tsx
@@ -1,33 +1,30 @@
 'use client'
+
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import type { ReactElement } from 'react'
 import { useThemeConfig } from './contexts'
-import { ThemeSwitch } from './theme-switch'
 
 export function Nav(): ReactElement {
-  const { navs, darkMode } = useThemeConfig()
+  const { navs } = useThemeConfig()
   const pathname = usePathname()
 
   return (
-    <>
-      <div className="_flex _grow _flex-wrap _items-center _justify-end _gap-3">
-        {navs?.map(nav =>
-          nav.url === pathname ? (
-            <span
-              key={nav.url}
-              className="_cursor-default dark:_text-gray-400 _text-gray-600"
-            >
-              {nav.name}
-            </span>
-          ) : (
-            <Link key={nav.url} href={nav.url}>
-              {nav.name}
-            </Link>
-          )
-        )}
-      </div>
-      {darkMode && <ThemeSwitch />}
-    </>
+    <div className="_flex _grow _flex-wrap _items-center _justify-end _gap-3">
+      {navs?.map(nav =>
+        nav.url === pathname ? (
+          <span
+            key={nav.url}
+            className="_cursor-default dark:_text-gray-400 _text-gray-600"
+          >
+            {nav.name}
+          </span>
+        ) : (
+          <Link key={nav.url} href={nav.url}>
+            {nav.name}
+          </Link>
+        )
+      )}
+    </div>
   )
 }
