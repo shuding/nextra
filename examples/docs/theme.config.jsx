16977d61be955e7efd2f5b10b95c83bb4a27e5c0
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-02-03 06:25:28 +0100

    more

--- examples/docs/theme.config.jsx
+++ examples/docs/theme.config.jsx
@@ -23,37 +23,12 @@ export default {
         <meta httpEquiv="Content-Language" content="en" />
         <meta name="apple-mobile-web-app-title" content="Nextra" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-
-        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="https://nextra.site" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-icon-180x180.png"
-        />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
           href="/android-icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
         />
       </>
     )
