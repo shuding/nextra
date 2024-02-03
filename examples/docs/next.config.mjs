495979c04d10dcd7b46ff503ed7aa77e0a475725
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-02-03 03:24:18 +0100

    move docs example files

--- examples/docs/next.config.mjs
+++ examples/docs/next.config.mjs
@@ -1,8 +1,6 @@
 import nextra from 'nextra'
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './src/theme.config.jsx',
   latex: true,
   search: {
     codeblocks: false
