2898937cd4a11d9ad6e1ce8b5a67038f49a2cc85
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-02-04 06:48:42 +0100

    more

--- packages/nextra/src/server/generate-page-map.ts
+++ packages/nextra/src/server/generate-page-map.ts
@@ -31,27 +31,27 @@ export async function getFilepaths({
 }
 
 export function generatePageMapFromFilepaths(filepaths: string[]): any {
-  const paths = new Map(
-    filepaths.map(r => {
-      const pathInfo = path.parse(r)
+  const mdxPages: Record<string, string> = {}
+  const metaFiles: Record<string, string> = {}
 
-      if (pathInfo.name === 'page') {
-        return [pathInfo.dir.replace(/\(.*\)\//, ''), r]
-      }
-      if (pathInfo.name === '_meta') {
-        return [`${pathInfo.dir}/_meta`.replace(/^\//, ''), r]
-      }
-      return [
-        `${pathInfo.dir}/${pathInfo.name}`
-          .replace(/\/index$/, '')
-          .replace(/^\//, ''),
-        r
-      ]
-    })
-  )
+  for (const r of filepaths) {
+    const pathInfo = path.parse(r)
+
+    if (pathInfo.name === 'page') {
+      mdxPages[pathInfo.dir.replace(/\(.*\)\//, '')] = r
+    } else if (pathInfo.name === '_meta') {
+      metaFiles[`${pathInfo.dir}/_meta`.replace(/^\//, '')] = r
+    } else {
+      const key = `${pathInfo.dir}/${pathInfo.name}`
+        .replace(/\/index$/, '')
+        .replace(/^\//, '')
+      mdxPages[key] = r
+    }
+  }
+
   const obj = Object.create(null)
 
-  for (const path of paths.keys()) {
+  for (const path of [...Object.keys(mdxPages), ...Object.keys(metaFiles)]) {
     ;`${path}/index`
       .replace(/^\//, '')
       .split('/')
@@ -70,7 +70,7 @@ export function generatePageMapFromFilepaths(filepaths: string[]): any {
 
       if (name === '_meta') {
         // @ts-expect-error
-        list.push({ __metaPath: paths.get(route.slice(1)) })
+        list.push({ __metaPath: metaFiles[route.slice(1)] })
         continue
       }
 
@@ -83,12 +83,12 @@ export function generatePageMapFromFilepaths(filepaths: string[]): any {
       const isFolder =
         keys.length > 1 || (keys.length === 1 && keys[0] !== 'index')
       if (isFolder) {
-        ;(item as Folder).children = getPageMap(value, [], route).sort((a, b) =>
-          a.name.localeCompare(b.name)
+        ;(item as Folder).children = getPageMap(value, [], route).sort(
+          (a, b) => a.name.localeCompare(b.name)
         )
       } else if (!name.startsWith('[')) {
         // @ts-expect-error
-        item.__pagePath = paths.get(item.route.slice(1))
+        item.__pagePath = mdxPages[item.route.slice(1)]
       }
       list.push(item)
     }
@@ -97,5 +97,5 @@ export function generatePageMapFromFilepaths(filepaths: string[]): any {
 
   const pageMap = getPageMap(obj, [])
 
-  return pageMap
+  return { pageMap, mdxPages }
 }
