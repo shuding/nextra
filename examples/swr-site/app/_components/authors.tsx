88317365f2ddc10d00cd873362fc6c73baebf76d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-02-04 05:58:29 +0100

    more refacto

--- examples/swr-site/app/_components/authors.tsx
+++ examples/swr-site/app/_components/authors.tsx
@@ -1,35 +1,7 @@
-function Authors({ date, children, lang }) {
-  date = new Date(date)
-  return (
-    <div className="mb-16 mt-8 text-sm text-gray-400">
-      <time dateTime={date.toISOString()}>
-        {date.toLocaleDateString(lang, {
-          month: 'long',
-          day: 'numeric',
-          year: 'numeric'
-        })}
-      </time>{' '}
-      {text[lang!]} {children}
-    </div>
-  )
-}
+import { getDictionary } from '@app/_dictionaries/get-dictionary'
+import type { Locale } from '@app/_dictionaries/i18n-config'
 
-function Author({ name, link }) {
-  return (
-    <span className="[&:not(:last-child)]:after:content-[',_']">
-      <a
-        href={link}
-        target="_blank"
-        rel="noreferrer"
-        className="text-gray-800 dark:text-gray-100"
-      >
-        {name}
-      </a>
-    </span>
-  )
-}
-
-export function TopContent({
+export async function TopContent({
   title,
   date,
   authors,
@@ -40,17 +12,39 @@ export function TopContent({
   authors: {
     name: string
     link: string
-  }[],
-  lang: string
+  }[]
+  lang: Locale
 }) {
+  const dictionary = await getDictionary(lang)
+  const dateObj = new Date(date)
   return (
     <>
       <h1>{title}</h1>
-      <Authors date={date} lang={lang}>
+      <div className="mb-16 mt-8 text-sm text-gray-400">
+        <time dateTime={dateObj.toISOString()}>
+          {dateObj.toLocaleDateString(lang, {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric'
+          })}
+        </time>{' '}
+        {dictionary.by}{' '}
         {authors.map(author => (
-          <Author key={author.name} name={author.name} link={author.link} />
+          <span
+            key={author.name}
+            className="[&:not(:last-child)]:after:content-[',_']"
+          >
+            <a
+              href={author.link}
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-800 dark:text-gray-100"
+            >
+              {author.name}
+            </a>
+          </span>
         ))}
-      </Authors>
+      </div>
     </>
   )
 }
