0ed596486c13662f6928883b701bd7903d191490
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-02-04 03:22:48 +0100

    comment

--- examples/swr-site/app/_dictionaries/es.ts
+++ examples/swr-site/app/_dictionaries/es.ts
@@ -1,21 +1,21 @@
 import type { Dictionary } from './i18n-config'
 
 export default {
-  logo: {
-    title: 'Biblioteca React Hooks para la obtención de datos'
-  },
-  notFound: 'Esta pagina no se pudo encontrar',
-  poweredBy: 'Desarrollado por',
-  link: {
-    vercel: 'https://vercel.com/?utm_source=swr_es-es'
-  },
-  baseDescription: 'Biblioteca React Hooks para la obtención de datos',
-  lightweight: 'Ligero',
-  realtime: 'Tiempo real',
-  suspense: 'Suspenso',
-  pagination: 'Paginación',
-  backendAgnostic: 'Agnóstico del Backend',
-  renderingStrategies: 'SSR / SSG',
-  typescript: 'TypeScript',
-  remoteLocal: 'Remoto + Local'
+  // logo: {
+  //   title: 'Biblioteca React Hooks para la obtención de datos'
+  // },
+  // notFound: 'Esta pagina no se pudo encontrar',
+  // poweredBy: 'Desarrollado por',
+  // link: {
+  //   vercel: 'https://vercel.com/?utm_source=swr_es-es'
+  // },
+  // baseDescription: 'Biblioteca React Hooks para la obtención de datos',
+  // lightweight: 'Ligero',
+  // realtime: 'Tiempo real',
+  // suspense: 'Suspenso',
+  // pagination: 'Paginación',
+  // backendAgnostic: 'Agnóstico del Backend',
+  // renderingStrategies: 'SSR / SSG',
+  // typescript: 'TypeScript',
+  // remoteLocal: 'Remoto + Local'
 } satisfies Dictionary
