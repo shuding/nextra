08207b799d9661842dec3e66a02586a98ba5b821
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-02-04 06:05:05 +0100

    fix build

--- examples/swr-site/app/[lang]/[[...mdxPath]]/page.tsx
+++ examples/swr-site/app/[lang]/[[...mdxPath]]/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-env node */
 
+import { Video } from '@app/_components/video'
 import type { Metadata } from 'next'
 import type { Heading } from 'nextra'
 import { useMDXComponents } from 'nextra-theme-docs'
@@ -49,7 +50,15 @@ export default async function Page(pageProps: PageProps) {
   return (
     // @ts-expect-error -- fixme
     <Wrapper toc={useTOC()} metadata={metadata} title={title}>
-      <MDXContent components={components} {...pageProps} />
+      <MDXContent
+        components={{
+          ...components,
+          // fixes Error: Could not find the module "/nextra/examples/swr-site/app/_components/video.tsx#Video" in the React Client Manifest.
+          // This is probably a bug in the React Server Components bundler.
+          Video
+        }}
+        {...pageProps}
+      />
     </Wrapper>
   )
 }
