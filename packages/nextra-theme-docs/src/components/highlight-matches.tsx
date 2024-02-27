21f13ec94e58055ceb71333f48ebb54eea896ef8
이재민 <jaem1n207@naver.com>
2024-02-28 01:06:56 +0900

    fix: resolve memory leak issue in highlight-matches component
    A critical update has been made to prevent potential infinite loops that could occur in certain scenarios, such as when a match is followed by two spaces and a character.

--- packages/nextra-theme-docs/src/components/highlight-matches.tsx
+++ packages/nextra-theme-docs/src/components/highlight-matches.tsx
@@ -7,6 +7,32 @@ type MatchArgs = {
   match: string
 }
 
+/**
+ * Processes the user's search term by escaping each word, separated by spaces,
+ * and then joining them with '|'. This supports searching for multiple words,
+ * allowing each word to be searched independently.
+ *
+ * For example, the search term "apple  banana" (with two spaces) is transformed
+ * into "apple|banana", enabling the search for any results containing "apple" or "banana".
+ * This approach also gracefully handles extra spaces between words.
+ *
+ * @param searchTerm - The user's search term.
+ * @returns A RegExp object constructed from the processed search term.
+ */
+const processSearchTerm = (searchTerm: string): RegExp => {
+  const trimmedSearchTerm = searchTerm.trim()
+
+  const searchWords = trimmedSearchTerm.split(/\s+/).filter(Boolean)
+
+  if (searchWords.length > 0) {
+    const escapedSearch = searchWords.map(escapeStringRegexp).join('|')
+
+    return new RegExp(escapedSearch, 'ig')
+  }
+
+  return /$.^/gi
+}
+
 export const HighlightMatches = memo<MatchArgs>(function HighlightMatches({
   value,
   match
@@ -15,27 +41,27 @@ export const HighlightMatches = memo<MatchArgs>(function HighlightMatches({
     return null
   }
   const splitText = value.split('')
-  const escapedSearch = escapeStringRegexp(match.trim())
-  const regexp = new RegExp(escapedSearch.replaceAll(' ', '|'), 'ig')
+  const regexp = processSearchTerm(match)
   let result
   let index = 0
   const content: (string | ReactNode)[] = []
 
-  while (
-    (result = regexp.exec(value)) &&
-    // case `>  ` replaced previously to `>||` + some character provoke memory leak because
-    // `RegExp#exec` will always return a match
-    regexp.lastIndex !== 0
-  ) {
-    const before = splitText.splice(0, result.index - index).join('')
-    const after = splitText.splice(0, regexp.lastIndex - result.index).join('')
-    content.push(
-      before,
-      <span key={result.index} className="nx-text-primary-600">
-        {after}
-      </span>
-    )
-    index = regexp.lastIndex
+  while ((result = regexp.exec(value))) {
+    if (result.index === regexp.lastIndex) {
+      regexp.lastIndex++
+    } else {
+      const before = splitText.splice(0, result.index - index).join('')
+      const after = splitText
+        .splice(0, regexp.lastIndex - result.index)
+        .join('')
+      content.push(
+        before,
+        <span key={result.index} className="nx-text-primary-600">
+          {after}
+        </span>
+      )
+      index = regexp.lastIndex
+    }
   }
 
   return (
