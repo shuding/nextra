3248e20388b84cc23d6ec12242844fa4ce3ca791
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-04-07 02:50:33 +0200

    add children for head

--- packages/nextra-theme-blog/src/components/head.tsx
+++ packages/nextra-theme-blog/src/components/head.tsx
@@ -1,19 +1,26 @@
+import type { ReactNode } from 'react'
+
 const defaultBackgroundColor = {
   dark: '17,17,17',
   light: '250,250,250'
 }
 
-export function Head({
-  backgroundColor: bgColor = defaultBackgroundColor
-}: {
+type HeadProps = {
   backgroundColor?: {
     dark: string
     light: string
   }
-}) {
+  children?: ReactNode
+}
+
+export function Head({
+  backgroundColor: bgColor = defaultBackgroundColor,
+  children
+}: HeadProps) {
   return (
     // eslint-disable-next-line @next/next/no-head-element -- ignore since we use app router
     <head>
+      {children}
       <style>
         {`:root{--nextra-bg:${bgColor.light};}.dark{--nextra-bg:${bgColor.dark};}`}
       </style>
