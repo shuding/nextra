b35b87862fe017a5023c92dd7f0293f57202cecd
Shubham-EV <koundals536@gmail.com>
2024-07-17 21:28:02 +0530

    fixed scroll to top button

--- packages/nextra-theme-docs/src/components/back-to-top.tsx
+++ packages/nextra-theme-docs/src/components/back-to-top.tsx
@@ -1,25 +1,37 @@
 import cn from 'clsx'
 import { ArrowRightIcon } from 'nextra/icons'
 import type { ReactElement } from 'react'
+import { useEffect, useRef } from 'react'
 
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' })
 }
 
-export function BackToTop({
-  className,
-  hidden
-}: {
-  className?: string
-  hidden: boolean
-}): ReactElement {
+export function BackToTop({ className }: { className?: string }): ReactElement {
+  const ref = useRef<HTMLButtonElement>(null)
+  useEffect(() => {
+    function toggleVisible() {
+      const { scrollTop } = document.documentElement
+      if (scrollTop < 300) {
+        ref.current?.classList.add('_opacity-0', '_pointer-events-none')
+      } else {
+        ref.current?.classList.remove('_opacity-0', '_pointer-events-none')
+      }
+    }
+
+    window.addEventListener('scroll', toggleVisible)
+    return () => {
+      window.removeEventListener('scroll', toggleVisible)
+    }
+  }, [])
+
   return (
     <button
       aria-hidden="true"
+      ref={ref}
       onClick={scrollToTop}
-      disabled={hidden}
       className={cn(
-        '_flex _items-center _gap-1.5 _transition _opacity-100 disabled:_opacity-0',
+        '_flex _items-center _gap-1.5 _transition _opacity-0',
         className
       )}
     >
