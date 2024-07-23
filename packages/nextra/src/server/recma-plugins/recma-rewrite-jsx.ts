77b1931e29a2f7ab2edc90a0f5015569cb377960
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-07-23 16:35:00 +0200

    lint

--- packages/nextra/src/server/recma-plugins/recma-rewrite-jsx.ts
+++ packages/nextra/src/server/recma-plugins/recma-rewrite-jsx.ts
@@ -1,9 +1,4 @@
-import type {
-  CallExpression,
-  FunctionDeclaration,
-  Program,
-  ReturnStatement
-} from 'estree'
+import type { CallExpression, FunctionDeclaration, Program } from 'estree'
 import type { JsxAttribute } from 'estree-util-to-js/lib/jsx'
 import type { Plugin } from 'unified'
 import { visit } from 'unist-util-visit'
