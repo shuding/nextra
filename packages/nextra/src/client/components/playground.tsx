a21bf19b0d541406ff2702cf713108db26c7f8ac
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-08-31 09:14:17 +0200

    more

--- packages/nextra/src/client/components/playground.tsx
+++ packages/nextra/src/client/components/playground.tsx
@@ -1,47 +1,29 @@
 import { useEffect, useState } from 'react'
 import type { ReactElement } from 'react'
 import { compileMdxClient } from '../compileMdxClient.js'
-import { jsxRuntime } from '../jsx-runtime.cjs'
+import { CrossCircledIcon } from '../icons/index.js'
 import type { MDXComponents } from '../mdx.js'
-import { useMDXComponents } from '../mdx.js'
-
-export function evaluate(
-  compiledSource: string,
-  scope: Record<string, unknown> = {}
-) {
-  // if we're ready to render, we can assemble the component tree and let React do its thing
-  // first we set up the scope which has to include the mdx custom
-  // create element function as well as any components we're using
-  const keys = Object.keys(scope)
-  const values = Object.values(scope)
-  // now we eval the source code using a function constructor
-  // in order for this to work we need to have React, the mdx `createElement`,
-  // and all our components in scope for the function, which is the case here
-  // we pass the names (via keys) in as the function's args, and execute the
-  // function with the actual values.
-  console.log(compiledSource)
-  const hydrateFn = Reflect.construct(Function, ['$', ...keys, compiledSource])
-
-  return hydrateFn({ useMDXComponents, ...jsxRuntime }, ...values)
-}
+import { Code } from './code.js'
+import { Pre } from './pre.js'
+import { evaluate } from './remote-content.js'
 
 export function Playground({
   source,
   scope,
   components,
-  fallback
+  fallback = null
 }: {
   /**
    * String with source MDX
    *
-   * For example: `# hello world <br /> nice to see you`
+   * @example '# hello world <br /> nice to see you'
    */
   source: string
   /**
    * An object mapping names to React components.
    * The key used will be the name accessible to MDX.
    *
-   * For example: `{ ComponentName: Component }` will be accessible in the MDX as `<ComponentName/>`.
+   * @example `{ ComponentName: Component }` will be accessible in the MDX as `<ComponentName/>`.
    */
   components?: MDXComponents
   /**
@@ -51,24 +33,50 @@ export function Playground({
   /**
    * Fallback component for loading
    */
-  fallback?: ReactElement
+  fallback?: ReactElement | null
 }) {
-  const [compiledSource, setCompiledSource] = useState<any>(null)
+  const [compiledSource, setCompiledSource] = useState('')
+  const [error, setError] = useState<unknown>()
 
   useEffect(() => {
-    compileMdxClient(source).then(compiledResult => {
-      console.log(compiledResult)
-      setCompiledSource(compiledResult)
-    })
+    async function doCompile() {
+      try {
+        const mdx = await compileMdxClient(source)
+        setCompiledSource(mdx.result)
+        setError(null)
+      } catch (error) {
+        setError(error)
+      }
+    }
+
+    doCompile()
   }, [source])
 
-  const { default: MDXContent } = compiledSource
-    ? evaluate(compiledSource.result, scope)
-    : { default: null }
+  if (error) {
+    return (
+      <div className="[&_svg]:_text-red-500">
+        <Pre
+          data-filename="Could not compile code"
+          // @ts-expect-error todo: fix type
+          icon={CrossCircledIcon}
+          className="_whitespace-pre-wrap"
+        >
+          <Code>
+            <span>
+              {error instanceof Error
+                ? `${error.name} ${error.message}`
+                : String(error)}
+            </span>
+          </Code>
+        </Pre>
+      </div>
+    )
+  }
 
-  return compiledSource ? (
-    <MDXContent components={components} />
-  ) : (
-    fallback || null
-  )
+  if (compiledSource) {
+    const MDXContent = evaluate(compiledSource, scope).default
+    return <MDXContent components={components} />
+  }
+
+  return fallback
 }
