6c7aa510878ca82c8bed7cc25f7a63483145f2b7
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-14 15:52:02 -0700

    anchor

--- packages/nextra-theme-docs/src/components/anchor.tsx
+++ packages/nextra-theme-docs/src/components/anchor.tsx
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-restricted-imports -- only in this file we determine either we include <a /> as child of <NextLink />
+import cn from 'clsx'
 import NextLink from 'next/link'
 import type { ComponentProps, ReactElement } from 'react'
 import { forwardRef } from 'react'
@@ -9,10 +10,11 @@ export type AnchorProps = Omit<ComponentProps<'a'>, 'ref'> & {
 
 export const Anchor = forwardRef<HTMLAnchorElement, AnchorProps>(
   (
-    { href = '', children, newWindow, ...props },
+    { href = '', children, newWindow, className: _className, ...props },
     // ref is used in <NavbarMenu />
     forwardedRef
   ): ReactElement => {
+    const className = cn('nextra-focus', _className)
     if (newWindow) {
       return (
         <a
@@ -21,22 +23,14 @@ export const Anchor = forwardRef<HTMLAnchorElement, AnchorProps>(
           target="_blank"
           rel="noreferrer"
           {...props}
+          className={className}
         >
           {children}
         </a>
       )
     }
-
-    if (!href) {
-      return (
-        <a ref={forwardedRef} {...props}>
-          {children}
-        </a>
-      )
-    }
-
     return (
-      <NextLink ref={forwardedRef} href={href} {...props}>
+      <NextLink ref={forwardedRef} href={href} {...props} className={className}>
         {children}
       </NextLink>
     )
