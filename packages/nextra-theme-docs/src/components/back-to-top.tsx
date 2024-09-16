88d3d5c1e9632251206eccde2584550effa71ad2
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-17 00:52:00 +0200

    Update packages/nextra-theme-docs/src/components/back-to-top.tsx

--- packages/nextra-theme-docs/src/components/back-to-top.tsx
+++ packages/nextra-theme-docs/src/components/back-to-top.tsx
@@ -26,6 +26,7 @@ export function BackToTop({
 }): ReactElement {
   return (
     <Button
+      // elements with `aria-hidden: true` must not be focusable or contain focusable elements
       aria-hidden={hidden ? 'true' : undefined}
       onClick={scrollToTop}
       disabled={hidden}
