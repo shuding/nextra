14d196f2eb6fa68cfefda4ebf22b73a1330cbbf9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-22 13:55:46 +0200

    refactor

--- packages/nextra-theme-docs/src/stores/active-anchor.ts
+++ packages/nextra-theme-docs/src/stores/active-anchor.ts
@@ -1,44 +1,42 @@
 import type { Dispatch } from 'react'
 import { create } from 'zustand'
 
+const cb: IntersectionObserverCallback = entries => {
+  const entry = entries.find(entry => entry.isIntersecting)
+  console.log(entries)
+  const activeSlug = entry
+    ? (entry.target as HTMLAnchorElement).hash.slice(1)
+    : ''
+
+  useActiveAnchorStore.setState({ activeSlug })
+}
+
+const observer: IntersectionObserver =
+  typeof window === 'undefined'
+    ? null!
+    : new IntersectionObserver(cb, {
+        // root: document.querySelector('.nextra-toc + article'),
+        // Can be null if error is thrown from app somewhere
+        rootMargin: `-${document.querySelector('article')?.offsetTop ?? 0}px 0px -80%`,
+        threshold: [0, 1]
+      })
+
 const useActiveAnchorStore = create<{
-  observer: IntersectionObserver
   activeSlug: string
   actions: {
     observeAnchor: (el: HTMLAnchorElement) => Dispatch<void>
   }
-}>((set, get) => {
-  const callback: IntersectionObserverCallback = entries => {
-    const entry = entries.find(entry => entry.isIntersecting) || entries[0]
-    console.log(entries)
-    const activeSlug = entry
-      ? (entry.target as HTMLAnchorElement).hash.slice(1)
-      : ''
-
-    set({ activeSlug })
-  }
-
-  return {
-    activeSlug: '',
-    observer:
-      typeof window === 'undefined'
-        ? null!
-        : new IntersectionObserver(callback, {
-            // Can be null if error is thrown from app somewhere
-            rootMargin: `-${document.querySelector('article')?.offsetTop ?? 0}px 0px -80%`,
-            threshold: [0, 1]
-          }),
-    actions: {
-      observeAnchor(anchorElement) {
-        const { observer } = get()
-        observer.observe(anchorElement)
-        return () => {
-          observer.unobserve(anchorElement)
-        }
+}>(() => ({
+  activeSlug: '',
+  actions: {
+    observeAnchor(anchorElement) {
+      observer.observe(anchorElement)
+      return () => {
+        observer.unobserve(anchorElement)
       }
     }
   }
-})
+}))
 
 export const useActiveAnchor = () =>
   useActiveAnchorStore(state => state.activeSlug)
