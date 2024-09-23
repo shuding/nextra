e267d6938a895c22f0643ff8839d320c2f6704ba
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-23 20:15:23 +0200

    more

--- packages/nextra-theme-docs/src/mdx-components/wrapper/index.tsx
+++ packages/nextra-theme-docs/src/mdx-components/wrapper/index.tsx
@@ -4,20 +4,18 @@ import { MobileNav, Sidebar, SkipNavContent } from '../../components'
 import { ClientWrapper } from './wrapper.client'
 
 export const Wrapper: MDXWrapper = ({ toc, children, ...props }) => {
+  // @ts-expect-error fixme
+  toc = toc.map(item => ({
+    ...item,
+    value: removeLinks(item.value)
+  }))
   return (
     <div className="_mx-auto _flex _max-w-[90rem]">
       <Sidebar toc={toc} />
 
       <MobileNav toc={toc} />
 
-      <ClientWrapper
-        // @ts-expect-error fixme
-        toc={toc.map(item => ({
-          ...item,
-          value: removeLinks(item.value)
-        }))}
-        {...props}
-      >
+      <ClientWrapper toc={toc} {...props}>
         <SkipNavContent />
         <main
           data-pagefind-body={
--- packages/nextra/src/types.ts
+++ packages/nextra/src/types.ts
@@ -1,6 +1,6 @@
 import type { Heading as MDASTHeading } from 'mdast'
 import type { Metadata, NextConfig } from 'next'
-import type { FC, ReactNode } from 'react'
+import type { FC, ReactElement, ReactNode } from 'react'
 import type { z } from 'zod'
 import type { NEXTRA_INTERNAL } from './constants.js'
 import type { mathJaxOptionsSchema, nextraConfigSchema } from './server/schemas'
@@ -54,7 +54,7 @@ export type Page = (MdxFile | Folder<Page>) & {
 
 export type Heading = {
   depth: Exclude<MDASTHeading['depth'], 1>
-  value: string
+  value: string | ReactElement
   id: string
 }
 
