494a259bc59f1e7565264cfa6c106cccd1a41405
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-24 03:55:54 +0200

    more

--- packages/nextra-theme-docs/src/mdx-components/link.tsx
+++ packages/nextra-theme-docs/src/mdx-components/link.tsx
@@ -5,10 +5,15 @@ import type { AnchorProps } from '../components/anchor'
 const EXTERNAL_HREF_REGEX = /^https?:\/\//
 
 // TODO: use remark plugin which adds target="_blank" to external links
-export const Link = ({ href = '', className, ...props }: AnchorProps) => (
+export const Link = ({
+  href = '',
+  newWindow = EXTERNAL_HREF_REGEX.test(href),
+  className,
+  ...props
+}: AnchorProps) => (
   <Anchor
     href={href}
-    newWindow={EXTERNAL_HREF_REGEX.test(href)}
+    newWindow={newWindow}
     className={cn(
       '_text-primary-600 _underline _decoration-from-font [text-underline-position:from-font]',
       className
