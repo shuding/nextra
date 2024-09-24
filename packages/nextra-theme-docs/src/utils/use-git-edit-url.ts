41b9d3e8518846df86d0962040186aa4558c2dab
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-24 15:57:10 +0200

    more

--- packages/nextra-theme-docs/src/utils/use-git-edit-url.ts
+++ packages/nextra-theme-docs/src/utils/use-git-edit-url.ts
@@ -4,8 +4,5 @@ import { gitUrlParse } from './git-url-parse'
 export function useGitEditUrl(filePath = ''): string {
   const config = useThemeConfig()
   const repo = gitUrlParse(config.docsRepositoryBase || '')
-
-  if (!repo) throw new Error('Invalid `docsRepositoryBase` URL!')
-
   return `${repo.href}/${filePath}`
 }
