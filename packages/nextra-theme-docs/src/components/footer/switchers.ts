c78d1d9d7805989b17599158186c39e35d7ebf47
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-26 15:30:06 +0200

    more

--- packages/nextra-theme-docs/src/components/footer/switchers.ts
+++ packages/nextra-theme-docs/src/components/footer/switchers.ts
@@ -7,8 +7,8 @@ export function Switchers({ children }: { children: ReactNode }) {
   const { hideSidebar } = useConfig()
   const { i18n, darkMode } = useThemeConfig()
 
-  if (!hideSidebar || (!darkMode && !i18n.length)) {
-    return null
+  if (hideSidebar && (darkMode || i18n.length)) {
+    return children
   }
-  return children
+  return null
 }
