2d89604e4ab91391c12965896e2292d530c262f9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-29 02:49:49 +0200

    more

--- docs/components/screenshot.tsx
+++ docs/components/screenshot.tsx
@@ -1,5 +1,5 @@
 import cn from 'clsx'
-import { FC, ReactNode } from 'react'
+import type { FC, ReactNode } from 'react'
 
 export const Screenshot: FC<{ children: ReactNode; full?: boolean }> = ({
   full,
