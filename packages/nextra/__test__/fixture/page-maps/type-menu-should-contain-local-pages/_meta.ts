56107495f3831629af55b809a70956b143e05874
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-10-20 19:23:09 +0300

    aa

---
+++ packages/nextra/__test__/fixture/page-maps/type-menu-should-contain-local-pages/_meta.ts
@@ -0,0 +1,21 @@
+export default {
+  one: {
+    type: 'menu',
+    title: 'One',
+    items: {
+      2: {
+        title: 'Nextra',
+        href: 'https://nextra.site',
+      },
+      one: {
+        title: 'One'
+      },
+      foo: {
+        title: 'Foo'
+      },
+      qux: {
+        title: 'Qux'
+      },
+    }
+  },
+}
