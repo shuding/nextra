355d4b4490e9ba4cd7f6847d50cc776788d35990
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-10-20 18:06:56 +0300

    aa

--- packages/nextra/__test__/normalize-page.spec.ts
+++ packages/nextra/__test__/normalize-page.spec.ts
@@ -324,4 +324,84 @@ describe('normalize-page', () => {
       }
     })
   })
+
+  it.only('should initialize `activeType` from `*`', async () => {
+    const dir = path.join(
+      __dirname,
+      'fixture',
+      'page-maps',
+      'active-type-should-be-initialized-from-star'
+    )
+    vi.doMock('../src/server/file-system.ts', () => ({ PAGES_DIR: dir }))
+    vi.doMock('../src/server/constants.ts', async () => ({
+      ...(await vi.importActual('../src/server/constants.ts')),
+      CHUNKS_DIR: dir
+    }))
+    const { collectPageMap } = await import('../src/server/page-map.js')
+
+    const result = await collectPageMap({ dir })
+    await fs.writeFile(path.join(dir, 'generated-page-map.ts'), result)
+
+    const { pageMap } = await import(
+      './fixture/page-maps/active-type-should-be-initialized-from-star/generated-page-map.js'
+      )
+
+    expect(pageMap).toEqual([
+      {
+        data: {
+          '1-level': {
+            display: 'hidden',
+            theme: {
+              layout: 'full'
+            }
+          }
+        }
+      },
+      {
+        name: '1-level',
+        route: '/1-level',
+        children: [
+          {
+            data: {
+              '*': {
+                type: 'page',
+                theme: {
+                  layout: 'default',
+                  toc: false
+                }
+              }
+            }
+          },
+          {
+            name: 'foo',
+            route: '/1-level/foo',
+            frontMatter: {
+              sidebarTitle: 'Foo'
+            }
+          }
+        ]
+      }
+    ])
+
+    const { activeType, activeIndex, activeThemeContext } = normalizePages({
+      list: pageMap,
+      route: '/1-level/not-exist'
+    })
+    expect({ activeType, activeIndex, activeThemeContext }).toEqual({
+      activeType: 'page',
+      activeIndex: 0,
+      activeThemeContext: {
+        breadcrumb: true,
+        collapsed: false,
+        footer: true,
+        layout: 'default',
+        navbar: true,
+        pagination: true,
+        sidebar: true,
+        timestamp: true,
+        toc: false,
+        typesetting: 'default'
+      }
+    })
+  })
 })
