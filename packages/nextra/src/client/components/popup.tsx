8118631ec6e5a25dea67be083cdeb05258860e0f
ziho <ziho.dev@gmail.com>
2024-10-25 01:37:18 +0900

    chore: unnecessary props

--- packages/nextra/src/client/components/popup.tsx
+++ packages/nextra/src/client/components/popup.tsx
@@ -81,16 +81,7 @@ export function PopupPanel({
   const { open, handleOpen, handleClose } = usePopupContext()
 
   return (
-    <Transition
-      show={open}
-      as={Fragment}
-      enter="transition ease-out duration-200"
-      enterFrom="opacity-0 translate-y-1"
-      enterTo="opacity-100 translate-y-0"
-      leave="transition ease-in duration-150"
-      leaveFrom="opacity-100 translate-y-0"
-      leaveTo="opacity-0 translate-y-1"
-    >
+    <Transition show={open} as={Fragment}>
       <PopoverPanel
         static={true}
         anchor={{ to: 'bottom', gap: -24 }}
