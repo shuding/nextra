90e8988a1ccbb8b7d93035cf248c44b5b46c69b8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-10-28 02:38:29 +0300

    - use ReactIcon for code blocks with `jsx`, `tsx` language - add JsonIcon for `json` language - parse language from filename if exist when `diff` language is specified - use JavaScript icon for `cjs` and `mjs` - use TypeScript icon for `cts` and `mts`

--- packages/nextra/src/client/components/with-icons.tsx
+++ packages/nextra/src/client/components/with-icons.tsx
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import type { FC, SVGProps } from 'react'
 import {
   CPPIcon,
@@ -6,10 +7,12 @@ import {
   GoIcon,
   GraphQLIcon,
   JavaScriptIcon,
+  JSONIcon,
   MarkdownIcon,
   MdxIcon,
   MoveIcon,
   PythonIcon,
+  ReactIcon,
   RustIcon,
   TerminalIcon,
   TerraformIcon,
@@ -17,40 +20,72 @@ import {
 } from '../icons/index.js'
 import type { PreProps } from '../mdx-components/pre/index.js'
 
-type T = Record<string, FC<SVGProps<SVGElement>>>
-
-export function withIcons(Component: FC, obj?: T): FC<PreProps> {
-  const LanguageToIcon: T = {
-    js: JavaScriptIcon,
-    jsx: JavaScriptIcon,
-    ts: TypeScriptIcon,
-    tsx: TypeScriptIcon,
-    md: MarkdownIcon,
-    mdx: MdxIcon,
-    sh: TerminalIcon,
-    bash: TerminalIcon,
-    css: CssIcon,
-    'c++': CPPIcon,
-    cpp: CPPIcon,
-    csharp: CsharpIcon,
-    cs: CsharpIcon,
-    'c#': CsharpIcon,
-    graphql: GraphQLIcon,
-    python: PythonIcon,
-    py: PythonIcon,
-    rust: RustIcon,
-    rs: RustIcon,
-    terraform: TerraformIcon,
-    tf: TerraformIcon,
-    move: MoveIcon,
-    go: GoIcon,
-    golang: GoIcon,
-    ...obj
+function getIcon(language: string) {
+  switch (language) {
+    case 'js':
+    case 'mjs':
+    case 'cjs':
+      return JavaScriptIcon
+    case 'ts':
+    case 'mts':
+    case 'cts':
+      return TypeScriptIcon
+    case 'jsx':
+    case 'tsx':
+      return ReactIcon
+    case 'md':
+      return MarkdownIcon
+    case 'mdx':
+      return MdxIcon
+    case 'sh':
+    case 'bash':
+      return TerminalIcon
+    case 'css':
+      return CssIcon
+    case 'c++':
+    case 'cpp':
+      return CPPIcon
+    case 'csharp':
+    case 'cs':
+    case 'c#':
+      return CsharpIcon
+    case 'graphql':
+    case 'gql':
+      return GraphQLIcon
+    case 'python':
+    case 'py':
+      return PythonIcon
+    case 'rust':
+    case 'rs':
+      return RustIcon
+    case 'terraform':
+    case 'tf':
+      return TerraformIcon
+    case 'move':
+      return MoveIcon
+    case 'go':
+    case 'golang':
+      return GoIcon
+    case 'json':
+    case 'jsonc':
+      return JSONIcon
   }
+}
 
+export function withIcons(
+  Component: FC,
+  obj?: Record<string, FC<SVGProps<SVGElement>>>
+): FC<PreProps> {
   return function Pre(props) {
-    const language = props['data-language']
-    const Icon = language && LanguageToIcon[language]
+    let language = props['data-language']
+    const filename = props['data-filename']
+    if (language === 'diff' && filename) {
+      const ext = path.parse(filename).ext.slice(1)
+      if (ext) {
+        language = ext
+      }
+    }
+    const Icon = language && (obj?.[language] || getIcon(language))
 
     return (
       <Component
