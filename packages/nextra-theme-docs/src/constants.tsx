739abad3d4a44522640b1461bbe613168d1000a7
wayne-shih <0x26@wayneshih.com>
2024-10-30 00:49:48 +1300

    chore: lint is angry with order

--- packages/nextra-theme-docs/src/constants.tsx
+++ packages/nextra-theme-docs/src/constants.tsx
@@ -42,11 +42,11 @@ export const DEFAULT_THEME: DocsThemeConfig = {
       dark: 204,
       light: 212
     },
-    saturation: 100,
     lightness: {
       dark: 55,
       light: 45
-    }
+    },
+    saturation: 100
   },
   darkMode: true,
   direction: 'ltr',
