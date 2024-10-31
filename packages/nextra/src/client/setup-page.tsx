e8f51b0ef21dffbc09205e626ed4dba3b22c603c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-10-31 03:19:56 +0300

    aa

--- packages/nextra/src/client/setup-page.tsx
+++ packages/nextra/src/client/setup-page.tsx
@@ -3,17 +3,18 @@
  * This file should be never used directly, only in loader.ts
  */
 
-import { useMDXComponents } from 'next-mdx-import-source-file'
+import { useMDXComponents as getMDXComponents } from 'next-mdx-import-source-file'
 import type { ComponentProps, FC } from 'react'
 import { createElement } from 'react'
 import type { Heading, MDXWrapper, PageOpts } from '../types'
+
+const Wrapper = getMDXComponents().wrapper
 
 export function HOC_MDXWrapper(
   MDXContent: MDXWrapper,
   hocProps: PageOpts & { toc: Heading[] }
 ): FC<ComponentProps<MDXWrapper>> {
   return function MDXWrapper(props) {
-    const Wrapper = useMDXComponents().wrapper
     const children = createElement(MDXContent, props)
 
     return Wrapper ? <Wrapper {...hocProps}>{children}</Wrapper> : children
