35472c424898d0d7ddf050abe98551535179dd2b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-02 14:16:57 +0300

    more

--- packages/nextra-theme-docs/tailwind.config.ts
+++ packages/nextra-theme-docs/tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
   content: [
     './src/**/*.{ts,tsx}',
     '../nextra/src/client/icons/*.svg',
-    '../nextra/src/client/mdx.tsx',
+    '../nextra/src/client/mdx-components.tsx',
     '../nextra/src/client/{components,mdx-components}/**/*.tsx'
   ],
   theme: {
