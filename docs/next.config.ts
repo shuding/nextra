8dc125d08abacfc1961a004b255db8b862a9bf31
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-02 13:40:00 +0300

    Update next.config.ts

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -51,6 +51,6 @@ export default withNextra({
     return config
   },
   experimental: {
-    optimizePackageImports: ['@components/icons']
+    optimizePackageImports: ['@components/icons', 'framer-motion']
   }
 })
