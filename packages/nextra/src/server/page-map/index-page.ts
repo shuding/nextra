31ee3248212ef07b3072396515f2bffff135e896
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-11 03:09:12 +0300

    more

---
+++ packages/nextra/src/server/page-map/index-page.ts
@@ -0,0 +1,41 @@
+import { MdxFile, PageMapItem } from '../../types'
+import { compileMdx } from '../compile.js'
+
+function renderCard(item: MdxFile): string {
+  const { icon, sidebarTitle } = item.frontMatter!
+  const Icon = icon ? `<${icon}/>` : 'null'
+  return `<Cards.Card title="${sidebarTitle}" href="${item.route}" icon={${Icon}} />`
+}
+
+export async function createIndexPage(pageMap: PageMapItem[]): Promise<string> {
+  let result = []
+  let hasCards = false
+  for (const item of pageMap) {
+    if ('data' in item) {
+      continue
+    }
+    // @ts-expect-error fixme
+    if (item.type === 'separator') {
+      if (hasCards) {
+        result.push('</Cards>')
+        hasCards = false
+      }
+      // @ts-expect-error fixme
+      result.push(`## ${item.title}`)
+      continue
+    }
+    if (!hasCards) {
+      hasCards = true
+      result.push('<Cards>')
+    }
+    result.push(renderCard(item))
+  }
+  if (hasCards) {
+    result.push('</Cards>')
+  }
+  const rawJsx = result.join('\n')
+
+  const mdx = await compileMdx(rawJsx)
+
+  return mdx.result
+}
