1d27429128036eb786949a482d3593f9d6cea0e8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-25 22:00:04 +0700

    Update packages/nextra/src/client/components/file-tree/tree.tsx [skip ci]

--- packages/nextra/src/client/components/file-tree/tree.tsx
+++ packages/nextra/src/client/components/file-tree/tree.tsx
@@ -8,7 +8,7 @@ export const Tree: FC<{ children: ReactNode }> = ({ children }) => {
         'nextra-filetree x:mt-6 x:select-none x:text-sm x:text-gray-800 x:dark:text-gray-300',
         'not-prose', // for nextra-theme-blog
         'x:rounded-lg x:border x:px-4 x:py-3 x:inline-flex x:flex-col x:gap-2',
-        'x:border-neutral-200/70 x:contrast-more:border-neutral-400 x:dark:border-primary-100/10 x:contrast-more:dark:border-neutral-400'
+        'x:border-neutral-200/70 x:contrast-more:border-neutral-400! x:dark:border-primary-100/10'
       )}
     >
       {children}
