44c8a74903a73f4eb8261b7f69053e4cfb161823
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-25 22:00:15 +0700

    Update packages/nextra-theme-docs/src/components/pagination.tsx [skip ci]

--- packages/nextra-theme-docs/src/components/pagination.tsx
+++ packages/nextra-theme-docs/src/components/pagination.tsx
@@ -10,7 +10,7 @@ const classes = {
     'x:focus-visible:nextra-focus x:text-gray-600 x:dark:text-gray-400',
     'x:hover:text-gray-800 x:dark:hover:text-gray-200',
     'x:contrast-more:text-gray-700 x:contrast-more:dark:text-gray-100',
-    'x:flex x:max-w-[50%] x:items-center x:gap-1 x:py-4 x:text-base x:font-medium x:transition-colors [word-break:break-word] x:md:text-lg'
+    'x:flex x:max-w-[50%] x:items-center x:gap-1 x:py-4 x:text-base x:font-medium x:transition-colors x:[word-break:break-word] x:md:text-lg'
   ),
   icon: cn('x:inline x:shrink-0')
 }
