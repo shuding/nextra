3d99511ecb808d06e91acd3f44225cda8c5221ed
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-27 02:53:18 +0700

    react com

--- packages/nextra/src/client/hooks/use-fs-route.ts
+++ packages/nextra/src/client/hooks/use-fs-route.ts
@@ -1,16 +1,13 @@
 import { usePathname } from 'next/navigation'
-import { useMemo } from 'react'
 
 const defaultLocale = process.env.NEXTRA_DEFAULT_LOCALE
 
 export function useFSRoute() {
   const pathname = usePathname()
-  return useMemo(
-    () =>
-      (defaultLocale ? '/' + pathname.split('/').slice(2).join('/') : pathname)
-        .replace(/\.html$/, '')
-        .replace(/\/index(\/|$)/, '$1')
-        .replace(/\/$/, '') || '/',
-    [pathname]
+  return (
+    (defaultLocale ? '/' + pathname.split('/').slice(2).join('/') : pathname)
+      .replace(/\.html$/, '')
+      .replace(/\/index(\/|$)/, '$1')
+      .replace(/\/$/, '') || '/'
   )
 }
