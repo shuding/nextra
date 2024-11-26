da7197957b771c8ee449a779d89856c7f3573d64
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-27 04:32:01 +0700

    aa

--- packages/nextra/src/client/hooks/use-mounted.ts
+++ packages/nextra/src/client/hooks/use-mounted.ts
@@ -1,9 +1,9 @@
 'use client'
+'use no memo' // don't need to memoize boolean `mounted` value
 
 import { useEffect, useState } from 'react'
 
 export function useMounted(): boolean {
-  'use no memo' // don't need to memoize boolean value
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
