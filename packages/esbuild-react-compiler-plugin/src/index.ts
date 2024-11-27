e45c95a6c51cc71731b68c3d01b789182bc6c34f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-28 02:17:37 +0700

    more

---
+++ packages/esbuild-react-compiler-plugin/src/index.ts
@@ -0,0 +1,71 @@
+import type { Options } from 'tsup'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+// @ts-expect-error
+import reactCompilerLoader from 'react-compiler-webpack/dist/react-compiler-loader.js'
+
+const reactCompilerConfig = {
+  sources(_filename: string) {
+    return true
+  },
+  target: '18'
+}
+
+
+export const reactCompilerPlugin= (filter: RegExp): NonNullable<Options['esbuildPlugins']>[number] => ({
+  name: 'react-compiler',
+  setup(build) {
+    build.onLoad({ filter }, async args => {
+      // Read the file content
+      const code = await fs.readFile(args.path)
+      return new Promise<{
+        contents: string
+        loader: 'ts' | 'tsx'
+      }>((resolve, reject) => {
+        reactCompilerLoader.call(
+          {
+            async: () =>
+              function callback(error: Error | null, result?: string) {
+                if (error) {
+                  reject(error)
+                } else {
+                  const loader = path.extname(args.path).slice(1) as
+                    | 'ts'
+                    | 'tsx'
+                  const relativePath = path.relative(process.cwd(), args.path)
+
+                  if (
+                    /^import \{ c as _c } from "react-compiler-runtime";/m.test(
+                      result!
+                    )
+                  ) {
+                    console.info(
+                      '🚀 File',
+                      relativePath,
+                      'was optimized with react-compiler'
+                    )
+                  } else if (!/^'use no memo'/m.test(result!)) {
+                    console.error(
+                      '❌ File',
+                      relativePath,
+                      'was not optimized with react-compiler'
+                    )
+                    // console.log(result)
+                    // process.exit(1)
+                  }
+
+                  resolve({
+                    contents: result!,
+                    loader // Mark the file as a JSX file
+                  })
+                }
+              },
+            getOptions: () => reactCompilerConfig,
+            resourcePath: args.path
+          },
+          code
+        )
+      })
+    })
+  }
+})
---
+++ packages/esbuild-react-compiler-plugin/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+import { defaultEntry } from '../nextra-theme-docs/tsup.config.js'
+import packageJson from './package.json'
+
+export default defineConfig({
+  name: packageJson.name,
+  entry: defaultEntry,
+  format: 'esm',
+  dts: true,
+  splitting: process.env.NODE_ENV === 'production',
+  bundle: false,
+})
