90ea6e5b5a4c8d097b0105733e6aeab351aba1d0
Elliot Courant <me@elliotcourant.dev>
2024-12-04 18:12:03 -0600

    feat(theme): Content width css variable for easier customization
    This moves the content width that was used throughout nextra
    `max-w-[90rem]` to a css variable. This way the changing the value in
    the future should be easier, but it also makes it possible to customize
    the max width of the content for people using nextra without needing to
    overload a tailwind class in a hacky way.

--- packages/nextra-theme-docs/src/components/footer.tsx
+++ packages/nextra-theme-docs/src/components/footer.tsx
@@ -10,7 +10,7 @@ export function Footer({ menu }: { menu?: boolean }): ReactElement {
     <footer className="_bg-gray-100 _pb-[env(safe-area-inset-bottom)] dark:_bg-neutral-900 print:_bg-transparent">
       <div
         className={cn(
-          '_mx-auto _flex _max-w-[90rem] _gap-2 _py-2 _px-4',
+          '_mx-auto _flex _max-w-[var(--nextra-content-width)] _gap-2 _py-2 _px-4',
           menu && (themeConfig.i18n.length > 0 || themeConfig.darkMode)
             ? '_flex'
             : '_hidden'
@@ -23,7 +23,7 @@ export function Footer({ menu }: { menu?: boolean }): ReactElement {
       <hr className="dark:_border-neutral-800" />
       <div
         className={cn(
-          '_mx-auto _flex _max-w-[90rem] _justify-center _py-12 _text-gray-600 dark:_text-gray-400 md:_justify-start',
+          '_mx-auto _flex _max-w-[var(--nextra-content-width)] _justify-center _py-12 _text-gray-600 dark:_text-gray-400 md:_justify-start',
           '_pl-[max(env(safe-area-inset-left),1.5rem)] _pr-[max(env(safe-area-inset-right),1.5rem)]'
         )}
       >
