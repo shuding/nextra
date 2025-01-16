2cd3fbd514bdcbd69cd9f431a4c9365e7743928c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-17 02:11:09 +0700

    upd

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -12,7 +12,10 @@ import styles from './page.module.css'
 import './page.css'
 import type { FC } from 'react'
 
-export const metadata: Metadata = {}
+export const metadata: Metadata = {
+  description:
+    'Build fast, customizable, and content-rich websites with Nextra. Powered by Next.js, it offers seamless Markdown support, customizable themes, file conventions, and easy integration with MDX, making it perfect for documentation, blogs, and static websites.'
+}
 
 const IndexPage: FC = () => {
   return (
