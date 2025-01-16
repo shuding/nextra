7cfd5a88cfe8877a05d4b6f38f8df75bf0b8b618
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-16 20:18:29 +0700

    prettier

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -228,7 +228,7 @@ const IndexPage: FC = () => {
                 </Link>
                 .
               </p>
-              <div className="z-1 absolute inset-0 size-full bg-[linear-gradient(to_right,white_250px,_transparent)] max-sm:hidden dark:bg-[linear-gradient(to_right,#202020_250px,_transparent)]" />
+              <div className="absolute inset-0 z-1 size-full bg-[linear-gradient(to_right,white_250px,_transparent)] max-sm:hidden dark:bg-[linear-gradient(to_right,#202020_250px,_transparent)]" />
               <video
                 autoPlay
                 loop
