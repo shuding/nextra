acb6d479e31c4398288f715085eae317c5193e46
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-24 00:26:56 +0700

    aa

--- packages/nextra/src/server/rehype-plugins/rehype-extract-toc-content.ts
+++ packages/nextra/src/server/rehype-plugins/rehype-extract-toc-content.ts
@@ -107,6 +107,7 @@ const transformer: Transformer<Root> = (ast, file) => {
           createAstExportConst('toc', {
             type: 'CallExpression',
             callee: { type: 'Identifier', name: 'useTOC' },
+            // https://github.com/shuding/nextra/issues/3979
             arguments: [{ type: 'ObjectExpression', properties: [] }],
             optional: false
           })
