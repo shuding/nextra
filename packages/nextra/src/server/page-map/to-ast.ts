a61c048b6d8e701a2b919eb5c3f81e8e7316142d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-23 23:42:13 +0700

    Update to-ast.ts

--- packages/nextra/src/server/page-map/to-ast.ts
+++ packages/nextra/src/server/page-map/to-ast.ts
@@ -6,18 +6,15 @@ import { createAstObject } from '../utils.js'
 function cleanFilePath(filePath: string): string {
   // Remove extension
   const { dir, name } = path.parse(filePath)
-
-  const path = `${dir.replace(/^(src\/)?content\/?/, '')}_${name}`
-  // handle non-"\w" characters
-  const encodePath = encodeURI(path)
-
+  // Remove `content` prefix
+  const fileName = `${dir.replace(/^(src\/)?content\/?/, '')}_${name}`
   return (
-    // Remove `content` prefix
-    encodePath
+    // handle non-"\w" characters
+    encodeURI(fileName)
       .replaceAll(/[\W_]+/g, '_')
       // Remove leading `_`
       .replace(/^_/, '')
-      // Variable can't start with number
+      // Variable can't start with a number
       .replace(/^\d/, match => `_${match}`)
   )
 }
