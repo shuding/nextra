abdba33025d458c1f2c739ca462bb547969f6e03
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-26 02:39:12 +0700

    upd

--- packages/nextra/src/client/mdx-components/summary.tsx
+++ packages/nextra/src/client/mdx-components/summary.tsx
@@ -1,35 +1,50 @@
 import cn from 'clsx'
 import type { ComponentProps, FC } from 'react'
-import { ArrowRightIcon } from '../icons/index.js'
+import { ArrowRightIcon, LinkIcon } from '../icons'
 
 export const Summary: FC<ComponentProps<'summary'>> = ({
   children,
   className,
+  id,
   ...props
 }) => {
   return (
     <summary
       className={cn(
         'x:focus-visible:nextra-focus',
-        'x:flex x:items-center x:cursor-pointer x:p-1 x:transition-colors x:hover:bg-gray-100 x:dark:hover:bg-neutral-800',
-        'x:[&::-webkit-details-marker]:hidden', // Safari
-        // display: flex removes whitespace when `<summary>` contains text with other elements, like `foo <strong>bar</strong>`
-        'x:whitespace-pre-wrap',
+        'x:cursor-pointer x:p-1 x:transition-colors',
+        'x:hover:not-has-[a:hover]:bg-gray-100 x:dark:hover:not-has-[a:hover]:bg-neutral-800',
         'x:select-none x:rounded',
+        'x:marker:content-[""]',
+        'x:px-[1.7em] x:relative',
         className
       )}
       {...props}
     >
-      {children}
       <ArrowRightIcon
-        height="16"
+        // ID attached to summary jumps to incorrect position in viewport
+        id={id}
+        height=".9em"
         className={cn(
-          'x:order-first', // if prettier formats `summary` it will have unexpected margin-top
-          'x:shrink-0 x:mx-1.5 x:motion-reduce:transition-none',
+          'x:motion-reduce:transition-none x:absolute x:start-1.5 x:top-2.5',
           'x:rtl:rotate-180 x:[[data-expanded]>summary:first-child>&]:rotate-90 x:transition'
         )}
         strokeWidth="3"
       />
+      {children}
+      {id && (
+        <a
+
+          href={`#${id}`}
+          className={cn(
+            'x:focus-visible:nextra-focus x:absolute x:end-0 x:top-0 x:rounded x:p-2 x:h-full',
+            'x:hover:bg-gray-100 x:dark:hover:bg-neutral-800',
+            className
+          )}
+        >
+          <LinkIcon height="1em" className="x:pointer-events-none" />
+        </a>
+      )}
     </summary>
   )
 }
