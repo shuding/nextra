d6e3328f57d546d52a57edf2cb27842b01edca30
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-26 03:35:19 +0700

    upd

--- packages/nextra/src/client/mdx-components/summary.tsx
+++ packages/nextra/src/client/mdx-components/summary.tsx
@@ -30,9 +30,11 @@ export const Summary: FC<ComponentProps<'summary'>> = ({
         strokeWidth="3"
       />
       <h3
-        // ID attached to summary jumps to incorrect position in viewport
+        // ID attached to `<summary>` jumps to incorrect position in viewport.
+        // Also, it's better to put `<summary>` content inside heading, so Pagefind will have
+        // sub result title of this `<summary>` content
         id={id}
-        className='x:grow x:hyphens-auto x:p-1'
+        className="x:grow x:hyphens-auto x:p-1"
       >
         {children}
       </h3>
@@ -45,10 +47,14 @@ export const Summary: FC<ComponentProps<'summary'>> = ({
             'x:flex x:items-center',
             'x:focus-visible:nextra-focus x:rounded x:px-2',
             'x:hover:bg-gray-100 x:dark:hover:bg-neutral-800',
-            'x:hover:[summary:has(&)]:bg-transparent',
+            'x:hover:[summary:has(&)]:bg-transparent'
           )}
         >
-          <LinkIcon height="1em" className="x:pointer-events-none" />
+          <LinkIcon
+            height="1em"
+            // avoid triggering by event.target in onClick handler from `<details>`
+            className="x:pointer-events-none"
+          />
         </a>
       )}
     </summary>
