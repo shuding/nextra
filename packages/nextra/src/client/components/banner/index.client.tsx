a1e496d5242e459f1c825d31a54e6742639d71c6
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-28 21:27:15 +0700

    a

--- packages/nextra/src/client/components/banner/index.client.tsx
+++ packages/nextra/src/client/components/banner/index.client.tsx
@@ -4,10 +4,10 @@ import type { ComponentPropsWithoutRef, FC } from 'react'
 import { useEffect, useRef } from 'react'
 
 export const ClientBanner: FC<ComponentPropsWithoutRef<'div'>> = props => {
-  const el = useRef<HTMLDivElement>(null!)
+  const banner = useRef<HTMLDivElement>(null!)
   // Set height on mount because text can be wrapped on next line and height can be different
   useEffect(() => {
-    const height = el.current.clientHeight
+    const height = banner.current.clientHeight
     document.documentElement.style.setProperty(
       '--nextra-banner-height',
       `${height}px`
@@ -16,7 +16,7 @@ export const ClientBanner: FC<ComponentPropsWithoutRef<'div'>> = props => {
 
   return (
     <div
-      ref={el}
+      ref={banner}
       // Because we update class in `<script>`
       suppressHydrationWarning
       {...props}
