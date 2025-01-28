0f061dcf1d5725414827ed124e827b54f89f4be3
Hichem Fantar <fantar.hichem@gmail.com>
2025-01-28 13:52:04 +0100

    Refactor GitHub alert capitalization to use CSS for styling

--- packages/nextra/src/client/hocs/with-github-alert.tsx
+++ packages/nextra/src/client/hocs/with-github-alert.tsx
@@ -36,13 +36,13 @@ export function withGitHubAlert(
               `Invalid GitHub alert type: "${alertName}". Should be one of: ${GITHUB_ALERTS.join(', ')}.`
             )
           }
-          const capitalizedName =
-            alertName[0]!.toUpperCase() + alertName.slice(1)
           return fn({
             ...props,
             type: alertName as T,
             children: [
-              <b key={0}>{capitalizedName}</b>,
+              <b key={0} style={{
+                textTransform: 'capitalize',
+              }}>{alertName}</b>,
               ...props.children.slice(2)
             ]
           })
