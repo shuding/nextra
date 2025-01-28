b2d5a2ec4d333bd1022a5a8d40aef67ff62e792e
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-28 21:41:13 +0700

    tiny polish

--- packages/nextra/src/client/components/banner/index.tsx
+++ packages/nextra/src/client/components/banner/index.tsx
@@ -20,19 +20,18 @@ export const Banner: FC<{
     <ClientBanner
       className={cn(
         BANNER_CLASS_NAME,
-        'x:max-md:sticky x:top-0 x:z-20 x:flex x:items-center',
+        'x:max-md:sticky x:top-0 x:z-20 x:flex x:items-center x:px-2',
         'x:text-slate-50 x:dark:text-white x:bg-neutral-900 x:dark:bg-[linear-gradient(1deg,#383838,#212121)]',
-        'x:px-2 x:py-2.5',
         'x:print:[display:none]' // to not match `x:[.nextra-banner:not([class$=hidden])~&]` class
       )}
     >
-      <div className="x:w-full x:text-center x:font-medium x:text-sm">
+      <div className="x:w-full x:text-center x:font-medium x:text-sm x:py-2.5">
         {children}
       </div>
       {dismissible && (
         <CloseBannerButton storageKey={storageKey}>
           <script dangerouslySetInnerHTML={{ __html: hideBannerScript }} />
-          <XIcon height="16" />
+          <XIcon height="1em" />
         </CloseBannerButton>
       )}
     </ClientBanner>
