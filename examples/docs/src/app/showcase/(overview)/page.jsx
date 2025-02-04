dd32eca9de0940faae5ea3eb9531a9ee75a487fa
Jeremy JIANG <JeremyJiang430@outlook.com>
2025-02-04 22:51:21 +0800

    fix: normalize path with nextjs `normalizeAppPath` utility function (#4149)
    * fix: normalize path with nextjs `normalizeAppPath` utility function
    
    * fix: import next.js utils with `.js` suffix
    
    * add changeset
    
    * Update packages/eslint-config/src/index.ts
    
    ---------
    
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

---
+++ examples/docs/src/app/showcase/(overview)/page.jsx
@@ -0,0 +1,20 @@
+/**
+ * @see https://github.com/shuding/nextra/issues/4148
+ */
+
+export const metadata = {}
+
+export default function Page() {
+  return (
+    <h1
+      style={{
+        textAlign: 'center',
+        fontSize: 64,
+        margin: '25vh 0',
+        fontWeight: 'bold'
+      }}
+    >
+      Showcase
+    </h1>
+  )
+}
