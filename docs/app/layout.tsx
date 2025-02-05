bbc760b7f415fa1bb9fb3e51edc44c04d0de4192
Hichem Fantar <fantar.hichem@gmail.com>
2025-02-05 17:16:40 +0100

    Add motion-reduce class to navbar logo for improved transition handling (#4160)
    * fix: add motion-reduce class to navbar logo for improved transition handling
    
    * fix: update motion-reduce class for navbar logo to improve hover transition

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -62,7 +62,7 @@ const navbar = (
       <NextraLogo
         height="20"
         className={cn(
-          'hover:transition-all hover:duration-1000',
+          'hover:transition-all hover:duration-1000 motion-reduce:hover:transition-none',
           '[mask-image:linear-gradient(60deg,#000_25%,rgba(0,0,0,.2)_50%,#000_75%)] [mask-position:0] [mask-size:400%]',
           'hover:[mask-position:100%]'
         )}
