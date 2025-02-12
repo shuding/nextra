a5586536524eb9e81c2f96c7342aadefc48710ac
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-12 21:56:51 +0700

    allow change `nextThemes.forcedTheme` prop

--- packages/nextra/src/server/schemas.ts
+++ packages/nextra/src/server/schemas.ts
@@ -1,6 +1,6 @@
 import type { ProcessorOptions } from '@mdx-js/mdx'
 import type { MathJax3Config } from 'better-react-mathjax'
-import type { ReactElement } from 'react'
+import type { ReactElement, ReactNode } from 'react'
 import { isValidElement } from 'react'
 import type { Options as RehypeKatexOptions } from 'rehype-katex'
 import type { Options as RehypePrettyCodeOptions } from 'rehype-pretty-code'
@@ -72,6 +72,9 @@ export const element = z.custom<ReactElement<Record<string, unknown>>>(
   isValidElement,
   { message: 'Must be React.ReactElement' }
 )
+export const reactNode = z.custom<ReactNode>(isValidElement, {
+  message: 'Must be React.ReactNode'
+})
 
 export const stringOrElement = z.union([z.string(), element])
 
