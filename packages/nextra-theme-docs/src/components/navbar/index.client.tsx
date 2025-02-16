23b1c48cbf82e0faba20823947ce673116cc0263
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-17 00:45:03 +0700

    Update index.client.tsx (#4233)
    * Update index.client.tsx
    
    * Create tough-shoes-check.md

--- packages/nextra-theme-docs/src/components/navbar/index.client.tsx
+++ packages/nextra-theme-docs/src/components/navbar/index.client.tsx
@@ -52,8 +52,8 @@ const NavbarMenu: FC<{
           'x:focus-visible:nextra-focus',
           'nextra-scrollbar x:origin-top x:transition x:duration-200 x:ease-out x:data-closed:scale-95 x:data-closed:opacity-0 x:motion-reduce:transition-none',
           'x:border x:border-black/5 x:dark:border-white/20',
-          'x:backdrop-blur-md x:bg-nextra-bg/70',
           'x:z-30 x:rounded-md x:py-1 x:text-sm x:shadow-lg',
+          'x:backdrop-blur-md x:bg-nextra-bg/70',
           // headlessui adds max-height as style, use !important to override
           'x:max-h-[min(calc(100vh-5rem),256px)]!'
         )}
