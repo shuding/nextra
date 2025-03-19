6a02a0134de232bb87cd1cd2631a097451041b00
Hichem Fantar <fantar.hichem@gmail.com>
2025-03-19 09:35:30 +0100

    Refactor sidebar component styles for improved readability and consistency

--- packages/nextra-theme-docs/src/components/sidebar.tsx
+++ packages/nextra-theme-docs/src/components/sidebar.tsx
@@ -158,8 +158,8 @@ const Folder: FC<FolderProps> = ({ item: _item, anchors, onFocus, level }) => {
           className={cn(
             'x:shrink-0',
             'x:rounded-sm x:p-0.5 x:hover:bg-gray-800/5 x:dark:hover:bg-gray-100/5',
-            'x:motion-reduce:*:transition-none x:*:origin-center x:*:transition-transform x:*:rtl:-rotate-180',
-            open && 'x:*:ltr:rotate-90 x:*:rtl:-rotate-270'
+            'x:motion-reduce:transition-none x:origin-center x:transition-all x:rtl:-rotate-180',
+            open && 'x:ltr:rotate-90 x:rtl:-rotate-270'
           )}
         />
       </ComponentToUse>
