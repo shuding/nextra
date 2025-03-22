ecfc792c3ededd001442897f7f3399a2e89878a8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-03-22 01:42:24 +0100

    refactor

--- packages/typescript/src/ui/type-table.tsx
+++ packages/typescript/src/ui/type-table.tsx
@@ -1,6 +1,6 @@
 import cn from 'clsx'
 import Link from 'next/link'
-import { Popup } from 'nextra/components'
+import { Code, Popup } from 'nextra/components'
 import { InformationCircleIcon as InfoIcon } from 'nextra/icons'
 import type { FC, ReactNode } from 'react'
 
@@ -8,9 +8,9 @@ const Info: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <Popup>
       <Popup.Button>
-        <InfoIcon className="size-4" />
+        <InfoIcon className="x:size-4" />
       </Popup.Button>
-      <Popup.Panel className="prose prose-no-margin max-h-[400px] max-w-[400px] min-w-[220px] overflow-auto text-sm">
+      <Popup.Panel className="x:max-h-[400px] x:max-w-[400px] x:min-w-[220px] x:overflow-auto x:text-sm">
         {children}
       </Popup.Panel>
     </Popup>
@@ -21,9 +21,9 @@ interface ObjectType {
   /**
    * Additional description of the field
    */
-  description?: ReactNode
+  description: ReactNode
   type: string
-  typeDescription?: ReactNode
+  typeDescription: ReactNode
   /**
    * Optional link to the type
    */
@@ -33,8 +33,8 @@ interface ObjectType {
 }
 
 const classes = {
-  field: cn('inline-flex flex-row items-center gap-1'),
-  code: cn('rounded-md bg-fd-secondary p-1 text-fd-secondary-foreground'),
+  field: cn('x:inline-flex x:items-center x:gap-1'),
+  code: cn('x:rounded-md bg-fd-secondary x:p-1 text-fd-secondary-foreground'),
   blue: 'bg-fd-primary/10 text-fd-primary'
 }
 
@@ -42,13 +42,15 @@ export const TypeTable: FC<{ type: Record<string, ObjectType> }> = ({
   type
 }) => {
   return (
-    <div className="prose prose-no-margin my-6 overflow-auto">
-      <table className="text-fd-muted-foreground text-sm whitespace-nowrap">
+    <div className="x:my-6 x:overflow-x-auto nextra-scrollbar">
+      <table className="text-fd-muted-foreground x:text-sm">
         <thead>
           <tr>
-            <th className="w-[45%]">Name</th>
-            <th className="w-[30%]">Type</th>
-            <th className="w-1/4">Default</th>
+            <th align="left" className="x:w-1/3">
+              Name
+            </th>
+            <th align="left">Type</th>
+            <th align="left">Default</th>
           </tr>
         </thead>
         <tbody>
@@ -56,32 +58,24 @@ export const TypeTable: FC<{ type: Record<string, ObjectType> }> = ({
             <tr key={key}>
               <td>
                 <div className={classes.field}>
-                  <code className={cn(classes.code, classes.blue)}>
+                  <Code>
                     {key}
                     {!value.required && '?'}
-                  </code>
+                  </Code>
                   {value.description && <Info>{value.description}</Info>}
                 </div>
               </td>
               <td>
-                <div className={classes.field}>
-                  <code className={classes.code}>{value.type}</code>
-                  {value.typeDescription && (
-                    <Info>{value.typeDescription}</Info>
-                  )}
-                  {value.typeDescriptionLink && (
-                    <Link href={value.typeDescriptionLink}>
-                      <InfoIcon className="size-4" />
-                    </Link>
-                  )}
-                </div>
+                <Code>{value.type}</Code>
+                {value.typeDescription && <Info>{value.typeDescription}</Info>}
+                {value.typeDescriptionLink && (
+                  <Link href={value.typeDescriptionLink}>
+                    <InfoIcon className="x:size-4" />
+                  </Link>
+                )}
               </td>
               <td>
-                {value.default ? (
-                  <code className={classes.code}>{value.default}</code>
-                ) : (
-                  <span>-</span>
-                )}
+                {value.default ? <Code>{value.default}</Code> : <span>-</span>}
               </td>
             </tr>
           ))}
