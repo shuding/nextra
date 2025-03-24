4373b04f8a8d0ce3ff0ef4688a8e8a963f32c61c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-03-24 17:48:46 +0100

    more

--- packages/typescript/src/ui/props-table.tsx
+++ packages/typescript/src/ui/props-table.tsx
@@ -2,7 +2,7 @@ import slugify from '@sindresorhus/slugify'
 import cn from 'clsx'
 import Link from 'next/link'
 import { Code } from 'nextra/components'
-import type { FC } from 'react'
+import type { FC, ReactNode } from 'react'
 
 type PropsTableProps = {
   props: {
@@ -18,13 +18,26 @@ type PropsTableProps = {
   deeplinkPrefix?: string
 }
 
-export const PropsTable: FC<PropsTableProps> = ({
-  props: data,
-  links = {}
-}) => {
+interface ObjectType {
+  /**
+   * Additional description of the field
+   */
+  description: ReactNode
+  type: string
+  typeDescription: ReactNode
+  /**
+   * Optional link to the type
+   */
+  typeDescriptionLink?: string
+  default?: string
+  required?: boolean
+}
+
+
+export const PropsTable: FC<{ type: Record<string, ObjectType> }> = ({ type }) => {
   // We can hide the default column entirely if none of the props have a default
   // value to document.
-  const showDefaultColumn = data.some(prop => prop.default)
+  const showDefaultColumn = true as any // data.some(prop => prop.default)
 
   // This function takes a string representing some type and attempts to turn any
   // types referenced inside into links, either internal or external.
@@ -34,7 +47,7 @@ export const PropsTable: FC<PropsTableProps> = ({
         <Link
           key={`${chunk}-${i}`}
           href={links[chunk] ?? ''}
-          className="text-primary-600"
+          className="x:text-primary-600"
         >
           {chunk}
         </Link>
@@ -44,12 +57,12 @@ export const PropsTable: FC<PropsTableProps> = ({
     )
 
   return (
-    <table className="my-8 w-full text-sm">
-      <thead className="nextra-border border-b text-left max-lg:hidden">
+    <table className="x:my-8 x:w-full x:text-sm">
+      <thead className="nextra-border x:border-b x:text-left x:max-lg:hidden">
         <tr>
-          <th className="py-1.5">Name</th>
-          <th className="p-1.5 px-3">Type</th>
-          {showDefaultColumn && <th className="py-1.5">Default</th>}
+          <th className="x:py-1.5">Name</th>
+          <th className="x:p-1.5 x:px-3">Type</th>
+          {showDefaultColumn && <th className="x:py-1.5">Default</th>}
         </tr>
       </thead>
       {data.map(prop => {
@@ -59,42 +72,42 @@ export const PropsTable: FC<PropsTableProps> = ({
         return (
           <tbody
             key={slug}
-            className="group nextra-border x:hover:bg-gray-100 x:dark:hover:bg-primary-100/5 mb-5 rounded-xl max-lg:block max-lg:border"
+            className="x:group nextra-border x:hover:bg-gray-100 x:dark:hover:bg-primary-100/5 x:mb-5 x:rounded-xl x:max-lg:block x:max-lg:border"
           >
             <tr
-              className="nextra-border max-lg:block lg:border-b lg:not-target:[&>td>a]:opacity-0"
+              className="nextra-border x:max-lg:block x:lg:border-b x:lg:not-target:[&>td>a]:opacity-0"
               id={slug}
             >
-              <td className="relative py-3 max-lg:block max-lg:px-3">
+              <td className="x:relative x:py-3 x:max-lg:block x:max-lg:px-3">
                 <a
                   href={`#${slug}`}
                   className={cn(
-                    'absolute top-0 right-0 text-lg font-black lg:top-1/2 lg:right-full lg:-translate-y-1/2',
-                    'group-hover:opacity-100! before:content-["#"] hover:text-black dark:hover:text-white',
-                    'p-3' // Increase hit box
+                    'x:absolute x:top-0 x:right-0 x:text-lg x:font-black x:lg:top-1/2 x:lg:right-full x:lg:-translate-y-1/2',
+                    'x:group-hover:opacity-100! x:before:content-["#"] x:hover:text-black x:dark:hover:text-white',
+                    'x:p-3' // Increase hit box
                   )}
                 />
                 {prop.name &&
                   (isHeading ? (
-                    <span className="px-1.5 py-0.5 font-bold">{prop.name}</span>
+                    <span className="x:px-1.5 x:py-0.5 x:font-bold">{prop.name}</span>
                   ) : (
                     <>
                       <Code>{prop.name}</Code>
                       {prop.optional && (
-                        <span className="ml-1 text-gray-500">(optional)</span>
+                        <span className="x:ml-1 x:text-gray-500">(optional)</span>
                       )}
                     </>
                   ))}
               </td>
-              <td className='p-3 max-lg:block max-lg:before:content-["Type:_"]'>
+              <td className='x:p-3 x:max-lg:block x:max-lg:before:content-["Type:_"]'>
                 <Code>{linkify(prop.type)}</Code>
                 {prop.description && (
-                  <div className="mt-2 max-w-md text-sm">
+                  <div className="x:mt-2 x:max-w-md x:text-sm">
                     {prop.description}
                   </div>
                 )}
                 {prop.example && (
-                  <div className="mt-2 rounded-lg border border-gray-200 bg-gray-50">
+                  <div className="x:mt-2 x:rounded-lg x:border x:border-gray-200 x:bg-gray-50">
                     <pre>
                       <code>{prop.example}</code>
                     </pre>
@@ -108,9 +121,9 @@ export const PropsTable: FC<PropsTableProps> = ({
                 // blank space we don't want.
                 <td
                   className={cn(
-                    'max-lg:block',
+                    'x:max-lg:block',
                     prop.default &&
-                      'py-3 max-lg:px-3 max-lg:before:content-["Default:_"]'
+                      'x:py-3 x:max-lg:px-3 x:max-lg:before:content-["Default:_"]'
                   )}
                 >
                   {prop.default && <Code>{linkify(prop.default)}</Code>}
