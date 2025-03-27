4ade007472a99490299ac29da15b76d80fc919fe
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-03-27 19:49:55 +0100

    not found page

--- packages/nextra-theme-docs/src/components/404/index.tsx
+++ packages/nextra-theme-docs/src/components/404/index.tsx
@@ -4,9 +4,22 @@ import { H1 } from '../../mdx-components/heading'
 import { NotFoundLink } from './index.client'
 
 type NotFoundPageProps = {
+  /**
+   * Content of the link.
+   * @default 'Submit an issue about broken link'
+   */
   content?: ReactNode
+  /**
+   * Labels that can be added to the new created issue.
+   * @default 'bug'
+   */
   labels?: string
+  /**
+   * Top content of page.
+   * @default <H1>404: Page Not Found</H1>
+   */
   children?: ReactNode
+  /** CSS class name. */
   className?: string
 }
 
