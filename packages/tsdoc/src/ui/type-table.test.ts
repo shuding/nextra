4b1d9d194cfd92be573c0fae397828dcc4ca21a4
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-03-27 19:39:51 +0100

    upd

--- packages/tsdoc/src/ui/type-table.test.ts
+++ packages/tsdoc/src/ui/type-table.test.ts
@@ -2,18 +2,23 @@ import { getTypeTableOutput } from '../utils/type-table'
 
 describe('TypeTable', () => {
   it('<Banner />', async () => {
-    const code = `import type { ComponentProps } from 'react'
-import type { Banner } from 'nextra/components'
-type BannerProps = ComponentProps<typeof Banner>
-export default BannerProps`
+    const code = `import type { Banner } from 'nextra/components'
+type $ = React.ComponentProps<typeof Banner>
+export default $`
     const result = await getTypeTableOutput({ code })
     expect(result).toMatchSnapshot()
   })
   it('<Search />', async () => {
-    const code = `import type { ComponentProps } from 'react'
-import type { Search } from 'nextra/components'
-type SearchProps = ComponentProps<typeof Search>
-export default SearchProps`
+    const code = `import type { Search } from 'nextra/components'
+type $ = React.ComponentProps<typeof Search>
+export default $`
+    const result = await getTypeTableOutput({ code })
+    expect(result).toMatchSnapshot()
+  })
+  it('<Callout />', async () => {
+    const code = `import type { Callout } from 'nextra/components'
+type $ = React.ComponentProps<typeof Callout>
+export default $`
     const result = await getTypeTableOutput({ code })
     expect(result).toMatchSnapshot()
   })
