b35f8d6ccfd4012dd260f6f5da2e58fb05c12da3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-03-27 12:49:16 +0100

    more

--- packages/typescript/src/lib/zod-to-ts.ts
+++ packages/typescript/src/lib/zod-to-ts.ts
@@ -3,20 +3,19 @@ import { z } from 'zod'
 type TsType = string | { type: string; optional: boolean }
 
 export function generateTsFromZod(schema: z.ZodTypeAny, indent = 2): string {
-  // Handle objects explicitly
-  if (schema instanceof z.ZodObject) {
-    return `{\n${Object.entries(schema.shape)
-      .map(([key, value]) => {
-        const result = generateTsFromZodType(value, indent + 2)
-        const tsType = typeof result === 'object' ? result.type : result
-        const optional = typeof result === 'object' && result.optional
-        const docComment = getDocComment(value, indent)
-        return `${docComment}${' '.repeat(indent)}${key}${optional ? '?' : ''}: ${tsType};\n`
-      })
-      .join('\n')}${' '.repeat(indent - 2)}}`
+  const isZodObject = schema instanceof z.ZodObject
+  if (!isZodObject) {
+    return generateTsFromZodType(schema, indent)
   }
-
-  return generateTsFromZodType(schema, indent)
+  return `{\n${Object.entries(schema.shape)
+    .map(([key, value]) => {
+      const result = generateTsFromZodType(value, indent + 2)
+      const tsType = typeof result === 'object' ? result.type : result
+      const optional = typeof result === 'object' && result.optional
+      const docComment = getDocComment(value, indent)
+      return `${docComment}${' '.repeat(indent)}${key}${optional ? '?' : ''}: ${tsType};\n`
+    })
+    .join('\n')}${' '.repeat(indent - 2)}}`
 }
 
 function generateTsFromZodType(schema: z.ZodTypeAny, indent: number): TsType {
@@ -57,18 +56,21 @@ function generateTsFromZodType(schema: z.ZodTypeAny, indent: number): TsType {
 }
 
 function getDocComment(schema: z.ZodTypeAny, indent: number): string {
-  const { description } = schema._def as any
+  const { description } = schema._def
   const defaultValue =
     schema instanceof z.ZodDefault ? schema._def.defaultValue() : undefined
   const comments: string[] = []
 
-  if (description) comments.push(` * @description ${description}`)
-  if (defaultValue !== undefined)
-    comments.push(` * @default ${JSON.stringify(defaultValue)}`)
-
-  if (comments.length) {
-    const spacing = ' '.repeat(indent)
-    return `${spacing}/**\n${comments.map(line => `${spacing}${line}`).join('\n')}\n${spacing} */\n`
+  if (description) {
+    comments.push(` * @description ${description}`)
   }
-  return ''
+  if (defaultValue !== undefined) {
+    comments.push(` * @default ${JSON.stringify(defaultValue)}`)
+  }
+
+  if (!comments.length) {
+    return ''
+  }
+  const spacing = ' '.repeat(indent)
+  return `${spacing}/**\n${comments.map(line => `${spacing}${line}`).join('\n')}\n${spacing} */\n`
 }
