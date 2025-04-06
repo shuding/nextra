812dbc6186de44305f977f0ef9df4f691f9ce5a3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-04-06 23:00:34 +0200

    upd

--- packages/nextra/src/client/components/callout.tsx
+++ packages/nextra/src/client/components/callout.tsx
@@ -1,81 +1,48 @@
 import cn from 'clsx'
 import type { FC, ReactNode } from 'react'
 import {
-  GitHubAlertCautionIcon,
-  GitHubAlertImportantIcon,
-  GitHubAlertNoteIcon,
-  GitHubAlertTipIcon,
-  GitHubAlertWarningIcon,
-  InformationCircleIcon
+  GitHubCautionIcon,
+  GitHubImportantIcon,
+  GitHubNoteIcon,
+  GitHubTipIcon,
+  GitHubWarningIcon
 } from '../icons/index.js'
 
-const test = {
-  i: ['#0969da', '#0969da', '#1f6feb', '#4493f8'],
-  t: ['#1a7f37', '#1a7f37', '#238636', '#3fb950'],
-  im: ['#8250df', '#8250df', '#8957e5', '#ab7df8'],
-  w: ['#9a6700', '#9a6700', '#9e6a03', '#d29922'],
-  c: ['#cf222e', '#d1242f', '#da3633', '#f85149']
-}
-
 const TypeToEmoji = {
-  default: '💡',
-  error: '🚫',
-  info: <InformationCircleIcon height="16" className="x:mt-1" />,
-  warning: '⚠️',
-  'github-note': <GitHubAlertNoteIcon height="16" className="x:mt-1.5" />,
-  'github-tip': <GitHubAlertTipIcon height="16" className="x:mt-1.5" />,
-  'github-important': (
-    <GitHubAlertImportantIcon height="16" className="x:mt-1.5" />
-  ),
-  'github-warning': <GitHubAlertWarningIcon height="16" className="x:mt-1.5" />,
-  'github-caution': <GitHubAlertCautionIcon height="16" className="x:mt-1.5" />
+  default: <GitHubTipIcon height="16" className="x:mt-1.5" />,
+  error: <GitHubCautionIcon height="16" className="x:mt-1.5" />,
+  info: <GitHubNoteIcon height="16" className="x:mt-1.5" />,
+  warning: <GitHubWarningIcon height="16" className="x:mt-1.5" />,
+  important: <GitHubImportantIcon height="16" className="x:mt-1.5" />
 }
 
 type CalloutType = keyof typeof TypeToEmoji
-export type GitHubAlertType =
-  | 'github-note'
-  | 'github-tip'
-  | 'github-important'
-  | 'github-warning'
-  | 'github-caution'
 
 const classes: Record<CalloutType, string> = {
   default: cn(
-    'x:border-orange-100 x:bg-orange-50 x:text-orange-800 x:dark:border-orange-400/30 x:dark:bg-orange-400/20 x:dark:text-orange-300'
-  ),
-  error: cn(
-    'x:border-red-200 x:bg-red-100 x:text-red-900 x:dark:border-red-200/30 x:dark:bg-red-900/30 x:dark:text-red-200'
-  ),
-  info: cn(
-    'x:border-blue-200 x:bg-blue-100 x:text-blue-900 x:dark:border-blue-200/30 x:dark:bg-blue-900/30 x:dark:text-blue-200'
-  ),
-  warning: cn(
-    'x:border-yellow-100 x:bg-yellow-50 x:text-yellow-900 x:dark:border-yellow-200/30 x:dark:bg-yellow-700/30 x:dark:text-yellow-200'
-  ),
-  'github-note': cn(
-    'x:bg-blue-100 x:dark:bg-blue-900/30',
-    'x:text-blue-700 x:dark:text-blue-400',
-    'x:border-blue-700 x:dark:border-blue-600'
-  ),
-  'github-tip': cn(
     'x:bg-green-100 x:dark:bg-green-900/30',
     'x:text-green-700 x:dark:text-green-500',
     'x:border-green-700 x:dark:border-green-800'
   ),
-  'github-important': cn(
-    'x:bg-purple-100 x:dark:bg-purple-900/30',
-    'x:text-purple-600 x:dark:text-purple-400',
-    'x:border-purple-600'
+  error: cn(
+    'x:bg-red-100 x:dark:bg-red-900/30',
+    'x:text-red-700 x:dark:text-red-500',
+    'x:border-red-700 x:dark:border-red-600'
   ),
-  'github-warning': cn(
+  info: cn(
+    'x:bg-blue-100 x:dark:bg-blue-900/30',
+    'x:text-blue-700 x:dark:text-blue-400',
+    'x:border-blue-700 x:dark:border-blue-600'
+  ),
+  warning: cn(
     'x:bg-yellow-50 x:dark:bg-yellow-700/30',
     'x:text-yellow-700 x:dark:text-yellow-500',
     'x:border-yellow-700'
   ),
-  'github-caution': cn(
-    'x:bg-red-100 x:dark:bg-red-900/30',
-    'x:text-red-700 x:dark:text-red-500',
-    'x:border-red-700 x:dark:border-red-600'
+  important: cn(
+    'x:bg-purple-100 x:dark:bg-purple-900/30',
+    'x:text-purple-600 x:dark:text-purple-400',
+    'x:border-purple-600'
   )
 }
 
@@ -91,10 +58,11 @@ type CalloutProps = {
    * Icon displayed in the callout. Can be a string emoji or a custom React element.
    *
    * Default values based on `type`:
-   * - `'💡'` for `type: 'default'`
-   * - `'🚫'` for `type: 'error'`
-   * - `<InformationCircleIcon />` for `type: 'info'`
-   * - `'⚠️'` for `type: 'warning'`
+   * - `<GitHubTipIcon />` for `type: 'default'`
+   * - `<GitHubCautionIcon />` for `type: 'error'`
+   * - `<GitHubNoteIcon />` for `type: 'info'`
+   * - `<GitHubWarningIcon />` for `type: 'warning'`
+   * - `<GitHubImportantIcon />` for `type: 'important'`
    * @default Determined by `type`
    */
   emoji?: ReactNode
