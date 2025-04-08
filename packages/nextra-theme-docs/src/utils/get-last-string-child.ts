3b7ef35304e3811f0bc710c86084c13a9c39849e
Eduard Martos <eduard.martos@avanade.com>
2025-04-08 21:30:38 +0200

    fix code issues and refactoring following existing code rules

--- packages/nextra-theme-docs/src/utils/get-last-string-child.ts
+++ packages/nextra-theme-docs/src/utils/get-last-string-child.ts
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from 'react'
 
 interface MyComponentProps {
-  children?: React.ReactNode | string;
-
+  children?: React.ReactNode | string
 }
 
-const getLastStringChild = (element: React.ReactNode | string): string | null => {
+export function getLastStringChild
+(
+  element: React.ReactNode | string
+): string {
   if (typeof element === 'string') {
-    return element;
+    return element
   }
 
   if (React.isValidElement(element)) {
-    const props = element.props as MyComponentProps; // Type assertion
-    const children = React.Children.toArray(props.children);
+    const props = element.props as MyComponentProps
+    const children = React.Children.toArray(props.children)
     for (let i = children.length - 1; i >= 0; i--) {
-      const child = children[i];
-      const result = getLastStringChild(child);
+      const child = children[i]
+      const result = getLastStringChild(child)
       if (result) {
-        return result;
+        return result
       }
     }
   }
-
-  return null;
-};
-
-export default getLastStringChild;
+  
+  return ''
+}
