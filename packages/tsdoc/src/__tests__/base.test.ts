aa2cfabcdabd373389c7f797bba8961ba59494b7
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-04-08 23:41:54 +0200

    upd

--- packages/tsdoc/src/__tests__/base.test.ts
+++ packages/tsdoc/src/__tests__/base.test.ts
@@ -1363,10 +1363,10 @@ export default foo`
 
        return (
          <ReactFlow
-           nodes={nodes}
-           edges={edges}
-           onNodesChange={onNodesChange}
-           onEdgesChange={onEdgesChange}
+           nodes='{nodes}'
+           edges='{edges}'
+           onNodesChange='{onNodesChange}'
+           onEdgesChange='{onEdgesChange}'
          />
        );
       }
@@ -1416,8 +1416,66 @@ export default foo`
     `)
   })
 
+  test.only('should flatten params', () => {
+    const result = generateDocumentation({
+      code: "export { getSmoothStepPath as default } from '@xyflow/react'",
+      flattened: true
+    })
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "description": "The \`getSmoothStepPath\` util returns everything you need to render a stepped path
+      between two nodes. The \`borderRadius\` property can be used to choose how rounded
+      the corners of those steps are.",
+        "name": "getSmoothStepPath",
+        "signatures": [
+          {
+            "params": [
+              {
+                "name": "__0",
+                "type": "GetSmoothStepPathParams",
+              },
+            ],
+            "returns": {
+              "type": "[path: string, labelX: number, labelY: number, offsetX: number, offsetY: number]",
+            },
+          },
+        ],
+        "tags": {
+          "example": "\`\`\`js
+       const source = { x: 0, y: 20 };
+       const target = { x: 150, y: 100 };
+
+       const [path, labelX, labelY, offsetX, offsetY] = getSmoothStepPath({
+         sourceX: source.x,
+         sourceY: source.y,
+         sourcePosition: Position.Right,
+         targetX: target.x,
+         targetY: target.y,
+         targetPosition: Position.Left,
+       });
+      \`\`\`",
+          "public": "",
+          "remarks": "This function returns a tuple (aka a fixed-size array) to make it easier to work with multiple edge paths at once.",
+          "returns": "A path string you can use in an SVG, the \`labelX\` and \`labelY\` position (center of path)
+      and \`offsetX\`, \`offsetY\` between source handle and label.
+
+      - \`path\`: the path to use in an SVG \`<path>\` element.
+      - \`labelX\`: the \`x\` position you can use to render a label for this edge.
+      - \`labelY\`: the \`y\` position you can use to render a label for this edge.
+      - \`offsetX\`: the absolute difference between the source \`x\` position and the \`x\` position of the
+      middle of this path.
+      - \`offsetY\`: the absolute difference between the source \`y\` position and the \`y\` position of the
+      middle of this path.",
+        },
+      }
+    `)
+  })
+
   test('should flatten only object', () => {
-    const result = generateDocumentation({ code: typesFixture, flattened: true })
+    const result = generateDocumentation({
+      code: typesFixture,
+      flattened: true
+    })
     expect(result).toMatchInlineSnapshot(`
       {
         "description": "",
