7c3c026982acce1b07595ed7aad587d8139a8d20
Eduard Martos <eduard.martos@avanade.com>
2025-04-08 21:15:21 +0200

    solve"error TS18046: 'element.props' is of type 'unknown'"

--- packages/nextra-theme-docs/src/utils/get-last-string-child.ts
+++ packages/nextra-theme-docs/src/utils/get-last-string-child.ts
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const getLastStringChild = (element: React.ReactNode): string | null => {
+interface MyComponentProps {
+  children?: React.ReactNode | string;
+
+}
+
+const getLastStringChild = (element: React.ReactNode | string): string | null => {
   if (typeof element === 'string') {
     return element;
   }
 
-  if (React.isValidElement(element) && element.props.children) {
-    const children = React.Children.toArray(element.props.children);
+  if (React.isValidElement(element)) {
+    const props = element.props as MyComponentProps; // Type assertion
+    const children = React.Children.toArray(props.children);
     for (let i = children.length - 1; i >= 0; i--) {
       const child = children[i];
       const result = getLastStringChild(child);
