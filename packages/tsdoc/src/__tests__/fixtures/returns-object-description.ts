24d6b22491f01da80966e3f3a3934979f99fd4a9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-04-09 03:08:58 +0200

    upd

--- packages/tsdoc/src/__tests__/fixtures/returns-object-description.ts
+++ packages/tsdoc/src/__tests__/fixtures/returns-object-description.ts
@@ -1,18 +1,21 @@
+/** Type B */
 type B = {
-  /** C */
-  c: {
-    /** D */
-    d: string
+  /** Field c */
+  c: 1
+  d: {
+    /** Field e */
+    e: string
   }
 }
 
+/** Function foo */
 function foo() {
   // @ts-expect-error -- ignore
   const b: B = {}
   return {
-    /** A */
-    a: 1,
-    /** B */
+    /** Field a */
+    a: 0,
+    /** Field b */
     b
   }
 }
