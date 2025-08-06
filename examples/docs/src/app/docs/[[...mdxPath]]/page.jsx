22e47eaf5fa59f47eb00a6b4dca0b19b9ecbce82
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-08-06 11:14:45 +0300

    add changeset

--- examples/docs/src/app/docs/[[...mdxPath]]/page.jsx
+++ examples/docs/src/app/docs/[[...mdxPath]]/page.jsx
@@ -13,8 +13,12 @@ const Wrapper = getMDXComponents().wrapper
 
 export default async function Page(props) {
   const params = await props.params
-  const result = await importPage(params.mdxPath)
-  const { default: MDXContent, toc, metadata, sourceCode } = result
+  const {
+    default: MDXContent,
+    toc,
+    metadata,
+    sourceCode
+  } = await importPage(params.mdxPath)
   return (
     <Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
       <MDXContent {...props} params={params} />
