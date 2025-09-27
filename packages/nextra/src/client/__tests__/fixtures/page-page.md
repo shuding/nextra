8ab2bb31e02afea96090789ac79a0617f67c60ff
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-09-28 00:33:58 +0300

    add test

---
+++ packages/nextra/src/client/__tests__/fixtures/page-page.md
@@ -0,0 +1,20 @@
+# `page.mdx` File
+
+[`page` file](https://nextjs.org/docs/app/api-reference/file-conventions/page) is a special [Next.js App Router convention file ](https://nextjs.org/docs/app/getting-started/project-structure#routing-files) which allows you to define UI that is unique to a route:
+
+- app
+
+  - layout.jsx
+
+  - page.jsx
+
+  - docs
+
+    - page.mdx
+    - getting-started
+      - page.mdx
+
+**Tip**
+
+By default, the `.js`, `.jsx`, or `.tsx` file extensions can be used for `page`.\
+Nextra enhance them with `.md` and `.mdx` extensions.
--- packages/nextra/src/client/__tests__/html-to-md.test.ts
+++ packages/nextra/src/client/__tests__/html-to-md.test.ts
@@ -2,10 +2,16 @@ import { describe } from 'vitest'
 import { htmlToMarkdown } from '../html-to-md'
 // @ts-expect-error
 import metaPage from './fixtures/meta-page.txt?raw'
+// @ts-expect-error
+import pagePage from './fixtures/page-page.txt?raw'
 
 describe('html-to-md', () => {
-  it.only('should work', async () => {
+  it.only('meta page', async () => {
     const md = await htmlToMarkdown(metaPage)
     expect(md).toMatchFileSnapshot('./fixtures/meta-page.md')
+  })
+  it.only('page page', async () => {
+    const md = await htmlToMarkdown(pagePage)
+    expect(md).toMatchFileSnapshot('./fixtures/page-page.md')
   })
 })
