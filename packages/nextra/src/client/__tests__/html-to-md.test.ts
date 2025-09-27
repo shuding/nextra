7fbbadbdb527d0f80e3f3317f2286ac303bae27f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-09-28 01:05:11 +0300

    remove only

--- packages/nextra/src/client/__tests__/html-to-md.test.ts
+++ packages/nextra/src/client/__tests__/html-to-md.test.ts
@@ -1,16 +1,14 @@
 import { describe } from 'vitest'
 import { htmlToMarkdown } from '../html-to-md'
-// @ts-expect-error
 import metaPage from './fixtures/meta-page.txt?raw'
-// @ts-expect-error
 import pagePage from './fixtures/page-page.txt?raw'
 
 describe('html-to-md', () => {
-  it.only('meta page', async () => {
+  it('meta page', async () => {
     const md = await htmlToMarkdown(metaPage)
     await expect(md).toMatchFileSnapshot('./fixtures/meta-page.md')
   })
-  it.only('page page', async () => {
+  it('page page', async () => {
     const md = await htmlToMarkdown(pagePage)
     await expect(md).toMatchFileSnapshot('./fixtures/page-page.md')
   })
